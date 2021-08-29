f84cfbffc3345adde0d822989eff456fe52a522a
paruma <paruma184az@gmail.com>
2021-08-30 01:58:00 +0900

    ゲノコン2021A AC

--- src/maina.rs
+++ src/maina.rs
@@ -1,11 +1,32 @@
-#[test]
-fn test1() {
-    let a = 3;
-    let b = 1 + 2;
-    assert_eq!(a, b);
+use proconio::input;
+use proconio::marker::Bytes;
+
+fn read() -> (usize, Vec<Vec<u8>>) {
+    input! {
+        m: usize,
+        seq_vec: [Bytes; m]
+    }
+    (m, seq_vec)
+}
+
+fn complementary_char(ch: u8) -> u8 {
+    match ch {
+        b'A' => b'T',
+        b'T' => b'A',
+        b'G' => b'C',
+        b'C' => b'G',
+        other => other,
+    }
+}
+
+fn reverse_complement_sequence(seq: Vec<u8>) -> Vec<u8> {
+    seq.into_iter().rev().map(complementary_char).collect()
 }
 
 fn main() {
-    let _x = 1;
-    println!("Hello, world!");
+    let (_, seq_vec) = read();
+
+    let ans = seq_vec.into_iter().map(reverse_complement_sequence);
+
+    ans.for_each(|x| println!("{}", String::from_utf8(x).unwrap()));
 }
--- src/mainb.rs
+++ src/mainb.rs
@@ -1,4 +1,4 @@
+
 fn main() {
-    let _x = 1;
-    println!("Hello, world! from mainb");
+    println!("hoge");
 }
