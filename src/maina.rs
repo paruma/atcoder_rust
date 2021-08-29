881466eb1716010e6645146c984960b4525d530e
paruma <paruma184az@gmail.com>
2021-08-29 23:15:33 +0900

    複数のエントリーポイントを持てるようにした

---
+++ src/maina.rs
@@ -0,0 +1,11 @@
+#[test]
+fn test1() {
+    let a = 3;
+    let b = 1 + 2;
+    assert_eq!(a, b);
+}
+
+fn main() {
+    let _x = 1;
+    println!("Hello, world!");
+}
---
+++ src/mainb.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let _x = 1;
+    println!("Hello, world! from mainb");
+}
