cddd23c87ed69b96825970ee2bf59572fff67358
paruma <paruma184az@gmail.com>
2021-09-08 19:29:44 +0900

    modintリファクタリング

--- src/mylib/modint.rs
+++ src/mylib/modint.rs
@@ -4,20 +4,6 @@
 mod rr {
     pub const MOD: i64 = 1_000_000_007;
 
-    pub trait Ring {
-        fn zero() -> Self;
-        fn one() -> Self;
-        fn mul(&self, other: &Self) -> Self;
-        fn add(&self, other: &Self) -> Self;
-        fn neg(&self) -> Self;
-        fn sub(&self, other: &Self) -> Self
-        where
-            Self: std::marker::Sized,
-        {
-            self.add(&other.neg())
-        }
-    }
-
     #[allow(clippy::upper_case_acronyms)]
     #[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
     pub struct RR {
@@ -37,25 +23,51 @@ mod rr {
         }
     }
 
+    trait Ring {
+        fn zero() -> Self;
+        fn one() -> Self;
+        fn mul(self, rhs: Self) -> Self;
+        fn add(self, rhs: Self) -> Self;
+        fn neg(self) -> Self;
+        fn sub(self, rhs: Self) -> Self
+        where
+            Self: std::marker::Sized,
+        {
+            self.add(rhs.neg())
+        }
+    }
+
     impl Ring for RR {
         fn zero() -> Self {
-            RR { rep: 0 }
+            RR::new(0)
         }
 
         fn one() -> Self {
-            RR { rep: 1 }
+            RR::new(1)
         }
 
-        fn mul(&self, other: &Self) -> Self {
-            RR::new(self.rep * other.rep)
+        fn mul(self, rhs: Self) -> Self {
+            RR::new(self.rep * rhs.rep)
         }
 
-        fn add(&self, other: &Self) -> Self {
-            RR::new(self.rep + other.rep)
+        fn add(self, rhs: Self) -> Self {
+            RR::new(self.rep + rhs.rep)
         }
 
-        fn neg(&self) -> Self {
+        fn neg(self) -> Self {
             RR::new(-self.rep)
+        }
+    }
+
+    impl RR {
+        #[allow(dead_code)]
+        pub fn zero() -> Self {
+            Ring::zero()
+        }
+
+        #[allow(dead_code)]
+        pub fn one() -> Self {
+            Ring::one()
         }
     }
 
@@ -63,15 +75,21 @@ mod rr {
         type Output = Self;
 
         fn add(self, rhs: Self) -> Self::Output {
-            Ring::add(&self, &rhs)
+            Ring::add(self, rhs)
         }
     }
+    impl std::ops::Neg for RR {
+        type Output = Self;
 
+        fn neg(self) -> Self::Output {
+            Ring::neg(self)
+        }
+    }
     impl std::ops::Sub for RR {
         type Output = Self;
 
         fn sub(self, rhs: Self) -> Self::Output {
-            Ring::sub(&self, &rhs)
+            Ring::sub(self, rhs)
         }
     }
 
@@ -79,42 +97,7 @@ mod rr {
         type Output = Self;
 
         fn mul(self, rhs: Self) -> Self::Output {
-            Ring::mul(&self, &rhs)
-        }
-    }
-
-    impl std::ops::Neg for RR {
-        type Output = Self;
-
-        fn neg(self) -> Self::Output {
-            Ring::neg(&self)
-        }
-    }
-}
-
-mod rf {}
-mod exam {
-    //use super::*;
-    use super::rr::*;
-    impl Ring for i32 {
-        fn zero() -> Self {
-            0
-        }
-
-        fn one() -> Self {
-            1
-        }
-
-        fn mul(&self, other: &Self) -> Self {
-            self * other
-        }
-
-        fn add(&self, other: &Self) -> Self {
-            self + other
-        }
-
-        fn neg(&self) -> Self {
-            -self
+            Ring::mul(self, rhs)
         }
     }
 }
@@ -124,24 +107,13 @@ mod tests {
     //use super::*;
 
     #[test]
-    fn test_ring_i32() {
-        use super::rr::*;
-        let x = 3;
-        let y = 4;
-        assert_eq!(x.mul(&y), 12);
-        assert_eq!(x.add(&y), 7);
-        assert_eq!(x.sub(&y), -1);
-        assert_eq!(x.neg(), -3);
-        assert_eq!(i32::one(), 1);
-        assert_eq!(i32::zero(), 0);
-    }
-
-    #[test]
     fn test_rr() {
         use super::rr::*;
         let x = RR::new(3);
         let y = RR::new(7);
-        x.add(&y);
+
+        assert_eq!(RR::one(), RR::new(1));
+        assert_eq!(RR::zero(), RR::new(0));
         assert_eq!(x + y, RR::new(10));
         assert_eq!(x - y, RR::new(MOD - 4));
         assert_eq!(y - x, RR::new(4));
