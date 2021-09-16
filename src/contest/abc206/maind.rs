a7b0f985cc6c336f35492e9ead7bc4afdc07a146
paruma <paruma184az@gmail.com>
2021-09-16 10:25:37 +0900

    ABC206,208-D

--- src/contest/abc206/maind.rs
+++ src/contest/abc206/maind.rs
@@ -1,17 +1,141 @@
 #![allow(clippy::let_unit_value)]
 use proconio::input;
+use union_find::*;
 
-fn read() {
-    input! {}
+pub mod union_find {
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    pub struct Root {
+        count: i32,
+    }
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    pub enum Node {
+        Root { root: Root },
+        NonRoot { parent_index: usize },
+    }
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    pub struct RootAndIndex {
+        root: Root,
+        index: usize,
+    }
+    #[derive(Clone, Debug)]
+    pub struct UnionFind {
+        pub nodes: Vec<Node>,
+    }
+    impl UnionFind {
+        pub fn new(n: usize) -> UnionFind {
+            UnionFind {
+                nodes: vec![
+                    Node::Root {
+                        root: Root { count: 1 }
+                    };
+                    n
+                ],
+            }
+        }
+        fn root_node(&mut self, index: usize) -> RootAndIndex {
+            match self.nodes[index] {
+                Node::Root { root } => RootAndIndex { root, index },
+                Node::NonRoot { parent_index } => {
+                    let root_and_index = self.root_node(parent_index);
+                    self.nodes[index] = Node::NonRoot {
+                        parent_index: root_and_index.index,
+                    };
+                    root_and_index
+                }
+            }
+        }
+        pub fn root(&mut self, index: usize) -> usize {
+            self.root_node(index).index
+        }
+        pub fn same_count(&mut self, index: usize) -> i32 {
+            self.root_node(index).root.count
+        }
+        pub fn same(&mut self, x: usize, y: usize) -> bool {
+            self.root(x) == self.root(y)
+        }
+        pub fn num_groups(&self) -> usize {
+            self.nodes
+                .iter()
+                .filter(|&node| matches ! (node , Node :: Root {.. } ))
+                .count()
+        }
+        pub fn unite(&mut self, x: usize, y: usize) {
+            if self.same(x, y) {
+                return;
+            }
+            let x_root_node = self.root_node(x);
+            let y_root_node = self.root_node(y);
+            let x_count = x_root_node.root.count;
+            let y_count = y_root_node.root.count;
+            let x_root_index = x_root_node.index;
+            let y_root_index = y_root_node.index;
+            if x_count < y_count {
+                self.nodes[x_root_index] = Node::NonRoot {
+                    parent_index: y_root_index,
+                };
+                self.nodes[y_root_index] = Node::Root {
+                    root: Root {
+                        count: x_count + y_count,
+                    },
+                }
+            } else {
+                self.nodes[y_root_index] = Node::NonRoot {
+                    parent_index: x_root_index,
+                };
+                self.nodes[x_root_index] = Node::Root {
+                    root: Root {
+                        count: x_count + y_count,
+                    },
+                }
+            }
+        }
+    }
 }
 
-fn solve() {}
+// 7:26~
+fn read() -> (i64, Vec<i64>) {
+    input! {n:i64, seq: [i64;n]}
+    (n, seq)
+}
 
-fn output() {}
+fn solve(n: i64, seq: &[i64]) -> i64 {
+    let n = n as usize;
+    let max = *seq.iter().max().unwrap() as usize;
+    let mut uf = UnionFind::new(max + 1);
+
+    for i in 0..n / 2 {
+        let small_i = i;
+        let big_i = n - 1 - i;
+
+        uf.unite(seq[small_i] as usize, seq[big_i] as usize);
+    }
+
+    /*
+        uf.nodes
+            .iter()
+            .filter(|&node| matches!(node, Node::NonRoot{..}))
+            .count() as i64;
+    */
+    ((max + 1) - uf.num_groups()) as i64
+}
+
+//fn output() {}
 
 fn main() {
-    let () = read();
-    let _ = solve();
-    output();
-    //println!("{}", ans);
+    let (n, seq) = read();
+    let ans = solve(n, &seq);
+    //output();
+    println!("{}", ans);
+}
+
+#[test]
+fn make_rand_input() {
+    use rand::rngs::SmallRng;
+    use rand::Rng;
+    use rand::SeedableRng;
+
+    let mut rng = SmallRng::from_entropy();
+    for _ in 0..10 {
+        println!("{}", rng.gen_range(1, 6));
+    }
 }
---
+++ src/contest/abc208/maind.rs
@@ -0,0 +1,106 @@
+#![allow(clippy::let_unit_value)]
+use ndarray::{s, Array, Array2, Array3};
+use proconio::input;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+struct Edge {
+    src: usize,
+    dst: usize,
+    cost: i64,
+}
+
+fn read() -> (usize, usize, Vec<Edge>) {
+    input! {n_vertex: usize, n_edge: usize,
+        edge_info: [(usize, usize, i64); n_edge],
+    }
+    let edges = edge_info
+        .iter()
+        .map(|(src, dst, cost)| Edge {
+            src: *src - 1,
+            dst: *dst - 1,
+            cost: *cost,
+        })
+        .collect::<Vec<_>>();
+    (n_vertex, n_edge, edges)
+}
+
+// flatten→minのイメージ
+fn min_opt(a: Option<i64>, b: Option<i64>) -> Option<i64> {
+    match (a, b) {
+        (None, None) => None,
+        (None, Some(_)) => b,
+        (Some(_), None) => a,
+        (Some(ae), Some(be)) => Some(std::cmp::min(ae, be)),
+    }
+}
+fn add_opt(a: Option<i64>, b: Option<i64>) -> Option<i64> {
+    match (a, b) {
+        (None, None) => None,
+        (None, Some(_)) => None,
+        (Some(_), None) => None,
+        (Some(ae), Some(be)) => Some(ae + be),
+    }
+}
+
+#[allow(dead_code)]
+fn output_dp(n_vertex: usize, dp: &Array3<Option<i64>>) {
+    for k in 0..=n_vertex {
+        for src in 0..n_vertex {
+            for dst in 0..n_vertex {
+                print!("{:?}", dp[[k, src, dst]]);
+            }
+            println!();
+        }
+        println!();
+    }
+}
+fn solve(n_vertex: usize, _n_edge: usize, edges: &[Edge]) -> i64 {
+    let mut dp: Array3<Option<i64>> =
+        Array::from_shape_fn((n_vertex + 1, n_vertex, n_vertex), |_| None);
+
+    for i in 0..n_vertex {
+        dp[[0, i, i]] = Some(0);
+    }
+
+    for &edge in edges {
+        dp[[0, edge.src, edge.dst]] = Some(edge.cost);
+    }
+
+    // forの順番間違えてた src/dst/kってしてた
+    for k in 1..=n_vertex {
+        for src in 0..n_vertex {
+            for dst in 0..n_vertex {
+                dp[[k, src, dst]] = min_opt(
+                    add_opt(dp[[k - 1, src, k - 1]], dp[[k - 1, k - 1, dst]]),
+                    dp[[k - 1, src, dst]],
+                )
+            }
+        }
+    }
+
+    //output_dp(n_vertex, &dp);
+
+    dp.slice(s![1.., .., ..])
+        .iter()
+        .map(|&opt| opt.unwrap_or(0))
+        .sum::<i64>()
+}
+
+//fn output() {}
+
+fn main() {
+    let (n_vertex, n_edge, edges) = read();
+    let ans = solve(n_vertex, n_edge, &edges);
+    //output();
+    println!("{}", ans);
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_name() {
+        assert_eq!(std::cmp::min(Some(3), Some(2)), Some(2));
+        assert_eq!(std::cmp::min(None, Some(2)), None);
+    }
+}
