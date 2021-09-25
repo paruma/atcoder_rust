a5504541c4e10e890d67697faceff593a4faa09f
paruma <paruma184az@gmail.com>
2021-09-25 14:53:29 +0900

    ABCのA問題用テンプレートの作成

---
+++ src/contest/template_a.rs
@@ -0,0 +1,14 @@
+#![allow(clippy::let_unit_value)]
+use proconio::input;
+
+//------snippet------
+
+//-------------------
+
+fn main() {
+    input! {
+        //
+    }
+
+    //println!("{}", ans);
+}
