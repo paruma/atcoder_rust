55010ff809810b618852695ec1b885922a477413
paruma <paruma184az@gmail.com>
2023-08-23 00:29:41 +0900

    ABC 2xx B埋め(240~249)

---
+++ src/contest/2023/abc2xx/b/243/243.rs
@@ -0,0 +1,191 @@
+use std::{collections::HashSet, io::stdin};
+
+struct Problem {
+    n: usize,
+    xs: Vec<i64>,
+    ys: Vec<i64>,
+}
+
+impl Problem {
+    fn read<R: IProconReader>(mut r: R) -> Problem {
+        let n = r.read_usize_1();
+        let xs = r.read_vec_i64();
+        let ys = r.read_vec_i64();
+        Problem { n, xs, ys }
+    }
+    fn solve(&self) -> Answer {
+        // O(n^2)解法
+        let Problem { n, xs, ys } = self;
+        let ans1 = (0..*n).filter(|&i| xs[i] == ys[i]).count();
+        let ans2 = iproduct!(0..*n, 0..*n).filter(|(i, j)| *i != *j && xs[*i] == ys[*j]).count();
+        Answer { ans1, ans2 }
+    }
+
+    fn solve2(&self) -> Answer {
+        // O(n) くらいの解法
+        let Problem { n, xs, ys } = self;
+
+        let xs_set = xs.iter().collect::<HashSet<_>>();
+        let ys_set = ys.iter().collect::<HashSet<_>>();
+        let ans1 = (0..*n).filter(|&i| xs[i] == ys[i]).count();
+        let ans2 = xs_set.intersection(&ys_set).count() - ans1; // 両方含まれている数 - 位置が一致している
+        Answer { ans1, ans2 }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Answer {
+    ans1: usize,
+    ans2: usize,
+}
+
+impl Answer {
+    fn print(&self) {
+        println!("{}", self.ans1);
+        println!("{}", self.ans2);
+    }
+}
+
+fn main() {
+    Problem::read(ProconReader::new(stdin().lock())).solve2().print();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[allow(dead_code)]
+    fn check(input: &str, expected: Answer) {
+        let actual = Problem::read(ProconReader::new(input.as_bytes())).solve();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_problem() {
+        let _input = "
+3
+4
+        "
+        .trim();
+        // check(_input, Answer { ans: 7 });
+    }
+}
+
+// ====== snippet ======
+
+use itertools::iproduct;
+#[allow(unused_imports)]
+use myio::*;
+pub mod myio {
+    use std::io::BufRead;
+
+    pub trait IProconReader {
+        fn read_line(&mut self) -> String;
+
+        fn read_bytes(&mut self) -> Vec<u8> {
+            self.read_line().as_bytes().to_vec()
+        }
+
+        fn read_any_1<T>(&mut self) -> T
+        where
+            T: std::str::FromStr,
+            T::Err: std::fmt::Debug,
+        {
+            let buf = self.read_line();
+            buf.parse::<T>().unwrap()
+        }
+
+        fn read_any_2<T0, T1>(&mut self) -> (T0, T1)
+        where
+            T0: std::str::FromStr,
+            T0::Err: std::fmt::Debug,
+            T1: std::str::FromStr,
+            T1::Err: std::fmt::Debug,
+        {
+            let buf = self.read_line();
+            let splitted = buf.trim().split(' ').collect::<Vec<_>>();
+            let a0 = splitted[0].parse::<T0>().unwrap();
+            let a1 = splitted[1].parse::<T1>().unwrap();
+            (a0, a1)
+        }
+
+        fn read_any_3<T0, T1, T2>(&mut self) -> (T0, T1, T2)
+        where
+            T0: std::str::FromStr,
+            T0::Err: std::fmt::Debug,
+            T1: std::str::FromStr,
+            T1::Err: std::fmt::Debug,
+            T2: std::str::FromStr,
+            T2::Err: std::fmt::Debug,
+        {
+            let buf = self.read_line();
+            let splitted = buf.trim().split(' ').collect::<Vec<_>>();
+            let a0 = splitted[0].parse::<T0>().unwrap();
+            let a1 = splitted[1].parse::<T1>().unwrap();
+            let a2 = splitted[2].parse::<T2>().unwrap();
+            (a0, a1, a2)
+        }
+        fn read_vec_any<T>(&mut self) -> Vec<T>
+        where
+            T: std::str::FromStr,
+            T::Err: std::fmt::Debug,
+        {
+            let buf = self.read_line();
+            buf.trim().split(' ').map(|s| s.parse::<T>().unwrap()).collect::<Vec<T>>()
+        }
+
+        fn read_vec_i64(&mut self) -> Vec<i64> {
+            self.read_vec_any::<i64>()
+        }
+
+        fn read_vec_usize(&mut self) -> Vec<usize> {
+            self.read_vec_any::<usize>()
+        }
+
+        fn read_vec_str(&mut self) -> Vec<String> {
+            self.read_vec_any::<String>()
+        }
+
+        fn read_i64_1(&mut self) -> i64 {
+            self.read_any_1::<i64>()
+        }
+
+        fn read_i64_2(&mut self) -> (i64, i64) {
+            self.read_any_2::<i64, i64>()
+        }
+
+        fn read_i64_3(&mut self) -> (i64, i64, i64) {
+            self.read_any_3::<i64, i64, i64>()
+        }
+
+        fn read_usize_1(&mut self) -> usize {
+            self.read_any_1::<usize>()
+        }
+
+        fn read_usize_2(&mut self) -> (usize, usize) {
+            self.read_any_2::<usize, usize>()
+        }
+
+        fn read_usize_3(&mut self) -> (usize, usize, usize) {
+            self.read_any_3::<usize, usize, usize>()
+        }
+    }
+
+    pub struct ProconReader<R: BufRead> {
+        buf_read: R,
+    }
+
+    impl<R: BufRead> ProconReader<R> {
+        pub fn new(buf_read: R) -> ProconReader<R> {
+            ProconReader { buf_read }
+        }
+    }
+
+    impl<R: BufRead> IProconReader for ProconReader<R> {
+        fn read_line(&mut self) -> String {
+            let mut buffer = String::new();
+            self.buf_read.read_line(&mut buffer).unwrap();
+            buffer.trim().to_string()
+        }
+    }
+}
---
+++ src/contest/2023/abc2xx/b/244/244.rs
@@ -0,0 +1,236 @@
+use std::io::stdin;
+
+struct Problem {
+    t: Vec<u8>,
+}
+
+impl Problem {
+    fn read<R: IProconReader>(mut r: R) -> Problem {
+        r.read_usize_1();
+        let t = r.read_bytes();
+        Problem { t }
+    }
+    fn solve(&self) -> Answer {
+        let t = &self.t;
+        let diff = [Pos::new(1, 0), Pos::new(0, -1), Pos::new(-1, 0), Pos::new(0, 1)];
+        struct State {
+            pos: Pos<i64>,
+            dir: usize, // 0: 東, 1: 南, 2: 西, 3: 北
+        }
+        let state =
+            t.iter().fold(State { pos: Pos::new(0, 0), dir: 0 }, |acc: State, action: &u8| {
+                match *action {
+                    b'S' => {
+                        // 真っ直ぐ進む
+                        State { pos: acc.pos + diff[acc.dir], dir: acc.dir }
+                    }
+                    b'R' => {
+                        // 右に曲がる
+                        State { pos: acc.pos, dir: (acc.dir + 1) % 4 }
+                    }
+                    _ => panic!(),
+                }
+            });
+        let ans = state.pos;
+        Answer { ans }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Answer {
+    ans: Pos<i64>,
+}
+
+impl Answer {
+    fn print(&self) {
+        println!("{} {}", self.ans.x, self.ans.y);
+    }
+}
+
+fn main() {
+    Problem::read(ProconReader::new(stdin().lock())).solve().print();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[allow(dead_code)]
+    fn check(input: &str, expected: Answer) {
+        let actual = Problem::read(ProconReader::new(input.as_bytes())).solve();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_problem() {
+        let _input = "
+3
+4
+        "
+        .trim();
+        // check(_input, Answer { ans: 7 });
+    }
+}
+
+// ====== snippet ======
+
+use pos::*;
+pub mod pos {
+    use std::ops::{Add, Mul, Sub};
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    pub struct Pos<T> {
+        pub x: T,
+        pub y: T,
+    }
+    impl<T> Pos<T> {
+        pub fn new(x: T, y: T) -> Pos<T> {
+            Pos { x, y }
+        }
+    }
+    impl<T: Mul<Output = T> + Copy> Pos<T> {
+        pub fn scala_mul(self, rhs: T) -> Pos<T> {
+            Pos::new(self.x * rhs, self.y * rhs)
+        }
+    }
+    impl<T: Add<Output = T> + Mul<Output = T> + Copy> Pos<T> {
+        pub fn norm_square(self) -> T {
+            self.x * self.x + self.y * self.y
+        }
+    }
+    impl<T: Add<Output = T> + Copy> Add for Pos<T> {
+        type Output = Pos<T>;
+        fn add(self, rhs: Self) -> Self::Output {
+            Pos::new(self.x + rhs.x, self.y + rhs.y)
+        }
+    }
+    impl<T: Sub<Output = T> + Copy> Sub for Pos<T> {
+        type Output = Pos<T>;
+        fn sub(self, rhs: Self) -> Self::Output {
+            Pos::new(self.x - rhs.x, self.y - rhs.y)
+        }
+    }
+    impl<T: num_traits::Zero + Copy> num_traits::Zero for Pos<T> {
+        fn zero() -> Self {
+            Pos::new(T::zero(), T::zero())
+        }
+        fn is_zero(&self) -> bool {
+            self.x.is_zero() && self.y.is_zero()
+        }
+    }
+}
+
+#[allow(unused_imports)]
+use myio::*;
+pub mod myio {
+    use std::io::BufRead;
+
+    pub trait IProconReader {
+        fn read_line(&mut self) -> String;
+
+        fn read_bytes(&mut self) -> Vec<u8> {
+            self.read_line().as_bytes().to_vec()
+        }
+
+        fn read_any_1<T>(&mut self) -> T
+        where
+            T: std::str::FromStr,
+            T::Err: std::fmt::Debug,
+        {
+            let buf = self.read_line();
+            buf.parse::<T>().unwrap()
+        }
+
+        fn read_any_2<T0, T1>(&mut self) -> (T0, T1)
+        where
+            T0: std::str::FromStr,
+            T0::Err: std::fmt::Debug,
+            T1: std::str::FromStr,
+            T1::Err: std::fmt::Debug,
+        {
+            let buf = self.read_line();
+            let splitted = buf.trim().split(' ').collect::<Vec<_>>();
+            let a0 = splitted[0].parse::<T0>().unwrap();
+            let a1 = splitted[1].parse::<T1>().unwrap();
+            (a0, a1)
+        }
+
+        fn read_any_3<T0, T1, T2>(&mut self) -> (T0, T1, T2)
+        where
+            T0: std::str::FromStr,
+            T0::Err: std::fmt::Debug,
+            T1: std::str::FromStr,
+            T1::Err: std::fmt::Debug,
+            T2: std::str::FromStr,
+            T2::Err: std::fmt::Debug,
+        {
+            let buf = self.read_line();
+            let splitted = buf.trim().split(' ').collect::<Vec<_>>();
+            let a0 = splitted[0].parse::<T0>().unwrap();
+            let a1 = splitted[1].parse::<T1>().unwrap();
+            let a2 = splitted[2].parse::<T2>().unwrap();
+            (a0, a1, a2)
+        }
+        fn read_vec_any<T>(&mut self) -> Vec<T>
+        where
+            T: std::str::FromStr,
+            T::Err: std::fmt::Debug,
+        {
+            let buf = self.read_line();
+            buf.trim().split(' ').map(|s| s.parse::<T>().unwrap()).collect::<Vec<T>>()
+        }
+
+        fn read_vec_i64(&mut self) -> Vec<i64> {
+            self.read_vec_any::<i64>()
+        }
+
+        fn read_vec_usize(&mut self) -> Vec<usize> {
+            self.read_vec_any::<usize>()
+        }
+
+        fn read_vec_str(&mut self) -> Vec<String> {
+            self.read_vec_any::<String>()
+        }
+
+        fn read_i64_1(&mut self) -> i64 {
+            self.read_any_1::<i64>()
+        }
+
+        fn read_i64_2(&mut self) -> (i64, i64) {
+            self.read_any_2::<i64, i64>()
+        }
+
+        fn read_i64_3(&mut self) -> (i64, i64, i64) {
+            self.read_any_3::<i64, i64, i64>()
+        }
+
+        fn read_usize_1(&mut self) -> usize {
+            self.read_any_1::<usize>()
+        }
+
+        fn read_usize_2(&mut self) -> (usize, usize) {
+            self.read_any_2::<usize, usize>()
+        }
+
+        fn read_usize_3(&mut self) -> (usize, usize, usize) {
+            self.read_any_3::<usize, usize, usize>()
+        }
+    }
+
+    pub struct ProconReader<R: BufRead> {
+        buf_read: R,
+    }
+
+    impl<R: BufRead> ProconReader<R> {
+        pub fn new(buf_read: R) -> ProconReader<R> {
+            ProconReader { buf_read }
+        }
+    }
+
+    impl<R: BufRead> IProconReader for ProconReader<R> {
+        fn read_line(&mut self) -> String {
+            let mut buffer = String::new();
+            self.buf_read.read_line(&mut buffer).unwrap();
+            buffer.trim().to_string()
+        }
+    }
+}
