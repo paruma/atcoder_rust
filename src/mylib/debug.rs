5d4d0e41eb75fd2c718e3c28b581ba652a8151bc
paruma <paruma184az@gmail.com>
2023-09-09 20:43:05 +0900

    2次元配列のprintをするスニペットを追加

--- src/mylib/debug.rs
+++ src/mylib/debug.rs
@@ -33,11 +33,36 @@ pub mod print_arr {
         }
     }
 }
+
+#[snippet(prefix = "use print_vec::*;")]
+pub mod print_vec {
+
+    pub fn print_vec<T: std::fmt::Debug>(arr: &[T]) {
+        for a in arr {
+            print!("{:?} ", a);
+        }
+        println!();
+    }
+
+    #[allow(clippy::needless_range_loop)]
+    pub fn print_vec2<T: std::fmt::Debug>(arr: &Vec<Vec<T>>) {
+        let height = arr.len();
+        let width = arr[0].len();
+        for y in 0..height {
+            for x in 0..width {
+                print!("{:?} ", arr[y][x]);
+            }
+            println!();
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use ndarray::{Array, Array2, Array3};
 
     use super::print_arr::*;
+    use super::print_vec::*;
 
     #[test]
     fn test_print_arr() {
@@ -58,5 +83,18 @@ mod tests {
         let arr: Array3<i64> = Array::from_shape_fn((2, 3, 4), |_| 3);
 
         print_arr3(&arr);
+    }
+
+    #[test]
+    fn test_print_vec() {
+        let arr = vec![1, 2, 3];
+        print_vec(&arr);
+    }
+
+    #[test]
+    fn test_print_vec2() {
+        let arr = vec![vec![1, 2, 3], vec![4, 5, 6]];
+
+        print_vec2(&arr);
     }
 }
