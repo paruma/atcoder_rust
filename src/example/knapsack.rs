8edd2df00b818182e5d669c859a8f5fb1628d668
paruma <paruma184az@gmail.com>
2023-09-09 18:26:46 +0900

    ナップサックのサンプルコードの修正

--- src/example/knapsack.rs
+++ src/example/knapsack.rs
@@ -1,22 +1,21 @@
 use std::cmp::max;
 
 use ndarray::{Array, Array2};
-use neg_tropical::NegTrop::{self, *};
 //---------snippet---------
-#[allow(dead_code)]
-pub mod neg_tropical {
+use mod_neg_ext_int::NegExtInt::{self, *};
+pub mod mod_neg_ext_int {
     use std::{cmp::Ordering, ops::Add};
-    use NegTrop::*;
+    use NegExtInt::*;
     #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-    pub enum NegTrop {
+    pub enum NegExtInt {
         NegInf,
         Fin(i64),
     }
-    impl NegTrop {
+    impl NegExtInt {
         pub fn get_fin(self) -> i64 {
             match self {
                 Fin(val) => val,
-                NegInf => panic!("called `Trop::get_fin()` on a `Fin` value"),
+                NegInf => panic!("called `NegExtInt::get_fin()` on a `Fin` value"),
             }
         }
         pub fn get_fin_or(self, default: i64) -> i64 {
@@ -37,9 +36,15 @@ pub mod neg_tropical {
                 Fin(a) => Some(a),
             }
         }
+        pub fn from_option(opt: Option<i64>) -> NegExtInt {
+            match opt {
+                Some(a) => Fin(a),
+                None => NegInf,
+            }
+        }
     }
-    impl Add for NegTrop {
-        type Output = NegTrop;
+    impl Add for NegExtInt {
+        type Output = NegExtInt;
         fn add(self, rhs: Self) -> Self::Output {
             match (self, rhs) {
                 (NegInf, NegInf) => NegInf,
@@ -49,7 +54,7 @@ pub mod neg_tropical {
             }
         }
     }
-    impl PartialOrd for NegTrop {
+    impl PartialOrd for NegExtInt {
         fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
             match (self, other) {
                 (NegInf, NegInf) => Some(Ordering::Equal),
@@ -59,7 +64,7 @@ pub mod neg_tropical {
             }
         }
     }
-    impl Ord for NegTrop {
+    impl Ord for NegExtInt {
         fn cmp(&self, other: &Self) -> Ordering {
             self.partial_cmp(other).unwrap()
         }
@@ -106,113 +111,53 @@ struct Item {
 }
 
 #[allow(dead_code)]
-fn solve(n: usize, items: &[Item], max_weight: i64) -> i64 {
-    let max_weight = max_weight as usize;
+fn solve4(n: usize, items: &[Item], max_weight: i64) -> i64 {
+    // dp[i][w] := [0,i) の items を使用したときの重さ w 以下での価値の最大値
+    // dp[0][0] = 0
+    // dp[0][w] = 0 (w!=0)
+    // dp[i+1][w] = max(dp[i][w], dp[i][w-items[i].weight] + items[i].value);
+    // 答えは必ず存在する。max_weight>=0なら、重さmax_weight以下となるような選び方は必ず存在するので（何も選ばないという選び方）
 
-    // dp[[i, w]] := [0,i)のitemsを使用したときの重さw以下での価値の最大値
-    // dp[[0, 0]] = 0
-    // dp[[0, w]] = -Inf (w!=0) ←ここ間違えてる
-    // dp[[i+1, w]] = max(dp[[i, w]], dp[[i, w-items[i].weight]] + items[i].value;
-    // 答えは必ず存在する、max_weight>=0なら、重さmax_weight以下となるような選び方は必ず存在するので（何も選ばないという選び方）
+    struct Dp {
+        dp: Vec<Vec<NegExtInt>>,
+    }
+    impl Dp {
+        fn new(n: usize, max_weight: i64) -> Dp {
+            let max_weight = max_weight as usize;
+            Dp { dp: vec![vec![NegInf; max_weight + 1]; n + 1] }
+        }
 
-    let mut dp: Array2<NegTrop> = Array::from_shape_fn((n + 1, max_weight + 1), |_| NegInf);
-
-    dp[[0, 0]] = Fin(0);
-
-    for (i, item) in items.iter().enumerate() {
-        for w in 0..=max_weight {
-            //let prev_w = max(w as i64 - item.weight, 0) as usize;
-
-            // 配列外での値には注意
-            let prev_val = if (w as i64) < item.weight {
-                NegInf
+        fn at(&self, i: usize, w: i64) -> &NegExtInt {
+            // 添字の小さい方だけ考慮すればよい（大きい方はスルー）
+            if w < 0 {
+                &NegInf
             } else {
-                let prev_w = (w as i64 - item.weight) as usize;
-                dp[[i, prev_w]]
-            };
-            dp[[i + 1, w]] = max(dp[[i, w]], prev_val + Fin(item.value));
+                &self.dp[i][w as usize]
+            }
+        }
+
+        fn at_mut(&mut self, i: usize, w: i64) -> &mut NegExtInt {
+            &mut self.dp[i][w as usize]
         }
     }
 
-    dp[[n, max_weight]].get_fin()
-}
+    let mut dp = Dp::new(n, max_weight);
 
-/*
-#[allow(dead_code)]
-fn solve2(n: usize, items: &[Item], max_weight: i64) -> i64 {
-    let max_weight = max_weight as usize;
-
-    // dp[[i, w]] := [0,i)のitemsを使用したときの重さw以下での価値の最大値
-    // dp[[0, 0]] = 0
-    // dp[[0, w]] = -Inf (w!=0)
-    // dp[[i+1, w]] = max(dp[[i, w]], dp[[i, w-items[i].weight]] + items[i].value;
-    // 答えは必ず存在する、max_weight>=0なら、重さmax_weight以下となるような選び方は必ず存在するので（何も選ばないという選び方）
-
-    let mut dp: Array2<NegTrop> = Array::from_shape_fn((n + 1, max_weight + 1), |_| NegInf);
-
-    let dpfn = |i: usize, w: i64| {
-        if w < 0 {
-            NegInf
-        } else {
-            dp[[i, w as usize]]
-        }
-    };
-    dp[[0, 0]] = Fin(0);
-
-    for (i, item) in items.iter().enumerate() {
-        for w in 0..=max_weight {
-            let w_i64 = w as i64;
-            dp[[i + 1, w]] = max(
-                dpfn(i, w_i64),
-                dpfn(i, w_i64 - item.weight) + Fin(item.value),
-            );
-        }
-    }
-    dp[[n, max_weight]].get_fin()
-}
-*/
-
-#[allow(dead_code)]
-fn solve3(n: usize, items: &[Item], max_weight: i64) -> i64 {
-    let max_weight = max_weight as usize;
-
-    // dp[[i, w]] := [0,i)のitemsを使用したときの重さw以下での価値の最大値
-    // dp[[0, 0]] = 0
-    // dp[[0, w]] = 0 (w!=0) (ここ-Infと勘違いしてた。何も考えずに経験だけから決めないようにする。ちゃんと考える。)
-    // dp[[i+1, w]] = max(dp[[i, w]], dp[[i, w-items[i].weight]] + items[i].value;
-    // 答えは必ず存在する、max_weight>=0なら、重さmax_weight以下となるような選び方は必ず存在するので（何も選ばないという選び方）
-
-    let mut dp: Array2<NegTrop> = Array::from_shape_fn((n + 1, max_weight + 1), |_| NegInf);
-
-    // dpを関数化するときは小さい方の添字の考慮をする。（もらうDPでは）大きい方の添字の考慮はいらない
-    // dpを引数に入れないと借用ルールに引っかかる
-    let dpfn = |dp: &Array2<NegTrop>, i: usize, w: i64| {
-        // 必要ないが、例えば[0, -2)=[0, 0)なので、iが負の場合は0にしてよい(今回はusizeで取ってるから関係ないけど)
-        // iに関する2項間漸化式だとこういう考慮が必要になるかもしれない。
-        if w < 0 {
-            NegInf
-        } else {
-            dp[[i, w as usize]]
-        }
-    };
-
-    //
     for w in 0..=max_weight {
-        dp[[0, w]] = Fin(0);
+        *dp.at_mut(0, w) = Fin(0);
     }
 
     for (i, item) in items.iter().enumerate() {
         for w in 0..=max_weight {
-            let w_i64 = w as i64;
-            dp[[i + 1, w]] = max(
-                dpfn(&dp, i, w_i64),
-                dpfn(&dp, i, w_i64 - item.weight) + Fin(item.value),
-            );
+            // 現在のアイテムを選択する/しない
+            let choose = *dp.at(i, w - item.weight) + Fin(item.value);
+            let no_choose = *dp.at(i, w);
+            *dp.at_mut(i + 1, w) = max(choose, no_choose);
         }
     }
-    //print_arr2(&dp);
-    dp[[n, max_weight]].get_fin()
+    dp.at(n, max_weight).get_fin()
 }
+
 #[cfg(test)]
 mod tests {
     use itertools::Itertools;
@@ -225,18 +170,12 @@ mod tests {
         let items = [(2, 3), (1, 2), (3, 6), (2, 1), (1, 3), (5, 85)];
         let items = items
             .iter()
-            .map(|(weight, value)| Item {
-                weight: *weight,
-                value: *value,
-            })
+            .map(|(weight, value)| Item { weight: *weight, value: *value })
             .collect_vec();
 
         let max_weight = 9;
 
-        let ans = solve(n, &items, max_weight);
-        assert_eq!(ans, 94);
-
-        let ans = solve3(n, &items, max_weight);
+        let ans = solve4(n, &items, max_weight);
         assert_eq!(ans, 94);
     }
 }
