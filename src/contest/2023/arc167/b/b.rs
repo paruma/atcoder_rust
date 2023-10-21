35ba006efedf631f6c5919d92b0cf32106732994
paruma <paruma184az@gmail.com>
2023-10-21 19:55:05 +0900

    ARC167 B ふりかえり

--- src/contest/2023/arc167/b/b.rs
+++ src/contest/2023/arc167/b/b.rs
@@ -4,6 +4,39 @@ struct Problem {
     b: i64,
 }
 
+// TODO: ここらへんはスニペット化したい。
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
+pub enum Mod2 {}
+
+impl Modulus for Mod2 {
+    const VALUE: u32 = 2;
+    const HINT_VALUE_IS_PRIME: bool = true;
+
+    fn butterfly_cache() -> &'static LocalKey<RefCell<Option<ButterflyCache<Self>>>> {
+        thread_local! {
+            static BUTTERFLY_CACHE: RefCell<Option<ButterflyCache<Mod2>>> = RefCell::default();
+        }
+        &BUTTERFLY_CACHE
+    }
+}
+pub type ModInt2 = StaticModInt<Mod2>;
+
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
+pub enum Mod2times998 {}
+
+impl Modulus for Mod2times998 {
+    const VALUE: u32 = 2 * 998244353;
+    const HINT_VALUE_IS_PRIME: bool = false;
+
+    fn butterfly_cache() -> &'static LocalKey<RefCell<Option<ButterflyCache<Self>>>> {
+        thread_local! {
+            static BUTTERFLY_CACHE: RefCell<Option<ButterflyCache<Mod2times998>>> = RefCell::default();
+        }
+        &BUTTERFLY_CACHE
+    }
+}
+pub type ModInt2times998 = StaticModInt<Mod2times998>;
+
 impl Problem {
     fn read() -> Problem {
         input! {
@@ -12,27 +45,64 @@ impl Problem {
         p
     }
     fn solve(&self) -> Answer {
-        let fa = prime_factorize(self.a);
-        let x: RF =
-            fa.iter().map(|(_prime, cnt)| RF::new(*cnt) * RF::new(self.b) + RF::new(1)).product2();
-        let x_mod2: i64 =
-            fa.iter().map(|(_prime, cnt)| ((cnt % 2) * (self.b % 2) + 1) % 2).product2();
-        //let ans = x * RF::new(self.b) / RF::new(2);
-        let ans0 = x * RF::new(self.b);
-        let ans0_mod2 = (x_mod2 % 2) * (self.b % 2);
-        let ans = if ans0_mod2 == 0 { ans0 / RF::new(2) } else { (ans0 - RF::new(1)) / RF::new(2) };
-        let ans = ans.rep();
+        // mod p と mod 2 を両方計算 (p=998244353)
+        use ac_library::ModInt998244353 as Mint998;
+        type Mint2 = ModInt2;
+
+        let a = self.a;
+        let b = self.b;
+
+        // (1) A^B の約数の個数を求める
+        // (1-1) A を素因数分解する
+        let fa = prime_factorize(a);
+        // (1-2) Aの素因数分解から A^B の約数の個数を求める
+        let n_divisors: Mint998 =
+            fa.values().map(|&cnt| Mint998::new(cnt) * Mint998::new(b) + Mint998::new(1)).product();
+        let n_divisors_mod2: Mint2 =
+            fa.values().map(|&cnt| Mint2::new(cnt) * Mint2::new(b) + Mint2::new(1)).product();
+
+        // (2) A^Bの約数の総積はAで何回割れるか求める
+        // A^Bの約数の総積は(A^B)^(n_divisors/2) =A^((B * n_divisors)/2)
+        // つまり、floor((B * n_divisors)/2) 回割れる
+        // ↓こう書くとWA になる
+        // let ans = x * RF::new(self.b) / RF::new(2);
+        // (2-1) B * n_divisors を計算
+        let ans0 = n_divisors * Mint998::new(b);
+        let ans0_mod2 = n_divisors_mod2 * Mint2::new(b);
+        // (2-2) floor((B * n_divisors)/2) を計算
+        let ans = if ans0_mod2.val() == 0 {
+            ans0 / Mint998::new(2)
+        } else {
+            (ans0 - Mint998::new(1)) / Mint998::new(2)
+        };
+        let ans = ans.val() as i64;
         Answer { ans }
     }
 
     fn solve2(&self) -> Answer {
-        let fa = prime_factorize(self.a);
-        let x: i64 = fa.iter().map(|(_prime, cnt)| cnt * self.b + 1).product2();
-        let x_mod2: i64 =
-            fa.iter().map(|(_prime, cnt)| ((cnt % 2) * (self.b % 2) + 1) % 2).product2();
-        let ans0_mod2 = (x_mod2 % 2) * (self.b % 2);
-        let ans0 = x * self.b;
-        let ans = if ans0_mod2 == 0 { ans0 / 2 } else { (ans0 - 1) / 2 };
+        // mod 2*p で計算
+        type Mint = ModInt2times998;
+
+        let a = self.a;
+        let b = self.b;
+
+        // (1) A^B の約数の個数を求める
+        // (1-1) A を素因数分解する
+        let fa = prime_factorize(a);
+        // (1-2) Aの素因数分解から A^B の約数の個数を求める
+        let n_divisors: Mint =
+            fa.values().map(|&cnt| Mint::new(cnt) * Mint::new(b) + Mint::new(1)).product();
+
+        // (2) A^Bの約数の総積はAで何回割れるか求める
+        // A^Bの約数の総積は(A^B)^(n_divisors/2) =A^((B * n_divisors)/2)
+        // つまり、floor((B * n_divisors)/2) 回割れる
+        // ↓こう書くとWA になる
+        // let ans = x * RF::new(self.b) / RF::new(2);
+        // (2-1) B * n_divisors を計算
+        let ans0 = n_divisors * Mint::new(b);
+        // (2-2) floor((B * n_divisors)/2) を計算
+        // ℤ/2pℤ → ℤ/pℤ; [x] ↦ [floor(x/2)] は well-defined なので、単にこう計算すれば良い。
+        let ans = (ans0.val() / 2) as i64;
         Answer { ans }
     }
 }
@@ -49,7 +119,7 @@ impl Answer {
 }
 
 fn main() {
-    Problem::read().solve().print();
+    Problem::read().solve2().print();
 }
 
 #[cfg(test)]
@@ -60,14 +130,13 @@ mod tests {
     #[test]
     fn test_problem() {
         assert_eq!(Problem { a: 4, b: 1 }.solve().ans, 1);
-        assert_eq!(Problem { a: 4, b: 1 }.solve2().ans, 1);
-        assert_eq!(Problem { a: 2, b: 3 }.solve2().ans, 6);
         assert_eq!(1 + 1, 2);
     }
 }
 
-use std::collections::HashMap;
+use std::{cell::RefCell, collections::HashMap, thread::LocalKey};
 
+use ac_library::{ButterflyCache, Modulus, StaticModInt};
 // ====== import ======
 #[allow(unused_imports)]
 use itertools::Itertools;
@@ -142,88 +211,4 @@ pub fn prime_factorize(n: i64) -> HashMap<i64, i64> {
         cnt_table.insert(n, 1);
     }
     cnt_table
-}
-
-use num::{One, Zero};
-use rf::*;
-pub mod rf {
-    pub const MOD: i64 = 998_244_353;
-    #[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
-    pub struct RF {
-        rep: i64,
-    }
-    impl RF {
-        pub fn new(x: i64) -> RF {
-            RF { rep: x.rem_euclid(MOD) }
-        }
-        pub fn rep(self) -> i64 {
-            self.rep
-        }
-    }
-    impl RF {
-        pub fn inv(self) -> Self {
-            num::pow(self, (MOD - 2) as usize)
-        }
-    }
-    impl num_traits::Zero for RF {
-        fn zero() -> Self {
-            RF::new(0)
-        }
-        fn is_zero(&self) -> bool {
-            self.rep == 0
-        }
-    }
-    impl num_traits::One for RF {
-        fn one() -> Self {
-            RF::new(1)
-        }
-    }
-    macro_rules ! bi_ops_impl {($ std_ops : ident , $ fn : ident , $ op : tt ) => {impl std :: ops ::$ std_ops for RF {type Output = Self ; fn $ fn (self , rhs : Self ) -> Self :: Output {RF :: new (self . rep $ op rhs . rep ) } } } ; }
-    bi_ops_impl ! (Add , add , + );
-    bi_ops_impl ! (Sub , sub , - );
-    bi_ops_impl ! (Mul , mul , * );
-    impl std::ops::Div for RF {
-        type Output = Self;
-        fn div(self, rhs: Self) -> Self::Output {
-            std::ops::Mul::mul(self, rhs.inv())
-        }
-    }
-    macro_rules ! bi_ops_assign_impl {($ std_ops_assign : ident , $ fn_assign : ident , $ op : tt ) => {impl std :: ops ::$ std_ops_assign for RF {fn $ fn_assign (& mut self , rhs : Self ) {* self = * self $ op rhs } } } ; }
-    bi_ops_assign_impl ! (AddAssign , add_assign , + );
-    bi_ops_assign_impl ! (SubAssign , sub_assign , - );
-    bi_ops_assign_impl ! (MulAssign , mul_assign , * );
-    bi_ops_assign_impl ! (DivAssign , div_assign , / );
-    impl std::ops::Neg for RF {
-        type Output = Self;
-        fn neg(self) -> Self::Output {
-            RF::new(-self.rep)
-        }
-    }
-}
-
-use iter_product::*;
-mod iter_product {
-    pub trait Product2<A>: Sized {
-        fn product2<I: Iterator<Item = A>>(iter: I) -> Self;
-    }
-    impl<'a, T: num::One + std::ops::Mul<T, Output = T> + Copy> Product2<&'a T> for T {
-        fn product2<I: Iterator<Item = &'a T>>(iter: I) -> Self {
-            iter.fold(Self::one(), |acc, x| acc * (*x))
-        }
-    }
-    impl<T: num::One + std::ops::Mul<T, Output = T> + Copy> Product2<T> for T {
-        fn product2<I: Iterator<Item = T>>(iter: I) -> Self {
-            iter.fold(Self::one(), |acc, x| acc * x)
-        }
-    }
-    pub trait IteratorExtProduct2: Iterator + Sized {
-        fn product2<S>(self) -> S
-        where
-            Self: Sized,
-            S: Product2<Self::Item>,
-        {
-            Product2::product2(self)
-        }
-    }
-    impl<T: Iterator> IteratorExtProduct2 for T {}
 }
