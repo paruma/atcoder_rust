cacd73cc0ca8a61487d4aacac29234d0785af6d4
paruma <paruma184az@gmail.com>
2023-11-26 23:26:26 +0900

    ICPC アジア地区予選 A問題

---
+++ src/contest/2023/_misc/icpc_1126_a.rs
@@ -0,0 +1,222 @@
+//#[derive_readable]
+struct Problem {
+    height: usize,
+    width: usize,
+    grid: Vec<Vec<u8>>,
+}
+
+struct Grid {
+    grid: Vec<Vec<u8>>,
+    height: usize,
+    width: usize,
+}
+
+impl Grid {
+    fn new(grid: Vec<Vec<u8>>) -> Grid {
+        let h = grid.len();
+        let w = grid[0].len();
+        Grid { grid, height: h, width: w }
+    }
+
+    fn is_within(&self, pos: Pos<i64>) -> bool {
+        let h = self.height as i64;
+        let w = self.width as i64;
+        0 <= pos.y && pos.y < h && 0 <= pos.x && pos.x < w
+    }
+
+    fn at(&self, pos: Pos<i64>) -> &u8 {
+        if self.is_within(pos) {
+            &self.grid[pos.y as usize][pos.x as usize]
+        } else {
+            &b'.'
+        }
+    }
+}
+
+impl Problem {
+    fn read() -> Problem {
+        input! {
+            height: usize,
+            width: usize,
+            grid: [Bytes; height],
+        }
+        Problem { height, width, grid }
+    }
+    fn solve(&self) -> Answer {
+        let Problem { height, width, grid } = self;
+        let grid = Grid { grid: grid.clone(), height: *height, width: *width };
+        let ans = iproduct!(0..*height, 0..*width)
+            .map(|(y, x)| {
+                std::iter::repeat(pos::DIR4_LIST)
+                    .take(7)
+                    .multi_cartesian_product()
+                    .filter(|dirs| {
+                        //
+                        let mut next = Pos::new(x as i64, y as i64);
+                        let mut str = vec![];
+                        str.push(*grid.at(next));
+                        for dir in dirs {
+                            next += *dir;
+                            str.push(*grid.at(next))
+                        }
+                        str == b"YOKOHAMA"
+                    })
+                    .count() as i64
+            })
+            .sum::<i64>();
+        Answer { ans }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Answer {
+    ans: i64,
+}
+
+impl Answer {
+    fn print(&self) {
+        println!("{}", self.ans);
+    }
+}
+
+fn main() {
+    Problem::read().solve().print();
+}
+
+#[cfg(test)]
+mod tests {
+    #[allow(unused_imports)]
+    use super::*;
+
+    #[test]
+    fn test_problem() {
+        assert_eq!(1 + 1, 2);
+    }
+}
+
+// ====== import ======
+use itertools::iproduct;
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    derive_readable, fastout, input,
+    marker::{Bytes, Usize1},
+};
+
+// ====== output func ======
+#[allow(unused_imports)]
+use print_vec::*;
+pub mod print_vec {
+    use itertools::Itertools;
+    use proconio::fastout;
+    #[fastout]
+    pub fn print_vec<T: std::fmt::Debug>(arr: &[T]) {
+        for a in arr {
+            println!("{:?}", a);
+        }
+    }
+    #[fastout]
+    pub fn print_vec_1line<T: std::fmt::Debug>(arr: &[T]) {
+        let msg = arr.iter().map(|x| format!("{:?}", x)).join(" ");
+        println!("{}", msg);
+    }
+    #[fastout]
+    pub fn print_vec2<T: std::fmt::Debug>(arr: &Vec<Vec<T>>) {
+        for row in arr {
+            let msg = row.iter().map(|x| format!("{:?}", x)).join(" ");
+            println!("{}", msg);
+        }
+    }
+    pub fn print_bytes(bytes: &[u8]) {
+        let msg = String::from_utf8(bytes.to_vec()).unwrap();
+        println!("{}", msg);
+    }
+    #[fastout]
+    pub fn print_vec_bytes(vec_bytes: &[Vec<u8>]) {
+        for row in vec_bytes {
+            let msg = String::from_utf8(row.to_vec()).unwrap();
+            println!("{}", msg);
+        }
+    }
+}
+
+#[allow(unused)]
+fn print_yesno(ans: bool) {
+    let msg = if ans { "Yes" } else { "No" };
+    println!("{}", msg);
+}
+
+// ====== snippet ======
+use pos::*;
+pub mod pos {
+    use std::ops::{Add, AddAssign, Mul, Neg, Sub, SubAssign};
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    pub struct Pos<T> {
+        pub x: T,
+        pub y: T,
+    }
+    impl<T> Pos<T> {
+        pub fn new(x: T, y: T) -> Pos<T> {
+            Pos { x, y }
+        }
+    }
+    impl<T: Mul<Output = T> + Copy> Pos<T> {
+        pub fn scala_mul(self, rhs: T) -> Pos<T> {
+            Pos::new(self.x * rhs, self.y * rhs)
+        }
+    }
+    impl<T: Add<Output = T> + Mul<Output = T> + Copy> Pos<T> {
+        pub fn norm_square(self) -> T {
+            self.x * self.x + self.y * self.y
+        }
+    }
+    impl<T: Add<Output = T> + Copy> Add for Pos<T> {
+        type Output = Pos<T>;
+        fn add(self, rhs: Self) -> Self::Output {
+            Pos::new(self.x + rhs.x, self.y + rhs.y)
+        }
+    }
+    impl<T: Sub<Output = T> + Copy> Sub for Pos<T> {
+        type Output = Pos<T>;
+        fn sub(self, rhs: Self) -> Self::Output {
+            Pos::new(self.x - rhs.x, self.y - rhs.y)
+        }
+    }
+    impl<T: Neg<Output = T>> Neg for Pos<T> {
+        type Output = Self;
+        fn neg(self) -> Self::Output {
+            Pos::new(-self.x, -self.y)
+        }
+    }
+    impl<T: num_traits::Zero + Copy> num_traits::Zero for Pos<T> {
+        fn zero() -> Self {
+            Pos::new(T::zero(), T::zero())
+        }
+        fn is_zero(&self) -> bool {
+            self.x.is_zero() && self.y.is_zero()
+        }
+    }
+    impl<T: Add<Output = T> + Copy> AddAssign for Pos<T> {
+        fn add_assign(&mut self, rhs: Self) {
+            *self = *self + rhs
+        }
+    }
+    impl<T: Sub<Output = T> + Copy> SubAssign for Pos<T> {
+        fn sub_assign(&mut self, rhs: Self) {
+            *self = *self - rhs
+        }
+    }
+    pub const DIR8_LIST: [Pos<i64>; 8] = [
+        Pos { x: 0, y: 1 },
+        Pos { x: 1, y: 1 },
+        Pos { x: 1, y: 0 },
+        Pos { x: 1, y: -1 },
+        Pos { x: 0, y: -1 },
+        Pos { x: -1, y: -1 },
+        Pos { x: -1, y: 0 },
+        Pos { x: -1, y: 1 },
+    ];
+    pub const DIR4_LIST: [Pos<i64>; 4] =
+        [Pos { x: 0, y: 1 }, Pos { x: 1, y: 0 }, Pos { x: 0, y: -1 }, Pos { x: -1, y: 0 }];
+}
