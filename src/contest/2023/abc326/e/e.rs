d27ebb6d097560f587837fc0672636baf4bbc53f
paruma <paruma184az@gmail.com>
2024-03-09 20:05:49 +0900

    ABC326 E 途中

--- src/contest/2023/abc326/e/e.rs
+++ src/contest/2023/abc326/e/e.rs
@@ -17,9 +17,39 @@ impl Problem {
     fn solve(&self) -> Answer {
         let n = self.n;
         let xs = &self.xs;
-        let dp = vec![Mint::new(0); n];
-        
-        let ans = 0;
+        // dp[x] := xにいるときから追加で支給される金額の期待値
+        // 樹形図を考えると見える。
+        let mut dp = vec![Mint::new(0); n + 1];
+        let n_inv = Mint::new(n as i64).inv();
+
+        let mut sum = Mint::new(0);
+
+        for i in (0..=n).rev() {
+            dp[i] = n_inv * sum;
+            if i != 0 {
+                sum += dp[i] + xs[i - 1];
+            }
+        }
+
+        let ans = dp[0].val() as i64;
+        Answer { ans }
+    }
+
+    fn solve2(&self) -> Answer {
+        // 寄与を考える。
+        // サイコロで y を引いて xs[y] 円受け取る確率を考える
+        // TODO: まだACしていない
+        let n = self.n;
+        let xs = &self.xs;
+        let mut prob = vec![Mint::new(0); n + 1];
+        prob[0] = 0.into();
+        let n_inv = Mint::new(n as i64).inv();
+        for i in 1..=n {
+            prob[i] = (prob[i - 1] + 1) * n_inv;
+            dbg!(prob[i].to_rational_str());
+        }
+        let ans = (0..n).map(|i| prob[i + 1] * xs[i]).sum::<Mint>();
+        let ans = ans.val() as i64;
         Answer { ans }
     }
 }
@@ -36,7 +66,7 @@ impl Answer {
 }
 
 fn main() {
-    Problem::read().solve().print();
+    Problem::read().solve2().print();
 }
 
 #[cfg(test)]
@@ -103,3 +133,32 @@ fn print_yesno(ans: bool) {
 }
 
 // ====== snippet ======
+use modint_to_rational::*;
+pub mod modint_to_rational {
+    use num_rational::Rational64;
+    pub trait ToRational {
+        fn to_rational(&self) -> Option<Rational64>;
+        fn to_rational_str(&self) -> String {
+            self.to_rational()
+                .map(|x| x.to_string())
+                .unwrap_or("cannot reconstruct".to_string())
+        }
+    }
+    impl ToRational for ac_library::ModInt998244353 {
+        /// 注意: 1000 * 2000 = 2*10^6 の計算をしている
+        fn to_rational(&self) -> Option<Rational64> {
+            if self.val() == 0 {
+                return Some(Rational64::new(0, 1));
+            }
+            for denom in 1..1000 {
+                let denom_inv = Self::new(denom).inv();
+                for numer in -1000..1000 {
+                    if *self == denom_inv * Self::new(numer) {
+                        return Some(Rational64::new(numer, denom));
+                    }
+                }
+            }
+            None
+        }
+    }
+}
