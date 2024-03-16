de181140bad1c0583d3971f680ebdc2d3cecdea2
paruma <paruma184az@gmail.com>
2024-03-16 17:27:31 +0900

    #21, #36: トポロジカルソート/有向グラフ閉路検出

--- src/mylib/graph.rs
+++ src/mylib/graph.rs
@@ -75,7 +75,7 @@ pub fn is_bipartite_graph_by_uf(n_vertex: usize, edges: &[Edge]) -> bool {
 }
 
 #[snippet]
-pub fn has_cycle(n_vertex: usize, edges: &[Edge]) -> bool {
+pub fn has_cycle_undirected(n_vertex: usize, edges: &[Edge]) -> bool {
     let mut uf = UnionFind::new(n_vertex);
     for &e in edges {
         if uf.equiv(e.from, e.to) {
@@ -86,7 +86,47 @@ pub fn has_cycle(n_vertex: usize, edges: &[Edge]) -> bool {
     false
 }
 
+#[snippet]
+#[snippet(include = "topo_sort")]
+pub fn has_cycle_directed(adj: &Vec<Vec<Edge>>) -> bool {
+    let topo_sorted = topo_sort(adj); // 戻り値にループの部分は入ってこない。
+    topo_sorted.len() != adj.len()
+}
+
+#[snippet]
+pub fn topo_sort(adj: &Vec<Vec<Edge>>) -> Vec<usize> {
+    let n_vertex = adj.len();
+    let mut in_deg = vec![0; n_vertex];
+    for v in 0..n_vertex {
+        for &e in &adj[v] {
+            in_deg[e.to] += 1;
+        }
+    }
+
+    let mut open: Queue<usize> = Queue::new();
+    for v in 0..n_vertex {
+        if in_deg[v] == 0 {
+            open.push(v);
+        }
+    }
+
+    let mut ans = vec![];
+
+    while let Some(current) = open.pop() {
+        ans.push(current);
+        for &e in &adj[current] {
+            in_deg[e.to] -= 1;
+            if in_deg[e.to] == 0 {
+                open.push(e.to);
+            }
+        }
+    }
+    ans
+}
+
 mod tests {
+    use itertools::Itertools;
+
     use super::*;
 
     fn edge1() -> (usize, Vec<Edge>) {
@@ -105,7 +145,15 @@ mod tests {
         // 0 → 1
         // ↓   ↓
         // 2 → 3
-        (4, vec![Edge::new(0, 1), Edge::new(0, 2), Edge::new(1, 3), Edge::new(2, 3)])
+        (
+            4,
+            vec![
+                Edge::new(0, 1),
+                Edge::new(0, 2),
+                Edge::new(1, 3),
+                Edge::new(2, 3),
+            ],
+        )
     }
 
     #[test]
@@ -113,8 +161,11 @@ mod tests {
         {
             let (n_vertex, edges) = edge1();
             let adj = make_adj(n_vertex, &edges);
-            let expected =
-                vec![vec![Edge::new(0, 1), Edge::new(0, 2)], vec![Edge::new(1, 2)], vec![]];
+            let expected = vec![
+                vec![Edge::new(0, 1), Edge::new(0, 2)],
+                vec![Edge::new(1, 2)],
+                vec![],
+            ];
             assert_eq!(adj, expected);
         }
         {
@@ -167,20 +218,77 @@ mod tests {
             assert!(is_bipartite_graph_by_uf(n_vertex, &edges));
         }
     }
-
     #[test]
-    fn test_has_cycle() {
+    fn test_has_cycle_undirected() {
         {
             let (n_vertex, edges) = edge1();
-            assert!(has_cycle(n_vertex, &edges));
+
+            assert!(has_cycle_undirected(n_vertex, &edges));
         }
         {
             let (n_vertex, edges) = edge2();
-            assert!(!has_cycle(n_vertex, &edges));
+            assert!(!has_cycle_undirected(n_vertex, &edges));
         }
         {
             let (n_vertex, edges) = edge3();
-            assert!(has_cycle(n_vertex, &edges));
+            assert!(has_cycle_undirected(n_vertex, &edges));
+        }
+    }
+    #[test]
+    fn test_has_cycle_directed() {
+        {
+            let (n_vertex, edges) = edge1();
+            let adj = make_adj(n_vertex, &edges);
+            assert!(!has_cycle_directed(&adj));
+        }
+        {
+            let (n_vertex, edges) = edge2();
+            let adj = make_adj(n_vertex, &edges);
+            assert!(!has_cycle_directed(&adj));
+        }
+        {
+            let (n_vertex, edges) = edge3();
+            let adj = make_adj(n_vertex, &edges);
+            assert!(!has_cycle_directed(&adj));
+        }
+        {
+            // 0 → 1 → 2
+            //     ↑   ↓
+            //     4 → 3
+            let n_vertex = 5;
+            let edges = vec![
+                Edge::new(0, 1),
+                Edge::new(1, 2),
+                Edge::new(2, 3),
+                Edge::new(3, 4),
+                Edge::new(4, 1),
+            ];
+            let adj = make_adj(n_vertex, &edges);
+            assert!(has_cycle_directed(&adj));
+        }
+    }
+
+    #[test]
+    fn test_topo_sort() {
+        //
+        // 0 → 1
+        // ↓ ↗
+        // 2
+        // 3 → 4
+        let n_vertex = 5;
+        let edges = [(0, 1), (0, 2), (2, 1), (3, 4)]
+            .into_iter()
+            .map(|(from, to)| Edge::new(from, to))
+            .collect_vec();
+        let adj = make_adj(n_vertex, &edges);
+
+        let sorted = topo_sort(&adj);
+
+        // ソートされているか確認
+        for &e in &edges {
+            let from_pos = sorted.iter().position(|&x| x == e.from).unwrap();
+            let to_pos = sorted.iter().position(|&x| x == e.to).unwrap();
+            assert!(from_pos <= to_pos);
         }
     }
 }
