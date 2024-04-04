ca3798cea19cb20687bce0320b06b5b01df6ef52
paruma <paruma184az@gmail.com>
2024-04-05 05:35:45 +0900

    ABC347 E リファクタリング

--- src/contest/2024/abc347/e/e.rs
+++ src/contest/2024/abc347/e/e.rs
@@ -23,6 +23,7 @@ impl Problem {
         // 各 x in 0..n がどの区間で合われたり消えたりするかを調べる。
         let mut appear_list: Vec<Vec<usize>> = vec![vec![]; self.n];
 
+        // set を HashSet で持って、s_size_list は set.len() で計算すればよかった
         for (i, x) in xs.iter().enumerate() {
             if set[*x] {
                 s_size_list[i] = s_size_list[i - 1] - 1;
@@ -51,7 +52,7 @@ impl Problem {
                         .map(|i| {
                             let begin = appear_info[i * 2];
                             let end = appear_info[i * 2 + 1];
-                            s_size_list_cumsum.get_interval_sum(begin, end)
+                            s_size_list_cumsum.range_sum(begin, end)
                         })
                         .sum::<i64>()
                 } else {
@@ -59,11 +60,70 @@ impl Problem {
                         .map(|i| {
                             let begin = appear_info[i * 2];
                             let end = appear_info[i * 2 + 1];
-                            s_size_list_cumsum.get_interval_sum(begin, end)
+                            s_size_list_cumsum.range_sum(begin, end)
                         })
                         .sum::<i64>();
                     let x2 = s_size_list_cumsum
-                        .get_interval_sum(*appear_info.last().unwrap(), s_size_list.len());
+                        .range_sum(*appear_info.last().unwrap(), s_size_list.len());
+                    x1 + x2
+                }
+            })
+            .collect_vec();
+
+        Answer { ans }
+    }
+
+    fn solve2(&self) -> Answer {
+        // solve のリファクタリング(set を Vec<bool> ではなく、HashSet で持つ)
+        let nq = self.nq;
+        let xs = &self.xs;
+
+        // これに fold をするのはやりすぎかもしれない（単純にブロック内に mut を閉じ込めるだけでも良かったかも）
+        let (s_size_list, _) = xs.iter().enumerate().fold(
+            (vec![0; self.nq], HashSet::<usize>::new()),
+            |(mut acc_s_size_list, mut acc_set), (i, x)| {
+                if acc_set.contains(x) {
+                    acc_set.remove(x);
+                } else {
+                    acc_set.insert(*x);
+                }
+                acc_s_size_list[i] = acc_set.len() as i64;
+                (acc_s_size_list, acc_set)
+            },
+        );
+
+        // 各 x in 0..n がどの区間で合われたり消えたりするかを調べる。
+        let appear_list = xs.iter().copied().enumerate().fold(
+            vec![vec![]; self.n],
+            |mut acc_appear_list, (i, x)| {
+                acc_appear_list[x].push(i);
+                acc_appear_list
+            },
+        );
+
+        //s_size_list の累積和をとる
+        let s_size_list_cumsum = CumSum::new(&s_size_list);
+
+        let ans = appear_list
+            .iter()
+            .map(|appear_info| {
+                if appear_info.len() % 2 == 0 {
+                    (0..appear_info.len() / 2)
+                        .map(|i| {
+                            let begin = appear_info[i * 2];
+                            let end = appear_info[i * 2 + 1];
+                            s_size_list_cumsum.range_sum(begin, end)
+                        })
+                        .sum::<i64>()
+                } else {
+                    let x1 = (0..appear_info.len() / 2)
+                        .map(|i| {
+                            let begin = appear_info[i * 2];
+                            let end = appear_info[i * 2 + 1];
+                            s_size_list_cumsum.range_sum(begin, end)
+                        })
+                        .sum::<i64>();
+                    let x2 = s_size_list_cumsum.suffix_sum(*appear_info.last().unwrap());
                     x1 + x2
                 }
             })
@@ -92,7 +152,7 @@ impl Answer {
 }
 
 fn main() {
-    Problem::read().solve().print();
+    Problem::read().solve2().print();
 }
 
 #[cfg(test)]
@@ -201,8 +261,14 @@ pub mod cumsum {
             CumSum { cumsum }
         }
         /// 計算量: O(1)
-        pub fn get_interval_sum(&self, begin: usize, end: usize) -> i64 {
+        pub fn range_sum(&self, begin: usize, end: usize) -> i64 {
             self.cumsum[end] - self.cumsum[begin]
+        }
+        pub fn prefix_sum(&self, end: usize) -> i64 {
+            self.cumsum[end]
+        }
+        pub fn suffix_sum(&self, begin: usize) -> i64 {
+            self.cumsum[self.cumsum.len() - 1] - self.cumsum[begin]
         }
     }
 }
