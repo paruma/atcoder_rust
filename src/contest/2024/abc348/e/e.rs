57b9913771bbd201eba884382f80238f039b8967
paruma <paruma184az@gmail.com>
2024-04-06 19:58:13 +0900

    ABC348 準備

---
+++ src/contest/2024/abc348/e/e.rs
@@ -0,0 +1,129 @@
+// #[derive_readable]
+#[derive(Debug)]
+struct Problem {
+    _a: usize,
+}
+
+impl Problem {
+    fn read() -> Problem {
+        input! {
+            _a: usize,
+        }
+        Problem { _a }
+    }
+    fn solve(&self) -> Answer {
+        let ans = 0;
+        Answer { ans }
+    }
+
+    #[allow(dead_code)]
+    fn solve_naive(&self) -> Answer {
+        todo!();
+        // let ans = 0;
+        // Answer { ans }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Answer {
+    ans: i64,
+}
+
+impl Answer {
+    fn print(&self) {
+        println!("{}", self.ans);
+    }
+}
+
+fn main() {
+    Problem::read().solve().print();
+}
+
+#[cfg(test)]
+mod tests {
+    #[allow(unused_imports)]
+    use super::*;
+    #[allow(unused_imports)]
+    use rand::{rngs::SmallRng, seq::SliceRandom, *};
+
+    #[test]
+    fn test_problem() {
+        assert_eq!(1 + 1, 2);
+    }
+
+    fn check(p: &Problem) {
+        assert_eq!(p.solve(), p.solve_naive());
+    }
+
+    fn make_random_problem() -> Problem {
+        todo!()
+        // let mut rng = SmallRng::from_entropy();
+        // let n = rng.gen_range(1..=10);
+        // let p = Problem { _a: n };
+        // println!("{:?}", &p);
+        // p
+    }
+
+    #[test]
+    fn test_with_naive() {
+        // 手動でテストを作るのもOK
+        for _ in 0..100 {
+            let p = make_random_problem();
+            check(&p);
+        }
+    }
+}
+
+// ====== import ======
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use proconio::{
+    derive_readable, fastout, input,
+    marker::{Bytes, Usize1},
+};
+
+// ====== output func ======
+#[allow(unused_imports)]
+use print_vec::*;
+pub mod print_vec {
+    use itertools::Itertools;
+    use proconio::fastout;
+    #[fastout]
+    pub fn print_vec<T: std::fmt::Debug>(arr: &[T]) {
+        for a in arr {
+            println!("{:?}", a);
+        }
+    }
+    #[fastout]
+    pub fn print_vec_1line<T: std::fmt::Debug>(arr: &[T]) {
+        let msg = arr.iter().map(|x| format!("{:?}", x)).join(" ");
+        println!("{}", msg);
+    }
+    #[fastout]
+    pub fn print_vec2<T: std::fmt::Debug>(arr: &Vec<Vec<T>>) {
+        for row in arr {
+            let msg = row.iter().map(|x| format!("{:?}", x)).join(" ");
+            println!("{}", msg);
+        }
+    }
+    pub fn print_bytes(bytes: &[u8]) {
+        let msg = String::from_utf8(bytes.to_vec()).unwrap();
+        println!("{}", msg);
+    }
+    #[fastout]
+    pub fn print_vec_bytes(vec_bytes: &[Vec<u8>]) {
+        for row in vec_bytes {
+            let msg = String::from_utf8(row.to_vec()).unwrap();
+            println!("{}", msg);
+        }
+    }
+}
+
+#[allow(unused)]
+fn print_yesno(ans: bool) {
+    let msg = if ans { "Yes" } else { "No" };
+    println!("{}", msg);
+}
+
+// ====== snippet ======
