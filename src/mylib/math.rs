6c0521f69c2b5a1597c0e5a250a4811fef269e67
paruma <paruma184az@gmail.com>
2024-04-14 20:00:23 +0900

    自作 modint をライブラリから削除

--- src/mylib/math.rs
+++ src/mylib/math.rs
@@ -1,8 +1,5 @@
 pub mod acl_modint;
 pub mod combinatorics;
 pub mod inversion;
-pub mod modint_field;
-pub mod modint_field_fake;
-pub mod modint_ring;
 pub mod number_thm;
 pub mod positional_notation;
