0d1942256c9614c1f1f47284b349724b2534fab7
paruma <paruma184az@gmail.com>
2024-04-27 01:12:48 +0900

    #79: ポテンシャル付き UnionFind の追加

--- src/contest/2023/abc328/f/f.rs
+++ src/contest/2023/abc328/f/f.rs
@@ -21,11 +21,28 @@ impl Problem {
     }
     fn solve(&self) -> Answer {
         let Problem { len, ns, qs } = self;
-        let mut uf = union_find::UnionFind::new(*len);
+        let mut uf = union_find::OldUnionFind::new(*len);
         let mut ans = vec![];
         for (i, Query { a, b, dist }) in qs.iter().enumerate() {
             if uf.unite(*a, *b, *dist) {
                 ans.push(i)
+            }
+        }
+        Answer { ans }
+    }
+
+    fn solve2(&self) -> Answer {
+        let Problem { len, ns, qs } = self;
+        let mut uf = potentialized_union_find::UnionFind::new(*len);
+        let mut ans = vec![];
+        for (i, &Query { a, b, dist }) in qs.iter().enumerate() {
+            if uf.same(b, a) {
+                if uf.diff(b, a) == Some(dist) {
+                    ans.push(i);
+                }
+            } else {
+                ans.push(i);
+                uf.unite(b, a, dist);
             }
         }
         Answer { ans }
@@ -44,7 +61,7 @@ impl Answer {
 }
 
 fn main() {
-    Problem::read().solve().print();
+    Problem::read().solve2().print();
 }
 
 #[cfg(test)]
@@ -60,7 +77,7 @@ mod tests {
     #[test]
     fn test_uf() {
         use super::union_find::*;
-        let mut uf = UnionFind::new(8);
+        let mut uf = OldUnionFind::new(8);
         uf.unite(0, 1, 0);
         uf.unite(3, 4, 0);
         uf.unite(4, 5, 0);
@@ -149,13 +166,20 @@ pub mod union_find {
     }
 
     #[derive(Clone, Debug)]
-    pub struct UnionFind {
+    pub struct OldUnionFind {
         nodes: Vec<Node>,
     }
 
-    impl UnionFind {
-        pub fn new(n: usize) -> UnionFind {
-            UnionFind { nodes: vec![Node::Root { root: Root { count: 1, value: 0 } }; n] }
+    impl OldUnionFind {
+        pub fn new(n: usize) -> OldUnionFind {
+            OldUnionFind {
+                nodes: vec![
+                    Node::Root {
+                        root: Root { count: 1, value: 0 }
+                    };
+                    n
+                ],
+            }
         }
 
         fn root_node(&mut self, index: usize) -> RootAndIndex {
@@ -190,7 +214,10 @@ pub mod union_find {
         }
 
         pub fn num_groups(&self) -> usize {
-            self.nodes.iter().filter(|&node| matches!(node, Node::Root { .. })).count()
+            self.nodes
+                .iter()
+                .filter(|&node| matches!(node, Node::Root { .. }))
+                .count()
         }
 
         // xの値 - yの値 = diff になるようにする
@@ -225,21 +252,155 @@ pub mod union_find {
                 let new_x_root_value = x_root_value + new_x_value - x_value;
                 let diff = new_x_root_value - y_root_value;
 
-                self.nodes[x_root_index] = Node::NonRoot { parent_index: y_root_index, diff };
+                self.nodes[x_root_index] = Node::NonRoot {
+                    parent_index: y_root_index,
+                    diff,
+                };
 
-                self.nodes[y_root_index] =
-                    Node::Root { root: Root { count: x_count + y_count, value: y_root_value } };
+                self.nodes[y_root_index] = Node::Root {
+                    root: Root {
+                        count: x_count + y_count,
+                        value: y_root_value,
+                    },
+                };
             } else {
                 let new_y_value = -diff + x_value;
                 let new_y_root_value = y_root_value + new_y_value - y_value;
                 let diff = new_y_root_value - x_root_value;
 
-                self.nodes[y_root_index] = Node::NonRoot { parent_index: x_root_index, diff };
+                self.nodes[y_root_index] = Node::NonRoot {
+                    parent_index: x_root_index,
+                    diff,
+                };
 
-                self.nodes[x_root_index] =
-                    Node::Root { root: Root { count: y_count + x_count, value: x_root_value } };
+                self.nodes[x_root_index] = Node::Root {
+                    root: Root {
+                        count: y_count + x_count,
+                        value: x_root_value,
+                    },
+                };
             }
             true
+        }
+    }
+}
+
+pub mod potentialized_union_find {
+    use itertools::Itertools;
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    struct RootInfo {
+        count: usize,
+    }
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    struct NonRootInfo {
+        parent_index: usize,
+        potential_diff: i64,
+    }
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    enum Node {
+        Root(RootInfo),
+        NonRoot(NonRootInfo),
+    }
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    struct ToRoot {
+        root_info: RootInfo,
+        root_index: usize,
+        potential_diff: i64,
+    }
+    #[derive(Clone, Debug)]
+    pub struct UnionFind {
+        nodes: Vec<Node>,
+        cnt_groups: usize,
+    }
+    impl UnionFind {
+        pub fn new(n: usize) -> UnionFind {
+            UnionFind {
+                nodes: vec![Node::Root(RootInfo { count: 1 }); n],
+                cnt_groups: n,
+            }
+        }
+        fn root_node(&mut self, index: usize) -> ToRoot {
+            match self.nodes[index] {
+                Node::Root(info) => ToRoot {
+                    root_info: info,
+                    root_index: index,
+                    potential_diff: 0,
+                },
+                Node::NonRoot(current_info) => {
+                    let parent_to_root = self.root_node(current_info.parent_index);
+                    let potential_diff =
+                        current_info.potential_diff + parent_to_root.potential_diff;
+                    self.nodes[index] = Node::NonRoot(NonRootInfo {
+                        parent_index: parent_to_root.root_index,
+                        potential_diff,
+                    });
+                    ToRoot {
+                        root_info: parent_to_root.root_info,
+                        root_index: parent_to_root.root_index,
+                        potential_diff,
+                    }
+                }
+            }
+        }
+        pub fn root(&mut self, index: usize) -> usize {
+            self.root_node(index).root_index
+        }
+        pub fn same_count(&mut self, index: usize) -> usize {
+            self.root_node(index).root_info.count
+        }
+        pub fn same(&mut self, x: usize, y: usize) -> bool {
+            self.root(x) == self.root(y)
+        }
+        pub fn num_groups(&self) -> usize {
+            self.cnt_groups
+        }
+        pub fn unite(&mut self, src: usize, dst: usize, diff: i64) {
+            if self.same(src, dst) {
+                return;
+            }
+            self.cnt_groups -= 1;
+            let src_root_node = self.root_node(src);
+            let dst_root_node = self.root_node(dst);
+            let root_diff = -src_root_node.potential_diff + diff + dst_root_node.potential_diff;
+            let (src_root_node, dst_root_node, root_diff) =
+                if src_root_node.root_info.count <= dst_root_node.root_info.count {
+                    (src_root_node, dst_root_node, root_diff)
+                } else {
+                    (dst_root_node, src_root_node, -root_diff)
+                };
+            self.nodes[src_root_node.root_index] = Node::NonRoot(NonRootInfo {
+                parent_index: dst_root_node.root_index,
+                potential_diff: root_diff,
+            });
+            self.nodes[dst_root_node.root_index] = Node::Root(RootInfo {
+                count: src_root_node.root_info.count + dst_root_node.root_info.count,
+            });
+        }
+        pub fn diff(&mut self, src: usize, dst: usize) -> Option<i64> {
+            if self.same(src, dst) {
+                Some(self.root_node(src).potential_diff - self.root_node(dst).potential_diff)
+            } else {
+                None
+            }
+        }
+        pub fn groups(&mut self) -> Vec<Vec<usize>> {
+            let n = self.nodes.len();
+            let roots = (0..n).map(|i| self.root(i)).collect_vec();
+            let group_size = (0..n).map(|i| roots[i]).fold(vec![0; n], |mut acc, x| {
+                acc[x] += 1;
+                acc
+            });
+            let result = {
+                let mut result = vec![Vec::new(); n];
+                for i in 0..n {
+                    result[i].reserve(group_size[i]);
+                }
+                for i in 0..n {
+                    result[roots[i]].push(i);
+                }
+                result
+            };
+            result.into_iter().filter(|x| !x.is_empty()).collect_vec()
         }
     }
 }
--- src/mylib/union_find0.rs
+++ src/mylib/union_find0.rs
@@ -87,7 +87,7 @@ pub mod simple_union_find {
             let x_root_node = self.root_node(x);
             let y_root_node = self.root_node(y);
 
-            let (smaller_root, larger_root) = if x_root_node.info.count < y_root_node.info.count {
+            let (smaller_root, larger_root) = if x_root_node.info.count <= y_root_node.info.count {
                 (x_root_node, y_root_node)
             } else {
                 (y_root_node, x_root_node)
@@ -100,6 +100,171 @@ pub mod simple_union_find {
             self.nodes[larger_root.index] = Node::Root(RootInfo {
                 count: smaller_root.info.count + larger_root.info.count,
             });
+        }
+
+        pub fn groups(&mut self) -> Vec<Vec<usize>> {
+            let n = self.nodes.len();
+
+            let roots = (0..n).map(|i| self.root(i)).collect_vec();
+
+            let group_size = (0..n).map(|i| roots[i]).fold(vec![0; n], |mut acc, x| {
+                acc[x] += 1;
+                acc
+            });
+
+            let result = {
+                let mut result = vec![Vec::new(); n];
+                for i in 0..n {
+                    result[i].reserve(group_size[i]);
+                }
+                for i in 0..n {
+                    result[roots[i]].push(i);
+                }
+                result
+            };
+            result.into_iter().filter(|x| !x.is_empty()).collect_vec()
+        }
+    }
+}
+
+#[snippet(prefix = "use potentialized_union_find::*;")]
+pub mod potentialized_union_find {
+    use itertools::Itertools;
+
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    struct RootInfo {
+        count: usize,
+    }
+
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    struct NonRootInfo {
+        parent_index: usize,
+        potential_diff: i64, // 親のポテンシャル - 自分のポテンシャル
+    }
+
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    enum Node {
+        Root(RootInfo),
+        NonRoot(NonRootInfo),
+    }
+
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    struct ToRoot {
+        root_info: RootInfo,
+        root_index: usize,
+        potential_diff: i64, // root のポテンシャル - 自分のポテンシャル
+    }
+
+    #[derive(Clone, Debug)]
+    pub struct UnionFind {
+        nodes: Vec<Node>,
+        cnt_groups: usize,
+    }
+
+    impl UnionFind {
+        pub fn new(n: usize) -> UnionFind {
+            UnionFind {
+                nodes: vec![Node::Root(RootInfo { count: 1 }); n],
+                cnt_groups: n,
+            }
+        }
+
+        fn root_node(&mut self, index: usize) -> ToRoot {
+            match self.nodes[index] {
+                Node::Root(info) => ToRoot {
+                    root_info: info,
+                    root_index: index,
+                    potential_diff: 0,
+                },
+                Node::NonRoot(current_info) => {
+                    let parent_to_root = self.root_node(current_info.parent_index);
+                    // 経路圧縮
+                    let potential_diff =
+                        current_info.potential_diff + parent_to_root.potential_diff;
+                    self.nodes[index] = Node::NonRoot(NonRootInfo {
+                        parent_index: parent_to_root.root_index,
+                        potential_diff,
+                    });
+                    ToRoot {
+                        root_info: parent_to_root.root_info,
+                        root_index: parent_to_root.root_index,
+                        potential_diff,
+                    }
+                }
+            }
+        }
+        // 経路圧縮しないバージョン
+        // fn root_node(&self, index: usize) -> RootAndIndex {
+        //     match self.nodes[index] {
+        //         Node::Root(info) => RootAndIndex { info, index },
+        //         Node::NonRoot(info) => self.root_node(info.parent_index),
+        //     }
+        // }
+
+        pub fn root(&mut self, index: usize) -> usize {
+            self.root_node(index).root_index
+        }
+
+        pub fn same_count(&mut self, index: usize) -> usize {
+            self.root_node(index).root_info.count
+        }
+
+        pub fn same(&mut self, x: usize, y: usize) -> bool {
+            self.root(x) == self.root(y)
+        }
+
+        pub fn num_groups(&self) -> usize {
+            self.cnt_groups
+        }
+
+        // diff = dst のポテンシャル - src のポテンシャル となるように統合する
+        pub fn unite(&mut self, src: usize, dst: usize, diff: i64) {
+            if self.same(src, dst) {
+                return;
+            }
+
+            self.cnt_groups -= 1;
+
+            let src_root_node = self.root_node(src);
+            let dst_root_node = self.root_node(dst);
+
+            //                  root_diff
+            //            src_root -→ dst_root
+            //                ↑           ↑
+            // src_root_node. |           | dst_root_node.
+            // potential_diff |           | potential_diff
+            //               src ------→ dst
+            //                    diff
+
+            let root_diff = -src_root_node.potential_diff + diff + dst_root_node.potential_diff;
+
+            // src_root_node が小さくなるように、必要に応じて swap する
+            let (src_root_node, dst_root_node, root_diff) =
+                if src_root_node.root_info.count <= dst_root_node.root_info.count {
+                    (src_root_node, dst_root_node, root_diff)
+                } else {
+                    (dst_root_node, src_root_node, -root_diff)
+                };
+
+            // dst に src をくっつける
+            self.nodes[src_root_node.root_index] = Node::NonRoot(NonRootInfo {
+                parent_index: dst_root_node.root_index,
+                potential_diff: root_diff,
+            });
+            self.nodes[dst_root_node.root_index] = Node::Root(RootInfo {
+                count: src_root_node.root_info.count + dst_root_node.root_info.count,
+            });
+        }
+
+        pub fn diff(&mut self, src: usize, dst: usize) -> Option<i64> {
+            //  root
+            //   ↑     ↖
+            //  src --> dst
+            if self.same(src, dst) {
+                Some(self.root_node(src).potential_diff - self.root_node(dst).potential_diff)
+            } else {
+                None
+            }
         }
 
         pub fn groups(&mut self) -> Vec<Vec<usize>> {
@@ -146,8 +311,59 @@ mod tests_simple_union_find {
         uf.unite(4, 5);
         uf.unite(4, 6);
         uf.unite(1, 4);
+        uf.unite(1, 5); // すでにつながっている
 
-        // {0,1,3,4,5,6}, {2}, {7}
+        /*
+        |           [6]
+        |            |
+        |   [0]-[1]-[4]-[5]
+        |            |
+        |           [3]
+        |   [2] [7]
+         */
+        assert_eq!(uf.num_groups(), 3);
+        assert!(uf.same(0, 4));
+        assert!(!uf.same(2, 4));
+        assert_eq!(
+            sorted(uf.groups()),
+            sorted(vec![vec![0, 1, 3, 4, 5, 6], vec![2], vec![7]])
+        );
+    }
+}
+
+#[cfg(test)]
+mod tests_potentialized_union_find {
+    use itertools::Itertools;
+
+    fn sorted(xss: Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+        xss.iter()
+            .map(|xs| xs.iter().copied().collect_vec())
+            .sorted()
+            .collect_vec()
+    }
+    #[test]
+    fn test_uf() {
+        use super::potentialized_union_find::*;
+        let mut uf = UnionFind::new(8);
+        uf.unite(0, 1, 1);
+        uf.unite(3, 4, 2);
+        uf.unite(4, 5, 3);
+        uf.unite(4, 6, 4);
+        uf.unite(1, 4, 5);
+        uf.unite(0, 5, 1000); // すでにつながっているので何も起こらない
+
+        /*
+        |                 [6]
+        |                  ↑
+        |                  |4
+        |      1       5   |   3
+        | [0] --→ [1] --→ [4] --→ [5]
+        |                  ↑
+        |                  |2
+        |                  |
+        |                 [3]
+        | [2]    [7]
+         */
 
         assert_eq!(uf.num_groups(), 3);
         assert!(uf.same(0, 4));
@@ -156,5 +372,9 @@ mod tests_simple_union_find {
             sorted(uf.groups()),
             sorted(vec![vec![0, 1, 3, 4, 5, 6], vec![2], vec![7]])
         );
+        assert_eq!(uf.diff(0, 4), Some(6));
+        assert_eq!(uf.diff(4, 0), Some(-6));
+        assert_eq!(uf.diff(6, 5), Some(-1));
+        assert_eq!(uf.diff(0, 2), None);
     }
 }
