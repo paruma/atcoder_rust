f953d7080c62226f8a4691fb73d34fa71df1a266
paruma <paruma184az@gmail.com>
2024-05-02 01:31:33 +0900

    転倒数を Fenwick Tree で計算

--- src/mylib/math/inversion.rs
+++ src/mylib/math/inversion.rs
@@ -1,3 +1,4 @@
+use ac_library::FenwickTree;
 use cargo_snippet::snippet;
 
 #[snippet]
@@ -21,9 +22,27 @@ pub fn inversion_number(xs: &[usize]) -> i64 {
     cnt
 }
 
+pub fn inversion_number_fenwick(xs: &[usize]) -> i64 {
+    if xs.is_empty() {
+        return 0;
+    }
+    let max_val = xs.iter().copied().max().unwrap();
+
+    // 各値が今までに現れた回数を記録する
+    let mut fenwick = FenwickTree::new(max_val + 1, 0_i64);
+    let mut cnt = 0;
+    for &x in xs {
+        cnt += fenwick.sum(x + 1..); // 今までに見たxより大きい値の数
+        fenwick.add(x, 1);
+    }
+
+    cnt
+}
+
 #[cfg(test)]
 mod tests {
     use super::inversion_number;
+    use super::inversion_number_fenwick;
 
     #[test]
     fn test_inversion_nubmer() {
@@ -31,5 +50,13 @@ mod tests {
         assert_eq!(inversion_number(&[1, 2, 3, 4, 5, 6]), 0);
         assert_eq!(inversion_number(&[1, 6, 3, 4, 5, 2]), 7);
         assert_eq!(inversion_number(&[1, 1, 0, 0, 0, 0]), 8);
+    }
+
+    #[test]
+    fn test_inversion_nubmer_fenwick() {
+        assert_eq!(inversion_number_fenwick(&[3, 2, 1]), 3);
+        assert_eq!(inversion_number_fenwick(&[1, 2, 3, 4, 5, 6]), 0);
+        assert_eq!(inversion_number_fenwick(&[1, 6, 3, 4, 5, 2]), 7);
+        assert_eq!(inversion_number_fenwick(&[1, 1, 0, 0, 0, 0]), 8);
     }
 }
