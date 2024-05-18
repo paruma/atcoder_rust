aa02f231196a8067ce765e77442627c113dc986b
paruma <paruma184az@gmail.com>
2024-05-18 19:52:52 +0900

    ARC169 B - Subsegments with Small Sums AC

--- src/contest/2023/arc169/b/b.rs
+++ src/contest/2023/arc169/b/b.rs
@@ -15,6 +15,42 @@ impl Problem {
         Problem { n, s, xs }
     }
     fn solve(&self) -> Answer {
+        // 各 begin in 0..n に対して、begin から始まる括弧の寄与を考える
+        let xs_cumsum = CumSum::new(&self.xs);
+        let n = self.n;
+        let s = self.s;
+
+        // 各 begin に対して xs[begin..end]の和が s 以下となる最大の end を求める
+        let end_list = (0..n)
+            .map(|begin| {
+                //
+                let end = bin_search(begin as i64, (n + 1) as i64, |x| {
+                    xs_cumsum.range_sum(begin..x as usize) <= s
+                });
+                end as usize
+            })
+            .collect_vec();
+
+        // (end_list[begin], begin) という辺を作って有向グラフを考える。各頂点に対応する部分木のサイズを求める
+        let subtree_size = {
+            let mut subtree_size = vec![1_i64; n + 1];
+            for begin in 0..n {
+                subtree_size[end_list[begin]] += subtree_size[begin]
+            }
+
+            subtree_size
+        };
+
+        let ans = (0..n)
+            .map(|begin| {
+                //begin から始まる括弧の数
+                subtree_size[begin] * (n - begin) as i64
+            })
+            .sum::<i64>();
+        Answer { ans }
+    }
+    fn solve_wrong(&self) -> Answer {
+        // コンテスト中の実装(WA)
         let Problem { n, s, xs } = self;
         let n = self.n;
         let mut sum = 0;
@@ -120,3 +156,89 @@ fn print_yesno(ans: bool) {
 }
 
 // ====== snippet ======
+use cumsum::*;
+pub mod cumsum {
+    use std::ops::{Bound, Range, RangeBounds};
+    pub struct CumSum {
+        pub cumsum: Vec<i64>,
+    }
+    impl CumSum {
+        /// 計算量: O(|xs|)
+        pub fn new(xs: &[i64]) -> CumSum {
+            let mut cumsum = vec![0; xs.len() + 1];
+            for i in 1..xs.len() + 1 {
+                cumsum[i] = cumsum[i - 1] + xs[i - 1];
+            }
+            CumSum { cumsum }
+        }
+        fn open(&self, range: impl RangeBounds<usize>) -> Range<usize> {
+            use Bound::Excluded;
+            use Bound::Included;
+            use Bound::Unbounded;
+            let begin = match range.start_bound() {
+                Unbounded => 0,
+                Included(&x) => x,
+                Excluded(&x) => x + 1,
+            };
+            let end = match range.end_bound() {
+                Excluded(&x) => x,
+                Included(&x) => x + 1,
+                Unbounded => self.cumsum.len() - 1,
+            };
+            begin..end
+        }
+        /// 計算量: O(1)
+        pub fn range_sum(&self, range: impl RangeBounds<usize>) -> i64 {
+            let range = self.open(range);
+            self.cumsum[range.end] - self.cumsum[range.start]
+        }
+        pub fn prefix_sum(&self, end: usize) -> i64 {
+            self.cumsum[end]
+        }
+        pub fn suffix_sum(&self, begin: usize) -> i64 {
+            self.cumsum[self.cumsum.len() - 1] - self.cumsum[begin]
+        }
+    }
+}
+
+/// 二分探索をする
+/// ```text
+/// ng ng ng ok ok ok
+///          ↑ここの引数の値を返す
+/// ```
+/// 計算量: O(log(|ok - ng|))
+/// ## Arguments
+/// * ok != ng
+/// * |ok - ng| <= 2^63 - 1, |ok + ng| <= 2^63 - 1
+/// * p の定義域について
+///     * ng < ok の場合、p は区間 ng..ok で定義されている。
+///     * ok < ng の場合、p は区間 ok..ng で定義されている。
+/// * p の単調性について
+///     * ng < ok の場合、p は単調増加
+///     * ok < ng の場合、p は単調減少
+/// ## Return
+/// * ng < ok の場合: I = { i in ng..ok | p(i) == true } としたとき
+///     * I が空でなければ、min I を返す。
+///     * I が空ならば、ok を返す。
+/// * ok < ng の場合: I = { i in ok..ng | p(i) == true } としたとき
+///     * I が空でなければ、max I を返す。
+///     * I が空ならば、ok を返す。
+pub fn bin_search<F>(mut ok: i64, mut ng: i64, p: F) -> i64
+where
+    F: Fn(i64) -> bool,
+{
+    assert!(ok != ng);
+    assert!(ok.checked_sub(ng).is_some());
+    assert!(ok.checked_add(ng).is_some());
+    while num::abs(ok - ng) > 1 {
+        let mid = (ok + ng) / 2;
+        assert!(mid != ok);
+        assert!(mid != ng);
+        if p(mid) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    ok
+}
