f12d032b1ed0275e7e132e98cdb2c68e47b23531
paruma <paruma184az@gmail.com>
2024-05-31 01:07:02 +0900

    ABC167 F - Bracket Sequencing

---
+++ src/contest/2024/_misc/abc167_f.rs
@@ -0,0 +1,540 @@
+//#[derive_readable]
+#[derive(Debug, Clone)]
+struct Problem {
+    n: usize,
+    xss: Vec<Vec<u8>>,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+struct ParensInfo {
+    min: i64,
+    last: i64,
+}
+
+fn is_ok(xss: &[Vec<i64>]) -> bool {
+    // y座標最小値の大きい順番に並べれば良い（貪欲）
+    // もしy座標最小値が大きい場合でうまくいかないならば、小さい場合もうまくいかない。
+    let paren_info_list = xss
+        .iter()
+        .map(|xs| {
+            // 累積和の最小値と和を計算する
+            // 折れ線で考えた場合は、「y座標最小値」と「最後のy座標」に対応する
+            let mut current = 0;
+            let mut min = 0;
+            for &x in xs {
+                current += x;
+                min = i64::min(min, current);
+            }
+            ParensInfo { min, last: current }
+        })
+        .sorted_by_key(|info| Reverse(info.min))
+        .collect_vec();
+
+    let mut current = 0;
+    for &info in &paren_info_list {
+        if current + info.min >= 0 {
+            current += info.last;
+        } else {
+            return false;
+        }
+    }
+    true
+}
+
+impl Problem {
+    fn read() -> Problem {
+        input! {
+            n: usize,
+            xss: [Bytes; n],
+        }
+        Problem { n, xss }
+    }
+    fn solve(&self) -> Answer {
+        // [方針]
+        // 括弧からなる文字列を折れ線で考えたとき、「どのくらい登るか」と「途中どのくらい下がるか」が大事になる。
+        // 上り・下り・上り だったら 上り・上り・下り にしても損をしない。つまり、左側は全部上り、右側は全部下りを考える。
+        //
+        // まずは、左側の全部上りのケースについて考える
+        // 各文字列に対して、折れ線のy座標の最小値を考える（始点を0とする）
+        // 折れ線のy座標の最小値から考えていって損をしない（貪欲法）
+        // あとは、y座標の最小値から考えていって、折れ線をつなげていったときにy座標が0未満にならないかどうかを確かめれば良い。
+        //
+        // 下りは逆から考えて括弧を反転させてあげれば上りと同じ議論になる。
+
+        // '(' を +1 に、')' を -1 にする
+        let xss = &self
+            .xss
+            .iter()
+            .cloned()
+            .map(|xs| {
+                xs.iter()
+                    .copied()
+                    .map(|x| if x == b'(' { 1 } else { -1 })
+                    .collect_vec()
+            })
+            .collect_vec();
+        let flattened = xss.iter().flatten().copied().collect_vec();
+
+        if flattened.iter().sum::<i64>() != 0 {
+            return Answer { ans: false };
+        }
+
+        // 上り
+        let xss_up = xss
+            .iter()
+            .filter(|xs| xs.iter().sum::<i64>() >= 0)
+            .cloned()
+            .collect_vec();
+
+        // 下り (逆順に考えて、上りと同じ議論をする)
+        let xss_down = xss
+            .iter()
+            .filter(|xs| xs.iter().sum::<i64>() < 0)
+            .map(|xs| xs.iter().copied().rev().map(|x| -x).collect_vec())
+            .collect_vec();
+
+        let ans = is_ok(&xss_up) && is_ok(&xss_down);
+        Answer { ans }
+    }
+
+    #[allow(dead_code)]
+    fn solve_naive(&self) -> Answer {
+        todo!();
+        // let ans = 0;
+        // Answer { ans }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Answer {
+    ans: bool,
+}
+
+impl Answer {
+    fn print(&self) {
+        print_yesno(self.ans);
+    }
+}
+
+fn main() {
+    Problem::read().solve().print();
+}
+
+#[cfg(test)]
+mod tests {
+    #[allow(unused_imports)]
+    use super::*;
+    #[allow(unused_imports)]
+    use rand::{rngs::SmallRng, seq::SliceRandom, *};
+
+    #[test]
+    fn test_problem() {
+        assert_eq!(1 + 1, 2);
+    }
+
+    #[allow(dead_code)]
+    #[derive(Debug)]
+    struct WrongTestCase {
+        problem: Problem,
+        main_ans: Answer,
+        naive_ans: Answer,
+    }
+
+    #[allow(dead_code)]
+    fn check(p: &Problem) -> Option<WrongTestCase> {
+        let main_ans = p.solve();
+        let naive_ans = p.solve_naive();
+        if main_ans != naive_ans {
+            Some(WrongTestCase {
+                problem: p.clone(),
+                main_ans,
+                naive_ans,
+            })
+        } else {
+            None
+        }
+    }
+
+    #[allow(dead_code)]
+    fn make_random_problem(rng: &mut SmallRng) -> Problem {
+        todo!()
+        // let n = rng.gen_range(1..=10);
+        // let p = Problem { _a: n };
+        // println!("{:?}", &p);
+        // p
+    }
+
+    #[allow(unreachable_code)]
+    #[test]
+    fn test_with_naive() {
+        let num_tests = 0;
+        let max_wrong_case = 10; // この件数間違いが見つかったら打ち切り
+        let mut rng = SmallRng::seed_from_u64(42);
+        // let mut rng = SmallRng::from_entropy();
+        let mut wrong_cases: Vec<WrongTestCase> = vec![];
+        for _ in 0..num_tests {
+            let p = make_random_problem(&mut rng);
+            let result = check(&p);
+            if let Some(wrong_test_case) = result {
+                wrong_cases.push(wrong_test_case);
+            }
+            if wrong_cases.len() >= max_wrong_case {
+                break;
+            }
+        }
+
+        if !wrong_cases.is_empty() {
+            for t in &wrong_cases {
+                println!("{:?}", t.problem);
+                println!("main ans : {:?}", t.main_ans);
+                println!("naive ans: {:?}", t.naive_ans);
+                println!();
+            }
+            println!("{} cases are wrong.", wrong_cases.len());
+            panic!();
+        }
+    }
+}
+
+// ====== import ======
+#[allow(unused_imports)]
+use itertools::{chain, iproduct, izip, Itertools};
+#[allow(unused_imports)]
+use proconio::{
+    derive_readable, fastout, input,
+    marker::{Bytes, Usize1},
+};
+use std::cmp::Reverse;
+#[allow(unused_imports)]
+use std::collections::{BinaryHeap, HashMap, HashSet};
+
+// ====== output func ======
+#[allow(unused_imports)]
+use print_vec::*;
+pub mod print_vec {
+
+    use itertools::Itertools;
+    use proconio::fastout;
+    #[fastout]
+    pub fn print_vec<T: std::fmt::Debug>(arr: &[T]) {
+        for a in arr {
+            println!("{:?}", a);
+        }
+    }
+    #[fastout]
+    pub fn print_vec_1line<T: std::fmt::Debug>(arr: &[T]) {
+        let msg = arr.iter().map(|x| format!("{:?}", x)).join(" ");
+        println!("{}", msg);
+    }
+    #[fastout]
+    pub fn print_vec2<T: std::fmt::Debug>(arr: &Vec<Vec<T>>) {
+        for row in arr {
+            let msg = row.iter().map(|x| format!("{:?}", x)).join(" ");
+            println!("{}", msg);
+        }
+    }
+    pub fn print_bytes(bytes: &[u8]) {
+        let msg = String::from_utf8(bytes.to_vec()).unwrap();
+        println!("{}", msg);
+    }
+    #[fastout]
+    pub fn print_vec_bytes(vec_bytes: &[Vec<u8>]) {
+        for row in vec_bytes {
+            let msg = String::from_utf8(row.to_vec()).unwrap();
+            println!("{}", msg);
+        }
+    }
+}
+
+#[allow(unused)]
+fn print_yesno(ans: bool) {
+    let msg = if ans { "Yes" } else { "No" };
+    println!("{}", msg);
+}
+
+// ====== snippet ======
+
+use lg::*;
+pub mod lg {
+    use std::borrow::Borrow;
+    use std::fmt;
+    use std::iter::once;
+    /// Print the values with the line number.
+    /// # Examples
+    /// ```rust
+    /// # use lg::*;
+    /// let x = 42;
+    /// let y = 43;
+    /// lg!(x);
+    /// lg!(x, y);
+    /// lg!(42, x, 43, y);
+    /// ```
+    #[macro_export]
+    macro_rules ! lg {(@ contents $ head : expr $ (, $ tail : expr ) * ) => {{$ crate :: __lg_internal ! ($ head ) ; $ (eprint ! ("," ) ; $ crate :: __lg_internal ! ($ tail ) ; ) * eprintln ! () ; } } ; ($ ($ expr : expr ) ,* $ (, ) ? ) => {{eprint ! ("{}\u{276f}" , line ! () ) ; $ crate :: lg ! (@ contents $ ($ expr ) ,* ) } } ; }
+    #[doc(hidden)]
+    #[macro_export]
+    macro_rules! __lg_internal {
+        ($ value : expr ) => {{
+            match $value {
+                head => {
+                    eprint!(
+                        " {} = {}",
+                        stringify!($value),
+                        $crate::__quiet(format!("{:?}", &head))
+                    );
+                }
+            }
+        }};
+    }
+    /// Print many 1D arrays side-by-side with the line number.
+    /// # Examples
+    /// ```rust
+    /// # use lg::*;
+    /// let a = [1, 2, 3];
+    /// let b = [4, 5, 6];
+    /// let c = [7, 8, 9];
+    /// rows! {
+    ///   "id", // the name of the index
+    ///   @"a" => a,
+    ///   b,
+    ///   @"c" => c,
+    /// }
+    /// ```
+    #[macro_export]
+    macro_rules ! rows {{$ index_label : literal , $ (@ offset $ offset : expr , ) ? $ (@ verticalbar $ verticalbar : expr , ) * $ ($ (@$ label : literal => ) ? $ values : expr ) ,* $ (, ) ? } => {{#! [allow (unused_assignments ) ] let mut rows = $ crate :: Rows :: default () ; rows . line_number (line ! () ) ; $ (rows . offset ($ offset ) ; ) ? $ (rows . verticalbar ($ verticalbar ) ; ) * rows . index_label ($ index_label ) ; $ ({let mut label = stringify ! ($ values ) . to_string () ; if label . starts_with ("&" ) {label = label [1 .. ] . to_string () ; } $ ({let label_ : &'static str = $ label ; label = label_ . to_string () ; } ) ? rows . row (label , $ values ) ; } ) * eprintln ! ("{}" , rows . to_string_table () ) ; } } ; }
+    /// Print the 2D array with the line number.
+    /// # Examples
+    /// ```rust
+    /// # use lg::*;
+    /// let a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    /// table! {
+    ///    @"a" => a,
+    /// }
+    /// table! {
+    ///   a,
+    /// }
+    /// ```
+    #[macro_export]
+    macro_rules ! table {{$ (@$ name : literal => ) ? $ values : expr $ (, ) ? } => {{#! [allow (unused_assignments ) ] let mut name = stringify ! ($ values ) . to_string () ; if name . starts_with ("&" ) {name = name [1 .. ] . to_string () ; } $ ({let name_ : &'static str = $ name ; name = name_ . to_string () ; } ) ? let mut rows = $ crate :: Rows :: default () ; rows . line_number (line ! () ) ; rows . table_name (name ) ; # [allow (array_into_iter ) ] for (i , row ) in $ values . into_iter () . enumerate () {rows . row (i . to_string () , row ) ; } eprintln ! ("{}" , rows . to_string_table () ) ; } } ; }
+    #[doc(hidden)]
+    pub fn __quiet(s: impl AsRef<str>) -> String {
+        s.as_ref()
+            .replace("340282366920938463463374607431768211455", "*")
+            .replace("170141183460469231731687303715884105727", "*")
+            .replace("18446744073709551615", "*")
+            .replace("9223372036854775807", "*")
+            .replace("-9223372036854775808", "*")
+            .replace("4294967295", "*")
+            .replace("2147483647", "*")
+            .replace("-2147483648", "*")
+            .replace("None", "*")
+            .replace("Some", "")
+            .replace("true", "#")
+            .replace("false", ".")
+            .replace(['"', '\''], "")
+    }
+    #[doc(hidden)]
+    #[derive(Default)]
+    pub struct Rows {
+        line_number: String,
+        index_label: String,
+        offset: usize,
+        verticalbars: Vec<usize>,
+        table_name: String,
+        rows: Vec<Row>,
+    }
+    impl Rows {
+        pub fn line_number(&mut self, line_number: u32) -> &mut Self {
+            self.line_number = format!("{}", line_number);
+            self
+        }
+        pub fn index_label(&mut self, index_label: impl Into<String>) -> &mut Self {
+            self.index_label = index_label.into();
+            self
+        }
+        pub fn offset(&mut self, offset: usize) -> &mut Self {
+            self.offset = offset;
+            self
+        }
+        pub fn verticalbar(&mut self, verticalbar: impl IntoIterator<Item = usize>) -> &mut Self {
+            self.verticalbars.extend(verticalbar);
+            self
+        }
+        pub fn table_name(&mut self, table_name: impl Into<String>) -> &mut Self {
+            self.table_name = table_name.into();
+            self
+        }
+        pub fn row(
+            &mut self,
+            label: impl Into<String>,
+            values: impl IntoIterator<Item = impl fmt::Debug>,
+        ) -> &mut Self {
+            self.rows.push(Row {
+                label: label.into(),
+                values: values
+                    .into_iter()
+                    .map(|value| __quiet(format!("{:?}", value)))
+                    .collect(),
+            });
+            self
+        }
+        pub fn to_string_table(self) -> StringTable {
+            let Self {
+                line_number,
+                index_label,
+                offset,
+                verticalbars,
+                table_name,
+                rows,
+            } = self;
+            let w = rows
+                .iter()
+                .map(|row| row.values.len())
+                .max()
+                .unwrap_or_default();
+            let mut verticalbar_count = vec![0; w + 1];
+            for &v in &verticalbars {
+                if (offset..=offset + w).contains(&v) {
+                    verticalbar_count[v - offset] += 1;
+                }
+            }
+            StringTable {
+                head: StringRow {
+                    label: format!(
+                        "{line_number}❯ {table_name}{index_label}",
+                        index_label = if index_label.is_empty() {
+                            String::new()
+                        } else {
+                            format!("[{}]", index_label)
+                        }
+                    ),
+                    values: (offset..offset + w)
+                        .map(|index| index.to_string())
+                        .collect(),
+                },
+                body: rows
+                    .iter()
+                    .map(|row| StringRow {
+                        label: row.label.clone(),
+                        values: row.values.clone(),
+                    })
+                    .collect(),
+                verticalbar_count,
+            }
+        }
+    }
+    struct Row {
+        label: String,
+        values: Vec<String>,
+    }
+    #[doc(hidden)]
+    pub struct StringTable {
+        head: StringRow,
+        body: Vec<StringRow>,
+        verticalbar_count: Vec<usize>,
+    }
+    impl fmt::Display for StringTable {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let Self {
+                head,
+                body,
+                verticalbar_count,
+            } = self;
+            let w = body
+                .iter()
+                .map(|row| row.values.len())
+                .max()
+                .unwrap_or_default();
+            let label_width = once(head.label.chars().count())
+                .chain(body.iter().map(|row| row.label.chars().count()))
+                .max()
+                .unwrap();
+            let value_width = (0..w)
+                .map(|j| {
+                    once(j.to_string().len())
+                        .chain(
+                            body.iter()
+                                .map(|row| row.values.get(j).map_or(0, |s| s.chars().count())),
+                        )
+                        .max()
+                        .unwrap()
+                })
+                .collect::<Vec<_>>();
+            gray(f)?;
+            write!(
+                f,
+                "{}",
+                head.to_string(label_width, &value_width, verticalbar_count, true)
+            )?;
+            resetln(f)?;
+            for row in body {
+                write!(
+                    f,
+                    "{}",
+                    row.to_string(label_width, &value_width, verticalbar_count, false)
+                )?;
+                writeln!(f)?;
+            }
+            Ok(())
+        }
+    }
+    struct StringRow {
+        label: String,
+        values: Vec<String>,
+    }
+    impl StringRow {
+        fn to_string(
+            &self,
+            label_width: usize,
+            value_width: &[usize],
+            varticalbars_count: &[usize],
+            label_align_left: bool,
+        ) -> String {
+            let Self { label, values } = self;
+            let w = value_width.len();
+            let mut s = String::new();
+            s.push_str(&if label_align_left {
+                format!("{label:<label_width$} |")
+            } else {
+                format!("{label:^label_width$} |")
+            });
+            for j in 0..w {
+                let value_width = value_width[j];
+                s.push_str("|".repeat(varticalbars_count[j]).as_str());
+                if varticalbars_count[j] == 0 && j != 0 && value_width <= 1 {
+                    s.push(' ');
+                }
+                match values.get(j) {
+                    Some(value) => {
+                        s.push_str(&format!(" {value:>value_width$}",));
+                    }
+                    None => {
+                        s.push_str(" ".repeat(value_width + 1).as_str());
+                    }
+                }
+            }
+            s
+        }
+    }
+    const GRAY: &str = "\x1b[48;2;127;127;127;37m";
+    const RESET: &str = "\x1b[0m";
+    fn gray(f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{GRAY}")
+    }
+    fn resetln(f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "{RESET}")
+    }
+    /// Format a iterator of [`bool`]s.
+    pub fn bools<B, I>(iter: I) -> String
+    where
+        B: Borrow<bool>,
+        I: IntoIterator<Item = B>,
+    {
+        format!(
+            "[{}]",
+            iter.into_iter()
+                .map(|b| ['.', '#'][usize::from(*(b.borrow()))])
+                .collect::<String>(),
+        )
+    }
+}
