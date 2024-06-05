c68c905789058ad25dc9175614cee321c7294a61
paruma <paruma184az@gmail.com>
2024-06-05 23:05:36 +0900

    転倒数の計算ライブラリの修正

--- src/mylib/math/inversion.rs
+++ src/mylib/math/inversion.rs
@@ -1,10 +1,9 @@
 use ac_library::FenwickTree;
 use cargo_snippet::snippet;
 
-#[snippet]
 /// 転倒数 #{(i, j) | i < j and xs[i] > xs[j]} を求める
 /// 計算量: O(n log n)
-pub fn inversion_number(xs: &[usize]) -> i64 {
+pub fn inversion_number_segtree(xs: &[usize]) -> i64 {
     use ac_library::{Additive, Segtree};
     if xs.is_empty() {
         return 0;
@@ -22,7 +21,10 @@ pub fn inversion_number(xs: &[usize]) -> i64 {
     cnt
 }
 
-pub fn inversion_number_fenwick(xs: &[usize]) -> i64 {
+#[snippet]
+/// 転倒数 #{(i, j) | i < j and xs[i] > xs[j]} を求める
+/// 計算量: O(n log n)
+pub fn inversion_number(xs: &[usize]) -> i64 {
     if xs.is_empty() {
         return 0;
     }
@@ -42,7 +44,15 @@ pub fn inversion_number_fenwick(xs: &[usize]) -> i64 {
 #[cfg(test)]
 mod tests {
     use super::inversion_number;
-    use super::inversion_number_fenwick;
+    use super::inversion_number_segtree;
+
+    #[test]
+    fn test_inversion_nubmer_segtree() {
+        assert_eq!(inversion_number_segtree(&[3, 2, 1]), 3);
+        assert_eq!(inversion_number_segtree(&[1, 2, 3, 4, 5, 6]), 0);
+        assert_eq!(inversion_number_segtree(&[1, 6, 3, 4, 5, 2]), 7);
+        assert_eq!(inversion_number_segtree(&[1, 1, 0, 0, 0, 0]), 8);
+    }
 
     #[test]
     fn test_inversion_nubmer() {
@@ -50,13 +60,5 @@ mod tests {
         assert_eq!(inversion_number(&[1, 2, 3, 4, 5, 6]), 0);
         assert_eq!(inversion_number(&[1, 6, 3, 4, 5, 2]), 7);
         assert_eq!(inversion_number(&[1, 1, 0, 0, 0, 0]), 8);
-    }
-
-    #[test]
-    fn test_inversion_nubmer_fenwick() {
-        assert_eq!(inversion_number_fenwick(&[3, 2, 1]), 3);
-        assert_eq!(inversion_number_fenwick(&[1, 2, 3, 4, 5, 6]), 0);
-        assert_eq!(inversion_number_fenwick(&[1, 6, 3, 4, 5, 2]), 7);
-        assert_eq!(inversion_number_fenwick(&[1, 1, 0, 0, 0, 0]), 8);
     }
 }
