86328ba2a282bbd5deb70f80f9de7e94d3bd0526
paruma <paruma184az@gmail.com>
2024-06-20 02:59:28 +0900

    ABC358 E - Alphabet Tiles

--- src/contest/2024/abc358/e/e.rs
+++ src/contest/2024/abc358/e/e.rs
@@ -14,48 +14,63 @@ impl Problem {
         Problem { k, cs }
     }
     fn solve(&self) -> Answer {
-        // 未AC
+        // 解法: 挿入DP
+        use ac_library::ModInt998244353 as Mint;
+        let k = self.k;
+        let cs = &self.cs;
+        // dp[i][x]: 0-origin (i-1)番目までのアルファベットを使ってx文字作る
+        let mut dp = vec![vec![Mint::new(0); k + 1]; 26 + 1];
+        dp[0][0] = Mint::new(1);
+
+        let comb = Comb::new(k);
+
+        for i in 0..26 {
+            for x in 0..=k {
+                // addition: i文字目のアルファベットを何文字使うか
+                for addition in 0..=cs[i] {
+                    if x + addition <= k {
+                        // 長さ x に addition 個の文字を挿入する場合の数は comb(x + addition, addition) 通り
+                        let tmp = dp[i][x] * comb.comb(x + addition, addition);
+                        dp[i + 1][x + addition] += tmp;
+                    }
+                }
+            }
+        }
+        let ans = (1..=k).map(|i| dp[26][i]).sum::<Mint>().val() as i64;
+        Answer { ans }
+    }
+
+    fn solve2(&self) -> Answer {
+        // FPS を使った解法
+        // 例えばA, B, Cの3種類のアルファベットで6文字を作る場合、
+        // Aを使う数をa, Bを使う数をb, Cを使う数をc としたとき
+        // a + b + c = 6 が成り立つ
+        // 作れる文字は 6!/(a!b!c!) 通り。
+        // 和が一定といえば畳み込み(多項式のかけ算) である。
+
         use ac_library::ModInt998244353 as Mint;
         let k = self.k;
         let cs = &self.cs;
         let c_max = cs.iter().copied().max().unwrap();
+        // dp[i][x]: 0-origin (i-1)番目までのアルファベットを使ってx文字作る
 
-        //let mut dp = vec![vec![vec![vec![Mint::new(0); c_max + 1]; k + 1]; 26]; 26];
-        let mut dp = vec![vec![vec![vec![Mint::new(0); 26]; c_max + 1]; 26]; k + 1];
+        let comb = Comb::new(usize::max(k, c_max));
+        let inv_fac_list = (0..=c_max).map(|i| comb.inv_factorial(i)).collect_vec();
 
-        for ch1 in 0..26 {
-            for res in 0..=c_max {
-                if res > cs[ch1] {
-                    continue;
-                }
-                dp[0][ch1][res][0] = Mint::new(1);
-            }
-        }
-        dbg!(&dp[0]);
+        // mod 998244353 での畳み込みは長さ 2^23 ≒ 8×10^6 までできる。
+        // 今回の制約であれば問題ない (長さはたかだか 2.6×10^4 にしかならない)
+        let convolution_result = (0..26)
+            .map(|i| &inv_fac_list[0..=cs[i]])
+            .fold(vec![Mint::new(1)], |acc, x| {
+                ac_library::convolution(&acc, x)
+            });
 
-        for i in 0..k {
-            for ch1 in 0..26 {
-                for res in 1..=c_max {
-                    if res > cs[ch1] {
-                        continue;
-                    }
-                    let tmp = (0..26).map(|ch| dp[i][ch1][res - 1][ch]).sum::<Mint>();
+        // 畳み込みの結果は長さが足りないケースがあるので注意(足りない分は0として扱う)
+        let ans = (1..=k)
+            .map(|l| comb.factorial(l) * convolution_result.get(l).unwrap_or(&Mint::new(0)))
+            .sum::<Mint>()
+            .val() as i64;
 
-                    lg!(tmp);
-
-                    for ch2 in 0..26 {
-                        dp[i + 1][ch1][res][ch2] = tmp;
-                    }
-                }
-            }
-        }
-        let ans = iproduct!(1..=k, 0..26)
-            .map(|(i, ch2)| dp[i][0][cs[0]][ch2])
-            .sum::<Mint>();
-
-        //lg!(&dp);
-
-        let ans = ans.val() as i64;
         Answer { ans }
     }
 
@@ -79,7 +94,7 @@ impl Answer {
 }
 
 fn main() {
-    Problem::read().solve().print();
+    Problem::read().solve2().print();
 }
 
 #[cfg(test)]
@@ -214,287 +229,51 @@ fn print_yesno(ans: bool) {
 }
 
 // ====== snippet ======
-use lg::*;
-pub mod lg {
-    use std::borrow::Borrow;
-    use std::fmt;
-    use std::iter::once;
-    /// Print the values with the line number.
-    /// # Examples
-    /// ```rust
-    /// # use lg::*;
-    /// let x = 42;
-    /// let y = 43;
-    /// lg!(x);
-    /// lg!(x, y);
-    /// lg!(42, x, 43, y);
-    /// ```
-    #[macro_export]
-    macro_rules ! lg {(@ contents $ head : expr $ (, $ tail : expr ) * ) => {{$ crate :: __lg_internal ! ($ head ) ; $ (eprint ! ("," ) ; $ crate :: __lg_internal ! ($ tail ) ; ) * eprintln ! () ; } } ; ($ ($ expr : expr ) ,* $ (, ) ? ) => {{eprint ! ("{}\u{276f}" , line ! () ) ; $ crate :: lg ! (@ contents $ ($ expr ) ,* ) } } ; }
-    #[doc(hidden)]
-    #[macro_export]
-    macro_rules! __lg_internal {
-        ($ value : expr ) => {{
-            match $value {
-                head => {
-                    eprint!(
-                        " {} = {}",
-                        stringify!($value),
-                        $crate::__quiet(format!("{:?}", &head))
-                    );
-                }
+use mod_combinatorics::*;
+pub mod mod_combinatorics {
+    use ac_library::ModInt998244353 as Mint;
+    pub struct Comb {
+        fac: Vec<Mint>,
+        invfac: Vec<Mint>,
+    }
+    impl Comb {
+        pub fn new(max_val: usize) -> Self {
+            let mut inv = vec![Mint::new(0); max_val + 1];
+            let mut fac = vec![Mint::new(0); max_val + 1];
+            let mut invfac = vec![Mint::new(0); max_val + 1];
+            fac[0] = 1.into();
+            fac[1] = 1.into();
+            invfac[0] = 1.into();
+            invfac[1] = 1.into();
+            inv[1] = 1.into();
+            let modulus = Mint::modulus() as usize;
+            for i in 2..=max_val {
+                inv[i] = -inv[modulus % i] * Mint::new(modulus / i);
+                fac[i] = fac[i - 1] * Mint::new(i);
+                invfac[i] = invfac[i - 1] * inv[i];
             }
-        }};
-    }
-    /// Print many 1D arrays side-by-side with the line number.
-    /// # Examples
-    /// ```rust
-    /// # use lg::*;
-    /// let a = [1, 2, 3];
-    /// let b = [4, 5, 6];
-    /// let c = [7, 8, 9];
-    /// rows! {
-    ///   "id", // the name of the index
-    ///   @"a" => a,
-    ///   b,
-    ///   @"c" => c,
-    /// }
-    /// ```
-    #[macro_export]
-    macro_rules ! rows {{$ index_label : literal , $ (@ offset $ offset : expr , ) ? $ (@ verticalbar $ verticalbar : expr , ) * $ ($ (@$ label : literal => ) ? $ values : expr ) ,* $ (, ) ? } => {{#! [allow (unused_assignments ) ] let mut rows = $ crate :: Rows :: default () ; rows . line_number (line ! () ) ; $ (rows . offset ($ offset ) ; ) ? $ (rows . verticalbar ($ verticalbar ) ; ) * rows . index_label ($ index_label ) ; $ ({let mut label = stringify ! ($ values ) . to_string () ; if label . starts_with ("&" ) {label = label [1 .. ] . to_string () ; } $ ({let label_ : &'static str = $ label ; label = label_ . to_string () ; } ) ? rows . row (label , $ values ) ; } ) * eprintln ! ("{}" , rows . to_string_table () ) ; } } ; }
-    /// Print the 2D array with the line number.
-    /// # Examples
-    /// ```rust
-    /// # use lg::*;
-    /// let a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-    /// table! {
-    ///    @"a" => a,
-    /// }
-    /// table! {
-    ///   a,
-    /// }
-    /// ```
-    #[macro_export]
-    macro_rules ! table {{$ (@$ name : literal => ) ? $ values : expr $ (, ) ? } => {{#! [allow (unused_assignments ) ] let mut name = stringify ! ($ values ) . to_string () ; if name . starts_with ("&" ) {name = name [1 .. ] . to_string () ; } $ ({let name_ : &'static str = $ name ; name = name_ . to_string () ; } ) ? let mut rows = $ crate :: Rows :: default () ; rows . line_number (line ! () ) ; rows . table_name (name ) ; # [allow (array_into_iter ) ] for (i , row ) in $ values . into_iter () . enumerate () {rows . row (i . to_string () , row ) ; } eprintln ! ("{}" , rows . to_string_table () ) ; } } ; }
-    #[doc(hidden)]
-    pub fn __quiet(s: impl AsRef<str>) -> String {
-        s.as_ref()
-            .replace("340282366920938463463374607431768211455", "*")
-            .replace("170141183460469231731687303715884105727", "*")
-            .replace("18446744073709551615", "*")
-            .replace("9223372036854775807", "*")
-            .replace("-9223372036854775808", "*")
-            .replace("4294967295", "*")
-            .replace("2147483647", "*")
-            .replace("-2147483648", "*")
-            .replace("None", "*")
-            .replace("Some", "")
-            .replace("true", "#")
-            .replace("false", ".")
-            .replace(['"', '\''], "")
-    }
-    #[doc(hidden)]
-    #[derive(Default)]
-    pub struct Rows {
-        line_number: String,
-        index_label: String,
-        offset: usize,
-        verticalbars: Vec<usize>,
-        table_name: String,
-        rows: Vec<Row>,
-    }
-    impl Rows {
-        pub fn line_number(&mut self, line_number: u32) -> &mut Self {
-            self.line_number = format!("{}", line_number);
-            self
+            Self { fac, invfac }
         }
-        pub fn index_label(&mut self, index_label: impl Into<String>) -> &mut Self {
-            self.index_label = index_label.into();
-            self
-        }
-        pub fn offset(&mut self, offset: usize) -> &mut Self {
-            self.offset = offset;
-            self
-        }
-        pub fn verticalbar(&mut self, verticalbar: impl IntoIterator<Item = usize>) -> &mut Self {
-            self.verticalbars.extend(verticalbar);
-            self
-        }
-        pub fn table_name(&mut self, table_name: impl Into<String>) -> &mut Self {
-            self.table_name = table_name.into();
-            self
-        }
-        pub fn row(
-            &mut self,
-            label: impl Into<String>,
-            values: impl IntoIterator<Item = impl fmt::Debug>,
-        ) -> &mut Self {
-            self.rows.push(Row {
-                label: label.into(),
-                values: values
-                    .into_iter()
-                    .map(|value| __quiet(format!("{:?}", value)))
-                    .collect(),
-            });
-            self
-        }
-        pub fn to_string_table(self) -> StringTable {
-            let Self {
-                line_number,
-                index_label,
-                offset,
-                verticalbars,
-                table_name,
-                rows,
-            } = self;
-            let w = rows
-                .iter()
-                .map(|row| row.values.len())
-                .max()
-                .unwrap_or_default();
-            let mut verticalbar_count = vec![0; w + 1];
-            for &v in &verticalbars {
-                if (offset..=offset + w).contains(&v) {
-                    verticalbar_count[v - offset] += 1;
-                }
-            }
-            StringTable {
-                head: StringRow {
-                    label: format!(
-                        "{line_number}❯ {table_name}{index_label}",
-                        index_label = if index_label.is_empty() {
-                            String::new()
-                        } else {
-                            format!("[{}]", index_label)
-                        }
-                    ),
-                    values: (offset..offset + w)
-                        .map(|index| index.to_string())
-                        .collect(),
-                },
-                body: rows
-                    .iter()
-                    .map(|row| StringRow {
-                        label: row.label.clone(),
-                        values: row.values.clone(),
-                    })
-                    .collect(),
-                verticalbar_count,
-            }
-        }
-    }
-    struct Row {
-        label: String,
-        values: Vec<String>,
-    }
-    #[doc(hidden)]
-    pub struct StringTable {
-        head: StringRow,
-        body: Vec<StringRow>,
-        verticalbar_count: Vec<usize>,
-    }
-    impl fmt::Display for StringTable {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            let Self {
-                head,
-                body,
-                verticalbar_count,
-            } = self;
-            let w = body
-                .iter()
-                .map(|row| row.values.len())
-                .max()
-                .unwrap_or_default();
-            let label_width = once(head.label.chars().count())
-                .chain(body.iter().map(|row| row.label.chars().count()))
-                .max()
-                .unwrap();
-            let value_width = (0..w)
-                .map(|j| {
-                    once(j.to_string().len())
-                        .chain(
-                            body.iter()
-                                .map(|row| row.values.get(j).map_or(0, |s| s.chars().count())),
-                        )
-                        .max()
-                        .unwrap()
-                })
-                .collect::<Vec<_>>();
-            gray(f)?;
-            write!(
-                f,
-                "{}",
-                head.to_string(label_width, &value_width, verticalbar_count, true)
-            )?;
-            resetln(f)?;
-            for row in body {
-                write!(
-                    f,
-                    "{}",
-                    row.to_string(label_width, &value_width, verticalbar_count, false)
-                )?;
-                writeln!(f)?;
-            }
-            Ok(())
-        }
-    }
-    struct StringRow {
-        label: String,
-        values: Vec<String>,
-    }
-    impl StringRow {
-        fn to_string(
-            &self,
-            label_width: usize,
-            value_width: &[usize],
-            varticalbars_count: &[usize],
-            label_align_left: bool,
-        ) -> String {
-            let Self { label, values } = self;
-            let w = value_width.len();
-            let mut s = String::new();
-            s.push_str(&if label_align_left {
-                format!("{label:<label_width$} |")
+        pub fn comb(&self, n: usize, k: usize) -> Mint {
+            if n < k {
+                0.into()
             } else {
-                format!("{label:^label_width$} |")
-            });
-            for j in 0..w {
-                let value_width = value_width[j];
-                s.push_str("|".repeat(varticalbars_count[j]).as_str());
-                if varticalbars_count[j] == 0 && j != 0 && value_width <= 1 {
-                    s.push(' ');
-                }
-                match values.get(j) {
-                    Some(value) => {
-                        s.push_str(&format!(" {value:>value_width$}",));
-                    }
-                    None => {
-                        s.push_str(" ".repeat(value_width + 1).as_str());
-                    }
-                }
+                self.fac[n] * self.invfac[k] * self.invfac[n - k]
             }
-            s
         }
-    }
-    const GRAY: &str = "\x1b[48;2;127;127;127;37m";
-    const RESET: &str = "\x1b[0m";
-    fn gray(f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{GRAY}")
-    }
-    fn resetln(f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "{RESET}")
-    }
-    /// Format a iterator of [`bool`]s.
-    pub fn bools<B, I>(iter: I) -> String
-    where
-        B: Borrow<bool>,
-        I: IntoIterator<Item = B>,
-    {
-        format!(
-            "[{}]",
-            iter.into_iter()
-                .map(|b| ['.', '#'][usize::from(*(b.borrow()))])
-                .collect::<String>(),
-        )
+        pub fn perm(&self, n: usize, k: usize) -> Mint {
+            if n < k {
+                0.into()
+            } else {
+                self.fac[n] * self.invfac[n - k]
+            }
+        }
+        pub fn factorial(&self, n: usize) -> Mint {
+            self.fac[n]
+        }
+
+        pub fn inv_factorial(&self, n: usize) -> Mint {
+            self.invfac[n]
+        }
     }
 }
