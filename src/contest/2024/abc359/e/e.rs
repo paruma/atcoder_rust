f5ca141619829270ae416de53dceb2415e5b0cd1
paruma <paruma184az@gmail.com>
2024-06-23 20:01:39 +0900

    ABC 359 E - Water Tank (LazySegtree 解法/Stack解法)

--- src/contest/2024/abc359/e/e.rs
+++ src/contest/2024/abc359/e/e.rs
@@ -14,11 +14,12 @@ impl Problem {
         Problem { n, hs }
     }
     fn solve(&self) -> Answer {
+        // Segtree Beats!(Range Chmax Range Sum) 解法
         let mut seg = SegtreeBeats::<RangeChmaxRangeSum>::from(
             vec![0; self.n]
                 .iter()
                 .copied()
-                .map(RangeSum::unit)
+                .map(range_chmax_range_sum::RangeSum::unit)
                 .collect_vec(),
         );
 
@@ -27,6 +28,66 @@ impl Problem {
         for (i, h) in self.hs.iter().copied().enumerate() {
             seg.apply_range(0..=i, ChmaxFunc::new(h));
             ans.push(seg.prod(0..=i).unwrap().sum + 1);
+        }
+
+        Answer { ans }
+    }
+
+    fn solve2(&self) -> Answer {
+        // Stack & Range Update Range Sum 解法
+
+        let mut seg = RangeAffineRangeSumSegtree::new(&vec![0_i64; self.n]);
+        let mut ans = vec![];
+        let mut stack = Stack::<usize>::new();
+
+        for (i, h) in self.hs.iter().copied().enumerate() {
+            while let Some(old_i) = stack.peek().copied() {
+                if self.hs[old_i] < h {
+                    stack.pop();
+                } else {
+                    break;
+                }
+            }
+
+            let begin = stack.peek().map(|old_i| old_i + 1).unwrap_or(0);
+            let end = i + 1;
+
+            seg.apply_range_update(begin..end, h);
+            ans.push(seg.range_sum(0..=i) + 1);
+            stack.push(i);
+        }
+
+        Answer { ans }
+    }
+
+    fn solve3(&self) -> Answer {
+        // Stack 解法 (Segtree free)
+
+        let mut sum = 0;
+        let mut ans = vec![];
+        let mut stack = Stack::<usize>::new();
+
+        for (i, h) in self.hs.iter().copied().enumerate() {
+            while let Some(old_i) = stack.peek().copied() {
+                if self.hs[old_i] < h {
+                    stack.pop();
+                    let width = old_i as i64
+                        - stack
+                            .peek()
+                            .map(|old_old_i| *old_old_i as i64)
+                            .unwrap_or(-1);
+                    sum -= width * self.hs[old_i];
+                } else {
+                    break;
+                }
+            }
+
+            let begin = stack.peek().map(|old_i| old_i + 1).unwrap_or(0);
+            let end = i + 1;
+
+            sum += (end - begin) as i64 * h;
+            ans.push(sum + 1);
+            stack.push(i);
         }
 
         Answer { ans }
@@ -53,7 +114,7 @@ impl Answer {
 }
 
 fn main() {
-    Problem::read().solve().print();
+    Problem::read().solve3().print();
 }
 
 #[cfg(test)]
@@ -132,6 +193,7 @@ mod tests {
     }
 }
 
+use ac_library::LazySegtree;
 // ====== import ======
 #[allow(unused_imports)]
 use itertools::{chain, iproduct, izip, Itertools};
@@ -638,6 +700,183 @@ pub mod range_chmax_range_sum {
             ChmaxFunc {
                 chmax_val: max(f.chmax_val, g.chmax_val),
             }
+        }
+    }
+}
+
+use range_affine_range_sum::*;
+pub mod range_affine_range_sum {
+    use ac_library::{LazySegtree, MapMonoid, Monoid};
+    use itertools::Itertools;
+    use std::convert::Infallible;
+    use std::marker::PhantomData;
+    use std::ops::{Add, Mul, RangeBounds};
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+    pub struct RangeSum<T> {
+        pub sum: T,
+        pub len: i64,
+    }
+    impl<T> RangeSum<T> {
+        pub fn unit(x: T) -> RangeSum<T> {
+            RangeSum { sum: x, len: 1 }
+        }
+    }
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+    pub struct Affine<T> {
+        pub slope: T,
+        pub intercept: T,
+    }
+    impl<T> Affine<T>
+    where
+        T: From<i64>,
+    {
+        /// 区間変更用（定数関数）
+        pub fn constant_func(x: T) -> Affine<T> {
+            Affine {
+                slope: 0.into(),
+                intercept: x,
+            }
+        }
+        /// 区間加算用
+        pub fn addition_func(x: T) -> Affine<T> {
+            Affine {
+                slope: 1.into(),
+                intercept: x,
+            }
+        }
+    }
+    pub struct ValueLenSum<T>(Infallible, PhantomData<fn() -> T>);
+    impl<T> Monoid for ValueLenSum<T>
+    where
+        T: Copy + Mul<Output = T> + Add<Output = T> + From<i64>,
+    {
+        type S = RangeSum<T>;
+        fn identity() -> RangeSum<T> {
+            RangeSum {
+                sum: 0.into(),
+                len: 0,
+            }
+        }
+        fn binary_operation(a: &RangeSum<T>, b: &RangeSum<T>) -> RangeSum<T> {
+            RangeSum {
+                sum: a.sum + b.sum,
+                len: a.len + b.len,
+            }
+        }
+    }
+    pub struct RangeAffineRangeSum<T>(Infallible, PhantomData<fn() -> T>);
+    impl<T> MapMonoid for RangeAffineRangeSum<T>
+    where
+        T: Copy + Mul<Output = T> + Add<Output = T> + From<i64>,
+    {
+        type M = ValueLenSum<T>;
+        type F = Affine<T>;
+        fn identity_map() -> Affine<T> {
+            Affine {
+                slope: 1.into(),
+                intercept: 0.into(),
+            }
+        }
+        fn composition(a: &Affine<T>, b: &Affine<T>) -> Affine<T> {
+            Affine {
+                slope: a.slope * b.slope,
+                intercept: a.slope * b.intercept + a.intercept,
+            }
+        }
+        fn mapping(f: &Affine<T>, x: &RangeSum<T>) -> RangeSum<T> {
+            RangeSum {
+                sum: f.slope * x.sum + f.intercept * x.len.into(),
+                len: x.len,
+            }
+        }
+    }
+    pub struct RangeAffineRangeSumSegtree<T>
+    where
+        T: Copy + Mul<Output = T> + Add<Output = T> + From<i64>,
+    {
+        segtree: LazySegtree<RangeAffineRangeSum<T>>,
+    }
+    impl<T> RangeAffineRangeSumSegtree<T>
+    where
+        T: Copy + Mul<Output = T> + Add<Output = T> + From<i64>,
+    {
+        pub fn new(xs: &[T]) -> RangeAffineRangeSumSegtree<T> {
+            let xs = xs.iter().copied().map(RangeSum::unit).collect_vec();
+            RangeAffineRangeSumSegtree {
+                segtree: LazySegtree::from(xs),
+            }
+        }
+        pub fn set(&mut self, p: usize, x: T) {
+            self.segtree.set(p, RangeSum::unit(x));
+        }
+        pub fn get(&mut self, p: usize) -> T {
+            self.segtree.get(p).sum
+        }
+        pub fn range_sum<R>(&mut self, range: R) -> T
+        where
+            R: RangeBounds<usize>,
+        {
+            self.segtree.prod(range).sum
+        }
+        pub fn all_sum(&self) -> T {
+            self.segtree.all_prod().sum
+        }
+        pub fn apply_affine(&mut self, p: usize, slope: T, intercept: T) {
+            self.segtree.apply(p, Affine { slope, intercept })
+        }
+        pub fn apply_update(&mut self, p: usize, x: T) {
+            self.segtree.apply(p, Affine::constant_func(x))
+        }
+        pub fn apply_add(&mut self, p: usize, x: T) {
+            self.segtree.apply(p, Affine::addition_func(x))
+        }
+        pub fn apply_range_affine<R>(&mut self, range: R, slope: T, intercept: T)
+        where
+            R: RangeBounds<usize>,
+        {
+            self.segtree.apply_range(range, Affine { slope, intercept })
+        }
+        pub fn apply_range_update<R>(&mut self, range: R, x: T)
+        where
+            R: RangeBounds<usize>,
+        {
+            self.segtree.apply_range(range, Affine::constant_func(x))
+        }
+        pub fn apply_range_add<R>(&mut self, range: R, x: T)
+        where
+            R: RangeBounds<usize>,
+        {
+            self.segtree.apply_range(range, Affine::addition_func(x))
+        }
+    }
+}
+
+use mod_stack::*;
+pub mod mod_stack {
+    #[derive(Clone, Debug, PartialEq, Eq)]
+    pub struct Stack<T> {
+        raw: Vec<T>,
+    }
+    impl<T> Stack<T> {
+        pub fn new() -> Self {
+            Stack { raw: Vec::new() }
+        }
+        pub fn push(&mut self, value: T) {
+            self.raw.push(value)
+        }
+        pub fn pop(&mut self) -> Option<T> {
+            self.raw.pop()
+        }
+        pub fn peek(&self) -> Option<&T> {
+            self.raw.last()
+        }
+        pub fn is_empty(&self) -> bool {
+            self.raw.is_empty()
+        }
+    }
+    impl<T> Default for Stack<T> {
+        fn default() -> Self {
+            Self::new()
         }
     }
 }
