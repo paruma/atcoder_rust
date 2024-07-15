3a16c6828b54e89d0216937ee638503d3169e3ea
paruma <paruma184az@gmail.com>
2024-07-16 01:43:28 +0900

    ABC362 E - Count Arithmetic Subsequences (DefaultHashMap 使用)

--- src/contest/2024/abc362/e/e.rs
+++ src/contest/2024/abc362/e/e.rs
@@ -165,6 +165,70 @@ impl Problem {
         Answer { ans }
     }
 
+    fn solve3(&self) -> Answer {
+        // DefaultHashMap を使った DP の実装
+        use ac_library::ModInt998244353 as Mint;
+        let n = self.n;
+        let xs = &self.xs;
+
+        // dp[i][k][c][d] = xs[0..i] で 長さk の部分文字列を取って、初項がcで公差がdとなる場合の数
+        let mut dp =
+            vec![vec![DefaultHashMap::<i64, DefaultHashMap<i64, Mint>>::default(); n + 1]; n + 1];
+
+        // diff_list[i] = 初項がxs[i] のときのありえる公差
+        let diff_list = {
+            let mut diff_list = vec![HashSet::<i64>::new(); n];
+            for i in 0..n {
+                for j in i + 1..n {
+                    diff_list[i].insert(xs[j] - xs[i]);
+                }
+            }
+            diff_list
+        };
+
+        // k=1 の初期化
+        for i in 0..=n {
+            for p in 0..i {
+                for &d in &diff_list[p] {
+                    dp[i][1][xs[p]][d] += 1;
+                }
+            }
+        }
+
+        for i in 1..n {
+            for k in 1..n {
+                let mut next_dp = DefaultHashMap::<i64, DefaultHashMap<i64, Mint>>::default();
+                // 初項
+                for &c in dp[i][k].keys() {
+                    // 公差
+                    for &d in dp[i][k][c].keys() {
+                        let term1 = if xs[i] == c + (k as i64) * d {
+                            dp[i][k][c][d]
+                        } else {
+                            Mint::new(0)
+                        };
+                        let term2 = dp[i][k + 1][c][d];
+                        next_dp[c][d] = term1 + term2;
+                    }
+                }
+                dp[i + 1][k + 1] = next_dp;
+            }
+        }
+
+        let mut ans = vec![];
+
+        ans.push(n as i64); // k = 1の答え
+        for k in 2..=n {
+            let sub_ans = dp[n][k]
+                .values()
+                .flat_map(|dp_sub| dp_sub.values())
+                .sum::<Mint>()
+                .val() as i64;
+            ans.push(sub_ans);
+        }
+
+        Answer { ans }
+    }
     #[allow(dead_code)]
     fn solve_naive(&self) -> Answer {
         todo!();
@@ -185,7 +249,7 @@ impl Answer {
 }
 
 fn main() {
-    Problem::read().solve2().print();
+    Problem::read().solve3().print();
 }
 
 #[cfg(test)]
@@ -320,3 +384,129 @@ fn print_yesno(ans: bool) {
 }
 
 // ====== snippet ======
+use default_hash_map::*;
+#[allow(clippy::module_inception)]
+pub mod default_hash_map {
+    use std::hash::Hash;
+    use std::{
+        borrow::Borrow,
+        collections::{
+            hash_map::{Iter, IterMut, Keys, Values, ValuesMut},
+            HashMap,
+        },
+    };
+    #[derive(Clone, Debug)]
+    pub struct DefaultHashMap<K, V> {
+        raw: HashMap<K, V>,
+        default: V,
+    }
+    impl<K, V> DefaultHashMap<K, V> {
+        pub fn new(default: V) -> DefaultHashMap<K, V> {
+            DefaultHashMap {
+                raw: HashMap::new(),
+                default,
+            }
+        }
+        pub fn from_hash_map(hash_map: HashMap<K, V>, default: V) -> DefaultHashMap<K, V> {
+            DefaultHashMap {
+                raw: hash_map,
+                default,
+            }
+        }
+        pub fn raw(&mut self) -> &mut HashMap<K, V> {
+            &mut self.raw
+        }
+        pub fn keys(&self) -> Keys<K, V> {
+            self.raw.keys()
+        }
+        pub fn values(&self) -> Values<K, V> {
+            self.raw.values()
+        }
+        pub fn values_mut(&mut self) -> ValuesMut<K, V> {
+            self.raw.values_mut()
+        }
+        pub fn iter(&self) -> Iter<K, V> {
+            self.raw.iter()
+        }
+        pub fn iter_mut(&mut self) -> IterMut<K, V> {
+            self.raw.iter_mut()
+        }
+        pub fn len(&mut self) -> usize {
+            self.raw.len()
+        }
+        pub fn is_empty(&mut self) -> bool {
+            self.raw.is_empty()
+        }
+    }
+    impl<K, V> DefaultHashMap<K, V>
+    where
+        K: Eq + Hash,
+    {
+        pub fn get<Q: ?Sized>(&self, k: &Q) -> &V
+        where
+            K: Borrow<Q>,
+            Q: Hash + Eq,
+        {
+            self.raw.get(k).unwrap_or(&self.default)
+        }
+        pub fn get_mut(&mut self, k: K) -> &mut V
+        where
+            V: Clone,
+        {
+            self.raw.entry(k).or_insert(self.default.clone())
+        }
+        pub fn insert(&mut self, k: K, v: V) -> Option<V> {
+            self.raw.insert(k, v)
+        }
+        pub fn remove<Q: ?Sized>(&mut self, k: &Q) -> Option<V>
+        where
+            K: Borrow<Q>,
+            Q: Hash + Eq,
+        {
+            self.raw.remove(k)
+        }
+    }
+    impl<K, V> PartialEq for DefaultHashMap<K, V>
+    where
+        K: Eq + Hash,
+        V: PartialEq,
+    {
+        fn eq(&self, other: &DefaultHashMap<K, V>) -> bool {
+            self.raw == other.raw && self.default == other.default
+        }
+    }
+    impl<K, V> Eq for DefaultHashMap<K, V>
+    where
+        K: Eq + Hash,
+        V: Eq,
+    {
+    }
+    impl<K, V> Default for DefaultHashMap<K, V>
+    where
+        V: Default,
+    {
+        fn default() -> DefaultHashMap<K, V> {
+            DefaultHashMap::new(V::default())
+        }
+    }
+    impl<K, V> std::ops::Index<K> for DefaultHashMap<K, V>
+    where
+        K: Eq + Hash,
+    {
+        type Output = V;
+        #[inline]
+        fn index(&self, key: K) -> &V {
+            self.get(&key)
+        }
+    }
+    impl<K, V> std::ops::IndexMut<K> for DefaultHashMap<K, V>
+    where
+        K: Eq + Hash,
+        V: Clone,
+    {
+        #[inline]
+        fn index_mut(&mut self, key: K) -> &mut V {
+            self.get_mut(key)
+        }
+    }
+}
