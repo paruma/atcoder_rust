7d30719214fee468a1962d4628eb617b4dd1723e
paruma <paruma184az@gmail.com>
2024-07-16 21:41:59 +0900

    acl_string のサンプルコード修正 (missisippi → mississippi)

--- src/example/acl_string.rs
+++ src/example/acl_string.rs
@@ -9,19 +9,19 @@ mod tests {
 
     #[test]
     fn test_suffix_string() {
-        let s = "missisippi".chars().collect_vec();
+        let s = "mississippi".chars().collect_vec();
         let n = s.len();
         let sa = suffix_array_arbitrary(&s);
         for i in 0..n {
             let suffix = &s[sa[i]..];
             let suffix_string = suffix.iter().collect::<String>();
-            println!("{} {}", i, suffix_string);
+            println!("{:>2} {}", sa[i], suffix_string);
         }
     }
 
     #[test]
     fn test_lcp_array() {
-        let s = "missisippi".chars().collect_vec();
+        let s = "mississippi".chars().collect_vec();
         let n = s.len();
         let sa = suffix_array_arbitrary(&s);
         let lcp_array = lcp_array_arbitrary(&s, &sa);
@@ -38,7 +38,7 @@ mod tests {
 
     #[test]
     fn test_z_algorithm() {
-        let s = "missisippi".chars().collect_vec();
+        let s = "mississippi".chars().collect_vec();
         let n = s.len();
         let lcp_len = z_algorithm_arbitrary(&s);
         for i in 0..n {
