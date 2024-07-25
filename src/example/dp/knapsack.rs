6d6d39ca93d95cd21c0ee8f95ecf3406d63268f0
paruma <paruma184az@gmail.com>
2024-07-25 23:34:42 +0900

    ナップサック復元サンプルコード

--- src/example/dp/knapsack.rs
+++ src/example/dp/knapsack.rs
@@ -8,7 +8,7 @@ struct Item {
 
 /// 計算量: O(|items| * max_weight)
 #[allow(dead_code)]
-fn solve(n: usize, items: &[Item], max_weight: i64) -> i64 {
+fn knapsack(n: usize, items: &[Item], max_weight: i64) -> i64 {
     // dp[i][w] := [0,i) の items を使用したときの重さ w 以下での価値の最大値
     // dp[0][0] = 0
     // dp[0][w] = 0 (w!=0)
@@ -57,6 +57,75 @@ fn solve(n: usize, items: &[Item], max_weight: i64) -> i64 {
     dp.at(n, max_weight).get_fin()
 }
 
+#[allow(dead_code)]
+fn knapsack_with_restore(n: usize, items: &[Item], max_weight: i64) -> (i64, Vec<bool>) {
+    // dp[i][w] := [0,i) の items を使用したときの重さ w 以下での価値の最大値
+    // dp[0][0] = 0
+    // dp[0][w] = 0 (w!=0)
+    // dp[i+1][w] = max(dp[i][w], dp[i][w-items[i].weight] + items[i].value);
+    // 答えは必ず存在する。max_weight>=0なら、重さmax_weight以下となるような選び方は必ず存在するので（何も選ばないという選び方）
+
+    struct Dp {
+        dp: Vec<Vec<NegExtInt>>,
+    }
+    impl Dp {
+        fn new(n: usize, max_weight: i64) -> Dp {
+            let max_weight = max_weight as usize;
+            Dp {
+                dp: vec![vec![NegInf; max_weight + 1]; n + 1],
+            }
+        }
+
+        fn at(&self, i: usize, w: i64) -> &NegExtInt {
+            // 添字の小さい方だけ考慮すればよい（大きい方はスルー）
+            if w < 0 {
+                &NegInf
+            } else {
+                &self.dp[i][w as usize]
+            }
+        }
+
+        fn at_mut(&mut self, i: usize, w: i64) -> &mut NegExtInt {
+            &mut self.dp[i][w as usize]
+        }
+    }
+
+    let mut dp = Dp::new(n, max_weight);
+    let mut prev = vec![vec![false; max_weight as usize + 1]; n];
+
+    for w in 0..=max_weight {
+        *dp.at_mut(0, w) = Fin(0);
+    }
+
+    for (i, item) in items.iter().enumerate() {
+        for w in 0..=max_weight {
+            // 現在のアイテムを選択する/しない
+            let choose = *dp.at(i, w - item.weight) + Fin(item.value);
+            let no_choose = *dp.at(i, w);
+            if choose > no_choose {
+                *dp.at_mut(i + 1, w) = choose;
+                prev[i][w as usize] = true;
+            } else {
+                *dp.at_mut(i + 1, w) = no_choose;
+                prev[i][w as usize] = false;
+            }
+        }
+    }
+    // 各アイテムを選んだかどうかを復元
+    let path = {
+        let mut path = vec![false; n];
+        let mut current_weight = max_weight;
+        for i in (0..n).rev() {
+            path[i] = prev[i][current_weight as usize];
+            if path[i] {
+                current_weight -= items[i].weight;
+            }
+        }
+        path
+    };
+
+    (dp.at(n, max_weight).get_fin(), path)
+}
 #[cfg(test)]
 mod tests {
     use itertools::Itertools;
@@ -64,7 +133,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test1() {
+    fn test_knapsack() {
         let n: usize = 6;
         let items = [(2, 3), (1, 2), (3, 6), (2, 1), (1, 3), (5, 85)];
         let items = items
@@ -77,8 +146,33 @@ mod tests {
 
         let max_weight = 9;
 
-        let ans = solve(n, &items, max_weight);
+        let ans = knapsack(n, &items, max_weight);
         assert_eq!(ans, 94);
+    }
+
+    #[test]
+    fn test_knapsack_restore() {
+        let n: usize = 6;
+        let items = [(2, 3), (1, 2), (3, 6), (2, 1), (1, 3), (5, 85)];
+        let items = items
+            .iter()
+            .map(|(weight, value)| Item {
+                weight: *weight,
+                value: *value,
+            })
+            .collect_vec();
+
+        let max_weight = 9;
+
+        let (ans, path) = knapsack_with_restore(n, &items, max_weight);
+        assert_eq!(ans, 94);
+        assert_eq!(
+            (0..n)
+                .filter(|i| path[*i])
+                .map(|i| items[i].value)
+                .sum::<i64>(),
+            94
+        )
     }
 }
 
