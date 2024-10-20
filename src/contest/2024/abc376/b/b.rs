4fd405c321e4e1ae6444d6bbb08c6f2b9af83cec
paruma <paruma184az@gmail.com>
2024-10-20 15:30:57 +0900

    ABC376 B - Hand on Ring (easy) リファクタリング

--- src/contest/2024/abc376/b/b.rs
+++ src/contest/2024/abc376/b/b.rs
@@ -1,15 +1,63 @@
 #[derive_readable]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-struct Instruction {
-    h: char,
-    t: Usize1,
+struct Query {
+    hand: char,
+    target: Usize1,
 }
 
 #[derive(Debug, Clone)]
 struct Problem {
     n: usize,
     nq: usize,
-    qs: Vec<Instruction>,
+    qs: Vec<Query>,
+}
+
+/// 円環
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+struct Ring {
+    len: usize,
+}
+
+impl Ring {
+    fn new(len: usize) -> Ring {
+        Ring { len }
+    }
+    fn inc(&self, x: usize) -> usize {
+        (x + 1) % self.len
+    }
+
+    fn dec(&self, x: usize) -> usize {
+        (x + self.len - 1) % self.len
+    }
+    /// 時計回りに src から dst に移動したときの道のり
+    fn dist_right(&self, src: usize, dst: usize) -> usize {
+        assert!((0..self.len).contains(&src));
+        assert!((0..self.len).contains(&dst));
+        if src > dst {
+            dst + self.len - src
+        } else {
+            dst - src
+        }
+    }
+
+    /// 反時計回りに src から dst に移動したときの道のり
+    fn dist_left(&self, src: usize, dst: usize) -> usize {
+        assert!((0..self.len).contains(&src));
+        assert!((0..self.len).contains(&dst));
+        self.dist_right(dst, src)
+    }
+
+    /// begin から end に時計回りに回ったときに x に当たるかどうか
+    fn contains(&self, begin: usize, end: usize, x: usize) -> bool {
+        assert!((0..self.len).contains(&begin));
+        assert!((0..self.len).contains(&end));
+        if end < begin {
+            (begin..end + self.len).contains(&x)
+                || (begin..end + self.len).contains(&(x + self.len))
+        } else {
+            (begin..end).contains(&x)
+        }
+    }
 }
 
 impl Problem {
@@ -17,12 +65,13 @@ impl Problem {
         input! {
             n: usize,
             nq: usize,
-            qs: [Instruction; nq],
+            qs: [Query; nq],
         }
         Problem { n, nq, qs }
     }
 
     fn solve(&self) -> Answer {
+        // 愚直シミュレーション
         let n = self.n;
         let qs = &self.qs;
 
@@ -35,12 +84,12 @@ impl Problem {
         let dec = |x: usize| (x + n - 1) % n;
 
         for q in &self.qs {
-            if q.h == 'R' {
+            if q.hand == 'R' {
                 let mut c_right = right;
                 let mut fail = false;
                 let mut c_cnt = 0;
                 // 左に動かす
-                while c_right != q.t {
+                while c_right != q.target {
                     c_right = dec(c_right);
                     c_cnt += 1;
                     if c_right == left {
@@ -56,19 +105,19 @@ impl Problem {
                     let mut c_right = right;
                     let mut c_cnt = 0;
                     // 左に動かす
-                    while c_right != q.t {
+                    while c_right != q.target {
                         c_right = inc(c_right);
                         c_cnt += 1;
                     }
                     right = c_right;
                     cnt += c_cnt;
                 }
-            } else if q.h == 'L' {
+            } else if q.hand == 'L' {
                 let mut c_left = left;
                 let mut fail = false;
                 let mut c_cnt = 0;
                 // 左に動かす
-                while c_left != q.t {
+                while c_left != q.target {
                     c_left = dec(c_left);
                     c_cnt += 1;
                     if c_left == right {
@@ -85,7 +134,7 @@ impl Problem {
                     let mut c_cnt = 0;
 
                     // 左に動かす
-                    while c_left != q.t {
+                    while c_left != q.target {
                         c_left = inc(c_left);
                         c_cnt += 1;
                     }
@@ -98,6 +147,92 @@ impl Problem {
         }
 
         let ans = cnt;
+        Answer { ans }
+    }
+
+    fn solve2(&self) -> Answer {
+        // solve のリファクタリング (愚直シミュレーション)
+        let n = self.n;
+        let inc = |x: usize| (x + 1) % n;
+        let dec = |x: usize| (x + n - 1) % n;
+
+        let mut left = 0;
+        let mut right = 1;
+
+        let mut ans = 0;
+
+        for q in &self.qs {
+            let (moving_hand, other_hand) = if q.hand == 'R' {
+                (&mut right, left)
+            } else {
+                (&mut left, right)
+            };
+
+            // 右に動かす
+            let moving_right_len = (|| {
+                let mut current_hand = *moving_hand;
+                let mut cnt = 0;
+                while current_hand != q.target {
+                    current_hand = inc(current_hand);
+                    cnt += 1;
+                    if current_hand == other_hand {
+                        return None;
+                    }
+                }
+                Some(cnt)
+            })();
+            // 左に動かす
+            let moving_left_len = (|| {
+                let mut current_hand = *moving_hand;
+                let mut cnt = 0;
+                while current_hand != q.target {
+                    current_hand = dec(current_hand);
+                    cnt += 1;
+                    if current_hand == other_hand {
+                        return None;
+                    }
+                }
+                Some(cnt)
+            })();
+
+            // *moving_had == q.target の場合はどちらも Some(0) になるので、xor にはできない。
+            let current_cnt = moving_left_len.or(moving_right_len).unwrap();
+            ans += current_cnt;
+            *moving_hand = q.target;
+        }
+        Answer { ans }
+    }
+
+    fn solve3(&self) -> Answer {
+        // O(Q) で計算
+        let n = self.n;
+
+        let mut left = 0;
+        let mut right = 1;
+
+        let ring = Ring::new(n);
+
+        let mut ans = 0;
+
+        for q in &self.qs {
+            let (moving_hand, other_hand) = if q.hand == 'R' {
+                (&mut right, left)
+            } else {
+                (&mut left, right)
+            };
+            // moving_hand から q.target に右に動かしたとき other_hand があるか
+            let moving_len = if ring.contains(*moving_hand, q.target, other_hand) {
+                // 左に動かす
+                ring.dist_left(*moving_hand, q.target)
+            } else {
+                // 右に動かす
+                ring.dist_right(*moving_hand, q.target)
+            };
+            ans += moving_len;
+            *moving_hand = q.target;
+        }
+
+        let ans = ans as i64;
         Answer { ans }
     }
 
@@ -121,7 +256,7 @@ impl Answer {
 }
 
 fn main() {
-    Problem::read().solve().print();
+    Problem::read().solve3().print();
 }
 
 #[cfg(test)]
