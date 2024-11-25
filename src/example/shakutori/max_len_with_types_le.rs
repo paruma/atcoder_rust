ef86cb5c361f4a7912282e2b287731646dd9bb6a
paruma <paruma184az@gmail.com>
2024-11-26 00:08:25 +0900

    尺取法サンプルコード(endを1ずつ進める尺取法)

--- src/example/shakutori/max_len_with_types_le.rs
+++ src/example/shakutori/max_len_with_types_le.rs
@@ -5,9 +5,10 @@ use itertools::Itertools;
 
 use crate::mylib::data_structure::queue0::mod_queue::Queue;
 
-/// 数列 xs の部分列で種類数が k 以下になる最大の長さを求める
+/// 数列 xs の連続部分列で種類数が k 以下になる最大の長さを求める
 /// 参考: 【ゆっくり解説】尺取り法と二分探索の「本当の」違い - YouTube https://www.youtube.com/watch?v=omD-yyb730k
 fn solve(xs: &[i64], k: usize) -> usize {
+    // begin を1ずつ動かす尺取法
     let n = xs.len();
     let mut begin = 0; // [begin, end) の半開区間を考える
     let mut end = 0;
@@ -15,7 +16,7 @@ fn solve(xs: &[i64], k: usize) -> usize {
     let mut max_len = 0;
 
     while begin < n {
-        // begin..end が条件を満たす範囲で end を繰り返し進める
+        // [begin, end) が条件を満たす範囲で end を繰り返し進める
         while end < n {
             // xs[end] を含めることで種類数 が k + 1 になりそうなときは break
             if bag.set_len() == k && bag.contains(&xs[end]) == 0 {
@@ -41,7 +42,51 @@ fn solve(xs: &[i64], k: usize) -> usize {
     max_len
 }
 
-fn solve_deque(xs: &[i64], k: usize) -> usize {
+fn solve2(xs: &[i64], k: usize) -> usize {
+    // 条件を満たさないギリギリまで伸ばす尺取法
+    let n = xs.len();
+    let mut end = 0;
+    let mut bag = HashBag::<i64>::new();
+    let mut max_len = 0;
+    for begin in 0..n {
+        while bag.set_len() <= k && end <= n {
+            if end != n {
+                bag.insert(xs[end]);
+            }
+            end += 1;
+        }
+        // end = min {end | xs[begin..end] の種類数が k より大きい} になっている（min ∅ は n + 1扱い)
+        // ↓
+        // end - 1 = max {end | xs[begin..end] の種類数が k 以下} になっている。
+        // begin を固定したとき、[begin, end - 1) が種類数 k 以下になる最長区間
+        max_len = max_len.max((end - 1) - begin);
+        bag.remove(&xs[begin]);
+    }
+
+    max_len
+}
+
+fn solve3(xs: &[i64], k: usize) -> usize {
+    // end を 1 ずつ動かす尺取法
+    let n = xs.len();
+    let mut begin = 0;
+    let mut bag = HashBag::<i64>::new();
+    let mut max_len = 0;
+    for end in 1..=n {
+        bag.insert(xs[end - 1]);
+        // 区間 [begin, end) が条件を満たすようになるまで begin を進める
+        while bag.set_len() > k {
+            bag.remove(&xs[begin]);
+            begin += 1;
+        }
+        max_len = max_len.max(end - begin);
+    }
+
+    max_len
+}
+
+fn solve_queue(xs: &[i64], k: usize) -> usize {
+    // Queue を使った尺取法
     let mut bag = HashBag::<i64>::new();
     let mut max_len = 0;
 
@@ -81,13 +126,17 @@ mod tests {
         let xs = [1, 2, 3, 4, 5];
         let k = 3;
         assert_eq!(solve(&xs, k), 3);
-        assert_eq!(solve_deque(&xs, k), 3);
+        assert_eq!(solve2(&xs, k), 3);
+        assert_eq!(solve3(&xs, k), 3);
+        assert_eq!(solve_queue(&xs, k), 3);
         assert_eq!(solve_naive(&xs, k), 3);
 
         let xs = [1, 2, 3, 3, 4, 4, 5];
         let k = 3;
         assert_eq!(solve(&xs, k), 5);
-        assert_eq!(solve_deque(&xs, k), 5);
+        assert_eq!(solve2(&xs, k), 5);
+        assert_eq!(solve3(&xs, k), 5);
+        assert_eq!(solve_queue(&xs, k), 5);
         assert_eq!(solve_naive(&xs, k), 5);
     }
 
@@ -101,7 +150,9 @@ mod tests {
             let k = rng.gen_range(0..6);
             let naive_ans = solve_naive(&xs, k);
             assert_eq!(solve(&xs, k), naive_ans);
-            assert_eq!(solve_deque(&xs, k), naive_ans,);
+            assert_eq!(solve2(&xs, k), naive_ans);
+            assert_eq!(solve3(&xs, k), naive_ans, "n={}, xs={:?}, k={}", n, &xs, k);
+            assert_eq!(solve_queue(&xs, k), naive_ans);
         }
     }
 }
