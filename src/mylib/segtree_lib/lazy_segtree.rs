d48030e7ea8c1caf16ffdd685df56b92219363fb
paruma <paruma184az@gmail.com>
2024-12-03 23:56:25 +0900

    range chmin range min ライブラリに追加

--- src/mylib/segtree_lib/lazy_segtree.rs
+++ src/mylib/segtree_lib/lazy_segtree.rs
@@ -380,6 +380,32 @@ pub mod range_affine_range_minmax {
     }
 }
 
+pub mod range_chmin_range_min {
+    // range chmax range max や range chmin range max なども同様に作れる
+    use ac_library::lazysegtree::MapMonoid;
+    use ac_library::Min;
+    use std::convert::Infallible;
+    use std::usize;
+
+    pub struct RangeChminRangeMin(Infallible);
+    impl MapMonoid for RangeChminRangeMin {
+        type M = Min<usize>;
+        type F = usize;
+        fn identity_map() -> Self::F {
+            usize::MAX
+        }
+        fn mapping(
+            f: &Self::F,
+            x: &<Self::M as ac_library::Monoid>::S,
+        ) -> <Self::M as ac_library::Monoid>::S {
+            *f.min(x)
+        }
+        fn composition(f: &Self::F, g: &Self::F) -> Self::F {
+            *f.min(g)
+        }
+    }
+}
+
 #[allow(unused_variables)]
 #[snippet(prefix = "use map_monoid_template::*;")]
 pub mod map_monoid_template {
@@ -1060,6 +1086,25 @@ mod test_range_affine_range_minmax {
             .map(|m| m.max)
             .collect_vec();
         assert_eq!(segtree_as_vec, vec![3, 4, 2, 5, 5, 5, 6, 7, 23, 25]);
+    }
+}
+
+#[cfg(test)]
+pub mod test_range_chmin_range_min {
+    use ac_library::LazySegtree;
+
+    use super::range_chmin_range_min::RangeChminRangeMin;
+
+    #[test]
+    fn test_sample_of_lazy_segtree() {
+        // range chmin range min の遅延セグ木の使用例
+        let xs = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        let mut segtree: LazySegtree<RangeChminRangeMin> = LazySegtree::from(xs);
+
+        segtree.apply_range(3..7, 5); // [0, 1, 2, 5, 5, 5, 5, 7, 8, 9]
+        assert_eq!(segtree.prod(2..5), 2);
+        assert_eq!(segtree.prod(5..8), 5);
     }
 }
 
