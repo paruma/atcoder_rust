65b8dcc60ac92e46ef165147cfd004e44789a366
paruma <paruma184az@gmail.com>
2024-12-14 20:54:58 +0900

    ARC189 本番

---
+++ src/contest/2024/arc189/a/a.rs
@@ -0,0 +1,333 @@
+//#[derive_readable]
+#[derive(Debug, Clone)]
+struct Problem {
+    n: usize,
+    xs: Vec<i64>,
+}
+
+impl Problem {
+    fn read() -> Problem {
+        input! {
+            n: usize,
+            xs: [i64; n],
+        }
+        Problem { n, xs }
+    }
+
+    fn solve(&self) -> Answer {
+        use ac_library::ModInt998244353 as Mint;
+        let rle = self.xs.iter().copied().dedup_with_count().collect_vec();
+        // 最初は1であるべき
+        if rle.first().unwrap().1 == 0 {
+            return Answer { ans: 0 };
+        }
+        // 最後は偶数の場合0、奇数の場合1であるべき
+        if rle.last().unwrap().1 == ((self.n + 1) % 2) as i64 {
+            return Answer { ans: 0 };
+        }
+
+        if rle.iter().copied().any(|(cnt, _)| cnt % 2 == 0) {
+            return Answer { ans: 0 };
+        }
+
+        // 全部奇数
+        let cnts = rle.iter().copied().map(|(cnt, _)| cnt).collect_vec();
+
+        // 1, 3×1, 5×3×1...
+        let cum_prod = (1..=self.n)
+            .scanl(Mint::new(1), |acc, i| *acc * Mint::new(2 * i + 1))
+            .collect_vec();
+
+        //dbg!(&cum_prod);
+
+        let term1 = cnts
+            .iter()
+            .copied()
+            .filter(|cnt| *cnt > 1)
+            .map(|cnt| cum_prod[cnt / 2 - 1])
+            .product::<Mint>();
+
+        let term2 = {
+            //
+
+            let comb = Comb::new(self.n);
+
+            let cnts2 = cnts.iter().copied().filter(|cnt| *cnt > 1).collect_vec();
+
+            if cnts2.is_empty() {
+                Mint::new(1)
+            } else {
+                let mut current = Mint::new(1);
+                let mut waku = 0;
+
+                for &cnt in &cnts2 {
+                    // waku + cnt から cnt 個選ぶ
+
+                    let cnt_op = cnt / 2;
+                    current *= comb.comb(waku + cnt_op, cnt_op);
+                    waku += cnt_op;
+                }
+                current
+            }
+        };
+
+        //dbg!(term1);
+        //dbg!(term2);
+        let ans = (term1 * term2).val() as i64;
+        Answer { ans }
+    }
+
+    #[allow(dead_code)]
+    fn solve_naive(&self) -> Answer {
+        todo!();
+        // let ans = 0;
+        // Answer { ans }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Answer {
+    ans: i64,
+}
+
+impl Answer {
+    fn print(&self) {
+        println!("{}", self.ans);
+    }
+}
+
+fn main() {
+    Problem::read().solve().print();
+}
+
+#[cfg(test)]
+mod tests {
+    #[allow(unused_imports)]
+    use super::*;
+    #[allow(unused_imports)]
+    use rand::{rngs::SmallRng, seq::SliceRandom, *};
+
+    #[test]
+    fn test_problem() {
+        assert_eq!(1 + 1, 2);
+    }
+
+    #[allow(dead_code)]
+    #[derive(Debug)]
+    struct WrongTestCase {
+        problem: Problem,
+        main_ans: Answer,
+        naive_ans: Answer,
+    }
+
+    #[allow(dead_code)]
+    fn check(p: &Problem) -> Option<WrongTestCase> {
+        let main_ans = p.solve();
+        let naive_ans = p.solve_naive();
+        if main_ans != naive_ans {
+            Some(WrongTestCase {
+                problem: p.clone(),
+                main_ans,
+                naive_ans,
+            })
+        } else {
+            None
+        }
+    }
+
+    #[allow(dead_code)]
+    fn make_random_problem(rng: &mut SmallRng) -> Problem {
+        todo!()
+        // let n = rng.gen_range(1..=10);
+        // let p = Problem { _a: n };
+        // println!("{:?}", &p);
+        // p
+    }
+
+    #[allow(unreachable_code)]
+    #[test]
+    fn test_with_naive() {
+        let num_tests = 100;
+        let max_wrong_case = 10; // この件数間違いが見つかったら打ち切り
+        let mut rng = SmallRng::seed_from_u64(42);
+        // let mut rng = SmallRng::from_entropy();
+        let mut wrong_cases: Vec<WrongTestCase> = vec![];
+        for _ in 0..num_tests {
+            let p = make_random_problem(&mut rng);
+            let result = check(&p);
+            if let Some(wrong_test_case) = result {
+                wrong_cases.push(wrong_test_case);
+            }
+            if wrong_cases.len() >= max_wrong_case {
+                break;
+            }
+        }
+
+        if !wrong_cases.is_empty() {
+            for t in &wrong_cases {
+                println!("{:?}", t.problem);
+                println!("main ans : {:?}", t.main_ans);
+                println!("naive ans: {:?}", t.naive_ans);
+                println!();
+            }
+            println!("{} cases are wrong.", wrong_cases.len());
+            panic!();
+        }
+    }
+}
+
+// ====== import ======
+#[allow(unused_imports)]
+use itertools::{chain, iproduct, izip, Itertools};
+#[allow(unused_imports)]
+use proconio::{
+    derive_readable, fastout, input,
+    marker::{Bytes, Chars, Usize1},
+};
+#[allow(unused_imports)]
+use std::cmp::Reverse;
+#[allow(unused_imports)]
+use std::collections::{BinaryHeap, HashMap, HashSet};
+
+// ====== output func ======
+#[allow(unused_imports)]
+use print_vec::*;
+pub mod print_vec {
+
+    use itertools::Itertools;
+    use proconio::fastout;
+    #[fastout]
+    pub fn print_vec<T: std::fmt::Debug>(arr: &[T]) {
+        for a in arr {
+            println!("{:?}", a);
+        }
+    }
+    #[fastout]
+    pub fn print_vec_1line<T: std::fmt::Debug>(arr: &[T]) {
+        let msg = arr.iter().map(|x| format!("{:?}", x)).join(" ");
+        println!("{}", msg);
+    }
+    #[fastout]
+    pub fn print_vec2<T: std::fmt::Debug>(arr: &Vec<Vec<T>>) {
+        for row in arr {
+            let msg = row.iter().map(|x| format!("{:?}", x)).join(" ");
+            println!("{}", msg);
+        }
+    }
+    pub fn print_bytes(bytes: &[u8]) {
+        let msg = String::from_utf8(bytes.to_vec()).unwrap();
+        println!("{}", msg);
+    }
+    pub fn print_chars(chars: &[char]) {
+        let msg = chars.iter().collect::<String>();
+        println!("{}", msg);
+    }
+    #[fastout]
+    pub fn print_vec_bytes(vec_bytes: &[Vec<u8>]) {
+        for row in vec_bytes {
+            let msg = String::from_utf8(row.to_vec()).unwrap();
+            println!("{}", msg);
+        }
+    }
+}
+
+#[allow(unused)]
+fn print_yesno(ans: bool) {
+    let msg = if ans { "Yes" } else { "No" };
+    println!("{}", msg);
+}
+
+// ====== snippet ======
+use scan_iter::*;
+pub mod scan_iter {
+    #[derive(Clone)]
+    pub struct Scanl<I, B, F> {
+        iter: I,
+        state: Option<B>,
+        f: F,
+    }
+    impl<I, B, F> Scanl<I, B, F> {
+        fn new(iter: I, init: B, f: F) -> Scanl<I, B, F> {
+            Scanl {
+                iter,
+                state: Some(init),
+                f,
+            }
+        }
+    }
+    impl<I, B, F> Iterator for Scanl<I, B, F>
+    where
+        B: Copy,
+        I: Iterator,
+        F: FnMut(&mut B, I::Item) -> B,
+    {
+        type Item = B;
+        #[inline]
+        fn next(&mut self) -> Option<B> {
+            let retval = self.state?;
+            let a_opt = self.iter.next();
+            self.state = self
+                .state
+                .and_then(|mut s| a_opt.map(|a| (self.f)(&mut s, a)));
+            Some(retval)
+        }
+    }
+    pub trait IteratorExtScanLeft: Iterator + Sized {
+        fn scanl<B, F>(self, init: B, f: F) -> Scanl<Self, B, F>
+        where
+            Self: Sized,
+            F: FnMut(&mut B, Self::Item) -> B,
+        {
+            Scanl::new(self, init, f)
+        }
+    }
+    impl<T: Iterator> IteratorExtScanLeft for T {}
+}
+
+use mod_combinatorics::*;
+pub mod mod_combinatorics {
+    use ac_library::ModInt998244353 as Mint;
+    pub struct Comb {
+        fac: Vec<Mint>,
+        invfac: Vec<Mint>,
+    }
+    impl Comb {
+        pub fn new(max_val: usize) -> Self {
+            let mut inv = vec![Mint::new(0); max_val + 1];
+            let mut fac = vec![Mint::new(0); max_val + 1];
+            let mut invfac = vec![Mint::new(0); max_val + 1];
+            fac[0] = 1.into();
+            fac[1] = 1.into();
+            invfac[0] = 1.into();
+            invfac[1] = 1.into();
+            inv[1] = 1.into();
+            let modulus = Mint::modulus() as usize;
+            for i in 2..=max_val {
+                inv[i] = -inv[modulus % i] * Mint::new(modulus / i);
+                fac[i] = fac[i - 1] * Mint::new(i);
+                invfac[i] = invfac[i - 1] * inv[i];
+            }
+            Self { fac, invfac }
+        }
+        pub fn comb(&self, n: usize, k: usize) -> Mint {
+            if n < k {
+                0.into()
+            } else {
+                self.fac[n] * self.invfac[k] * self.invfac[n - k]
+            }
+        }
+        pub fn perm(&self, n: usize, k: usize) -> Mint {
+            if n < k {
+                0.into()
+            } else {
+                self.fac[n] * self.invfac[n - k]
+            }
+        }
+        pub fn factorial(&self, n: usize) -> Mint {
+            self.fac[n]
+        }
+        pub fn inv_factorial(&self, n: usize) -> Mint {
+            self.invfac[n]
+        }
+    }
+}
