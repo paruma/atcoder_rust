b3f180c14e9c65c633dc9fe52cea74c09e55e3ce
paruma <paruma184az@gmail.com>
2025-01-05 16:39:28 +0900

    ABC370 E - Avoid K Partition

--- src/contest/2024/abc370/e/e.rs
+++ src/contest/2024/abc370/e/e.rs
@@ -2,6 +2,7 @@
 #[derive(Debug, Clone)]
 struct Problem {
     n: usize,
+    k: i64,
     xs: Vec<i64>,
 }
 
@@ -9,13 +10,46 @@ impl Problem {
     fn read() -> Problem {
         input! {
             n: usize,
+            k: i64,
             xs: [i64; n],
         }
-        Problem { n, xs }
+        Problem { n, k, xs }
     }
 
     fn solve(&self) -> Answer {
-        let ans = 0;
+        // f(i) を [0, i) での場合の数とする。
+        // ■ 漸化式
+        // f(i) = \sum_{j \in [0, i), \sum A[j, i) \neq K} f(j)
+        // f(0) = 1
+        // ■ 答え
+        // f(N)
+        //
+        // A の累積和を S としたとき、\sum A[j, i) \neq K の部分は
+        // S_i - S_j = K
+        // つまり S_j = S_i - K
+        // と書ける。
+        // そういう j に対する f(j) の和を map で収集しておいて、引き戻すみたいなことをする
+        use ac_library::ModInt998244353 as Mint;
+        let n = self.n;
+        let k = self.k;
+        let xs = &self.xs;
+        let xs_prefix_sum = prefix_sum(xs);
+
+        let mut dp = vec![Mint::new(0); n + 1];
+        let mut dp_prefix_sum = vec![Mint::new(0); n + 2]; // dp[i]の累積和
+        let mut map: DefaultHashMap<i64, Mint> = DefaultHashMap::new(Mint::new(0));
+        dp[0] = Mint::new(1);
+        dp_prefix_sum[0] = Mint::new(0);
+        dp_prefix_sum[1] = dp_prefix_sum[0] + dp[0];
+        map[xs_prefix_sum[0]] += dp[0];
+
+        for i in 1..=n {
+            dp[i] = dp_prefix_sum[i] - map[xs_prefix_sum[i] - k];
+            dp_prefix_sum[i + 1] = dp_prefix_sum[i] + dp[i];
+            map[xs_prefix_sum[i]] += dp[i];
+        }
+
+        let ans = dp[n].val() as i64;
         Answer { ans }
     }
 
@@ -180,3 +214,181 @@ fn print_yesno(ans: bool) {
 }
 
 // ====== snippet ======
+use cumsum::*;
+pub mod cumsum {
+    pub fn prefix_sum(xs: &[i64]) -> Vec<i64> {
+        let mut prefix_sum = vec![0; xs.len() + 1];
+        for i in 1..xs.len() + 1 {
+            prefix_sum[i] = prefix_sum[i - 1] + xs[i - 1];
+        }
+        prefix_sum
+    }
+    use std::ops::{Bound, Range, RangeBounds};
+    #[derive(Clone, Debug, PartialEq, Eq)]
+    pub struct CumSum {
+        pub cumsum: Vec<i64>,
+    }
+    impl CumSum {
+        /// 計算量: O(|xs|)
+        pub fn new(xs: &[i64]) -> CumSum {
+            let mut cumsum = vec![0; xs.len() + 1];
+            for i in 1..xs.len() + 1 {
+                cumsum[i] = cumsum[i - 1] + xs[i - 1];
+            }
+            CumSum { cumsum }
+        }
+        fn open(&self, range: impl RangeBounds<usize>) -> Range<usize> {
+            use Bound::Excluded;
+            use Bound::Included;
+            use Bound::Unbounded;
+            let begin = match range.start_bound() {
+                Unbounded => 0,
+                Included(&x) => x,
+                Excluded(&x) => x + 1,
+            };
+            let end = match range.end_bound() {
+                Excluded(&x) => x,
+                Included(&x) => x + 1,
+                Unbounded => self.cumsum.len() - 1,
+            };
+            begin..end
+        }
+        /// 計算量: O(1)
+        pub fn range_sum(&self, range: impl RangeBounds<usize>) -> i64 {
+            let range = self.open(range);
+            self.cumsum[range.end] - self.cumsum[range.start]
+        }
+        pub fn prefix_sum(&self, end: usize) -> i64 {
+            self.cumsum[end]
+        }
+        pub fn suffix_sum(&self, begin: usize) -> i64 {
+            self.cumsum[self.cumsum.len() - 1] - self.cumsum[begin]
+        }
+    }
+}
+use default_hash_map::*;
+#[allow(clippy::module_inception)]
+pub mod default_hash_map {
+    use std::hash::Hash;
+    use std::{
+        borrow::Borrow,
+        collections::{
+            hash_map::{Iter, IterMut, Keys, Values, ValuesMut},
+            HashMap,
+        },
+    };
+    #[derive(Clone, Debug)]
+    pub struct DefaultHashMap<K, V> {
+        raw: HashMap<K, V>,
+        default: V,
+    }
+    impl<K, V> DefaultHashMap<K, V> {
+        pub fn new(default: V) -> DefaultHashMap<K, V> {
+            DefaultHashMap {
+                raw: HashMap::new(),
+                default,
+            }
+        }
+        pub fn from_hash_map(hash_map: HashMap<K, V>, default: V) -> DefaultHashMap<K, V> {
+            DefaultHashMap {
+                raw: hash_map,
+                default,
+            }
+        }
+        pub fn raw(&mut self) -> &mut HashMap<K, V> {
+            &mut self.raw
+        }
+        pub fn keys(&self) -> Keys<K, V> {
+            self.raw.keys()
+        }
+        pub fn values(&self) -> Values<K, V> {
+            self.raw.values()
+        }
+        pub fn values_mut(&mut self) -> ValuesMut<K, V> {
+            self.raw.values_mut()
+        }
+        pub fn iter(&self) -> Iter<K, V> {
+            self.raw.iter()
+        }
+        pub fn iter_mut(&mut self) -> IterMut<K, V> {
+            self.raw.iter_mut()
+        }
+        pub fn len(&mut self) -> usize {
+            self.raw.len()
+        }
+        pub fn is_empty(&mut self) -> bool {
+            self.raw.is_empty()
+        }
+    }
+    impl<K, V> DefaultHashMap<K, V>
+    where
+        K: Eq + Hash,
+    {
+        pub fn get<Q: ?Sized>(&self, k: &Q) -> &V
+        where
+            K: Borrow<Q>,
+            Q: Hash + Eq,
+        {
+            self.raw.get(k).unwrap_or(&self.default)
+        }
+        pub fn get_mut(&mut self, k: K) -> &mut V
+        where
+            V: Clone,
+        {
+            self.raw.entry(k).or_insert(self.default.clone())
+        }
+        pub fn insert(&mut self, k: K, v: V) -> Option<V> {
+            self.raw.insert(k, v)
+        }
+        pub fn remove<Q: ?Sized>(&mut self, k: &Q) -> Option<V>
+        where
+            K: Borrow<Q>,
+            Q: Hash + Eq,
+        {
+            self.raw.remove(k)
+        }
+    }
+    impl<K, V> PartialEq for DefaultHashMap<K, V>
+    where
+        K: Eq + Hash,
+        V: PartialEq,
+    {
+        fn eq(&self, other: &DefaultHashMap<K, V>) -> bool {
+            self.raw == other.raw && self.default == other.default
+        }
+    }
+    impl<K, V> Eq for DefaultHashMap<K, V>
+    where
+        K: Eq + Hash,
+        V: Eq,
+    {
+    }
+    impl<K, V> Default for DefaultHashMap<K, V>
+    where
+        V: Default,
+    {
+        fn default() -> DefaultHashMap<K, V> {
+            DefaultHashMap::new(V::default())
+        }
+    }
+    impl<K, V> std::ops::Index<K> for DefaultHashMap<K, V>
+    where
+        K: Eq + Hash,
+    {
+        type Output = V;
+        #[inline]
+        fn index(&self, key: K) -> &V {
+            self.get(&key)
+        }
+    }
+    impl<K, V> std::ops::IndexMut<K> for DefaultHashMap<K, V>
+    where
+        K: Eq + Hash,
+        V: Clone,
+    {
+        #[inline]
+        fn index_mut(&mut self, key: K) -> &mut V {
+            self.get_mut(key)
+        }
+    }
+}
