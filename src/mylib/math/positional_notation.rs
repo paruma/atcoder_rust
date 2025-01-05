aa73217adb409b63eaec3de8478a5dece51bd8d7
paruma <paruma184az@gmail.com>
2025-01-05 15:28:17 +0900

    positional_notation にコメント追加

--- src/mylib/math/positional_notation.rs
+++ src/mylib/math/positional_notation.rs
@@ -2,10 +2,15 @@ use cargo_snippet::snippet;
 #[allow(clippy::module_inception)]
 #[snippet(prefix = "use positional_notation::*;")]
 pub mod positional_notation {
+
+    /// 配列 xs で表された base 進数の値を評価する
+    /// 例: `eval_base_n_value(&[1, 2, 3], 10) == 123`
     pub fn eval_base_n_value(xs: &[i64], base: i64) -> i64 {
         xs.iter().fold(0, |acc, &x| acc * base + x)
     }
 
+    /// n の base 進数での表記を Vec で表す
+    /// 例: `to_base_n_value(123, 10) == vec![1, 2, 3]`
     pub fn to_base_n_value(x: i64, base: i64) -> Vec<i64> {
         let mut ret = vec![];
         let mut x = x;
