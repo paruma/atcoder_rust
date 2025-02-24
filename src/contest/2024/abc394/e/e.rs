7bd4ad8ecc2d209f72b66c3b1aa402809f9eb925
paruma <paruma184az@gmail.com>
2025-02-24 16:14:55 +0900

    ABC394  E - Palindromic Shortest Path  を AC

--- src/contest/2024/abc394/e/e.rs
+++ src/contest/2024/abc394/e/e.rs
@@ -2,23 +2,141 @@
 #[derive(Debug, Clone)]
 struct Problem {
     n: usize,
-    xs: Vec<i64>,
+    adj_matrix: Vec<Vec<char>>,
+}
+
+#[allow(clippy::module_inception)]
+#[macro_use]
+pub mod chminmax {
+    #[allow(unused_macros)]
+    #[macro_export]
+    macro_rules! chmin {
+        ($ a : expr , $ b : expr ) => {
+            if $a > $b {
+                $a = $b;
+                true
+            } else {
+                false
+            }
+        };
+    }
+    #[allow(unused_macros)]
+    #[macro_export]
+    macro_rules! chmax {
+        ($ a : expr , $ b : expr ) => {
+            if $a < $b {
+                $a = $b;
+                true
+            } else {
+                false
+            }
+        };
+    }
 }
 
 impl Problem {
     fn read() -> Problem {
         input! {
             n: usize,
-            xs: [i64; n],
+            adj_matrix: [Chars; n],
         }
-        Problem { n, xs }
+        Problem { n, adj_matrix }
     }
 
     fn solve(&self) -> Answer {
-        let ans = 0;
+        // パスの端点に辺を付け加えてパスを伸ばしていくイメージのダイクストラ法
+        // 計算量は O(n^4 log n)
+
+        let n = self.n;
+        let adj_matrix = &self.adj_matrix;
+
+        // dp[from][to]: from から to までの最短回文路
+        let mut dp = vec![vec![INF; n]; n];
+
+        let mut open: BinaryHeap<(Reverse<ExtInt>, usize, usize)> = BinaryHeap::new();
+        for from in 0..n {
+            for to in 0..n {
+                if from == to {
+                    dp[from][to] = fin(0);
+                    open.push((Reverse(dp[from][to]), from, to));
+                } else {
+                    if adj_matrix[from][to] != '-' {
+                        dp[from][to] = fin(1);
+                        open.push((Reverse(dp[from][to]), from, to));
+                    }
+                }
+            }
+        }
+
+        while let Some((Reverse(cur_dist), cur_from, cur_to)) = open.pop() {
+            if dp[cur_from][cur_to] < cur_dist {
+                panic!();
+            }
+            for next_from in 0..n {
+                for next_to in 0..n {
+                    // next_from → cur_from → cur_to → next_to と考える
+                    if adj_matrix[next_from][cur_from] != '-'
+                        && adj_matrix[next_from][cur_from] == adj_matrix[cur_to][next_to]
+                    {
+                        if chmin!(dp[next_from][next_to], dp[cur_from][cur_to] + 2) {
+                            open.push((Reverse(dp[next_from][next_to]), next_from, next_to));
+                        }
+                    }
+                }
+            }
+        }
+
+        let ans = dp;
         Answer { ans }
     }
 
+    fn solve2(&self) -> Answer {
+        // パスの端点に辺を付け加えてパスを伸ばしていくイメージのBFS
+        // 計算量は O(n^4)
+
+        let n = self.n;
+        let adj_matrix = &self.adj_matrix;
+
+        // dp[from][to]: from から to までの最短回文路
+        let mut dp = vec![vec![INF; n]; n];
+        let mut visited = vec![vec![false; n]; n];
+
+        let mut open: Queue<(usize, usize)> = Queue::new();
+        // 初期値として距離 0 のパスを先頭に加えて、その後に距離 1 のパスを加える。
+        for i in 0..n {
+            dp[i][i] = fin(0);
+            open.push((i, i));
+            visited[i][i] = true;
+        }
+        for from in 0..n {
+            for to in 0..n {
+                if from != to && adj_matrix[from][to] != '-' {
+                    dp[from][to] = fin(1);
+                    open.push((from, to));
+                    visited[from][to] = true;
+                }
+            }
+        }
+
+        while let Some((cur_from, cur_to)) = open.pop() {
+            for next_from in 0..n {
+                for next_to in 0..n {
+                    // next_from → cur_from → cur_to → next_to と考える
+                    if !visited[next_from][next_to]
+                        && adj_matrix[next_from][cur_from] != '-'
+                        && adj_matrix[next_from][cur_from] == adj_matrix[cur_to][next_to]
+                    {
+                        dp[next_from][next_to] = dp[cur_from][cur_to] + 2;
+                        visited[next_from][next_to] = true;
+                        open.push((next_from, next_to));
+                    }
+                }
+            }
+        }
+
+        let ans = dp;
+        Answer { ans }
+    }
     #[allow(dead_code)]
     fn solve_naive(&self) -> Answer {
         todo!();
@@ -29,17 +147,22 @@ impl Problem {
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 struct Answer {
-    ans: i64,
+    ans: Vec<Vec<ExtInt>>,
 }
 
 impl Answer {
     fn print(&self) {
-        println!("{}", self.ans);
+        let msg = self
+            .ans
+            .iter()
+            .map(|row| row.iter().copied().map(|x| x.get_fin_or(-1)).collect_vec())
+            .collect_vec();
+        print_vec2(&msg);
     }
 }
 
 fn main() {
-    Problem::read().solve().print();
+    Problem::read().solve2().print();
 }
 
 #[cfg(test)]
@@ -180,3 +303,202 @@ fn print_yesno(ans: bool) {
 }
 
 // ====== snippet ======
+use mod_ext_int::*;
+pub mod mod_ext_int {
+    use ac_library::Monoid;
+    use std::{
+        cmp::Ordering,
+        convert::Infallible,
+        fmt,
+        ops::{Add, AddAssign, Sub, SubAssign},
+    };
+    pub const INF: ExtInt = ExtInt::INF;
+    pub fn fin(x: i64) -> ExtInt {
+        ExtInt::fin(x)
+    }
+    #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct ExtInt(i64);
+    impl ExtInt {
+        pub const INF: Self = Self(i64::MAX);
+        pub fn fin(x: i64) -> Self {
+            Self(x)
+        }
+        pub fn get_fin(self) -> i64 {
+            if self.is_fin() {
+                self.0
+            } else {
+                panic!("called `ExtInt::get_fin()` on a infinity")
+            }
+        }
+        pub fn get_fin_or(self, default: i64) -> i64 {
+            if self.is_fin() {
+                self.0
+            } else {
+                default
+            }
+        }
+        #[inline]
+        pub fn is_fin(self) -> bool {
+            self.0 != i64::MAX
+        }
+        pub fn is_inf(self) -> bool {
+            self.0 == i64::MAX
+        }
+        pub fn to_option(self) -> Option<i64> {
+            if self.is_fin() {
+                Some(self.0)
+            } else {
+                None
+            }
+        }
+        pub fn from_option(opt: Option<i64>) -> ExtInt {
+            match opt {
+                Some(a) => Self(a),
+                None => Self::INF,
+            }
+        }
+        pub fn times(self, t: i64) -> Self {
+            match t.cmp(&0) {
+                Ordering::Less => panic!("t must be non-negative."),
+                Ordering::Equal => Self(0),
+                Ordering::Greater => {
+                    if self.is_fin() {
+                        Self(self.0 * t)
+                    } else {
+                        Self::INF
+                    }
+                }
+            }
+        }
+    }
+    impl Add for ExtInt {
+        type Output = ExtInt;
+        fn add(self, rhs: Self) -> Self::Output {
+            if self.is_inf() || rhs.is_inf() {
+                Self::INF
+            } else {
+                Self::fin(self.0 + rhs.0)
+            }
+        }
+    }
+    impl AddAssign for ExtInt {
+        fn add_assign(&mut self, rhs: Self) {
+            *self = *self + rhs;
+        }
+    }
+    impl Add<i64> for ExtInt {
+        type Output = ExtInt;
+        fn add(self, rhs: i64) -> Self::Output {
+            if self.is_inf() {
+                Self::INF
+            } else {
+                Self::fin(self.0 + rhs)
+            }
+        }
+    }
+    impl AddAssign<i64> for ExtInt {
+        fn add_assign(&mut self, rhs: i64) {
+            *self = *self + rhs;
+        }
+    }
+    impl Sub<i64> for ExtInt {
+        type Output = ExtInt;
+        fn sub(self, rhs: i64) -> Self::Output {
+            if self.is_inf() {
+                Self::INF
+            } else {
+                Self::fin(self.0 - rhs)
+            }
+        }
+    }
+    impl SubAssign<i64> for ExtInt {
+        fn sub_assign(&mut self, rhs: i64) {
+            *self = *self - rhs;
+        }
+    }
+    impl std::iter::Sum for ExtInt {
+        fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+            let mut s = 0;
+            for x in iter {
+                if x.is_inf() {
+                    return Self::INF;
+                }
+                s += x.0;
+            }
+            Self::fin(s)
+        }
+    }
+    impl fmt::Display for ExtInt {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            if self.is_inf() {
+                write!(f, "+∞")
+            } else {
+                write!(f, "{}", self.0)
+            }
+        }
+    }
+    impl fmt::Debug for ExtInt {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            if self.is_inf() {
+                write!(f, "+∞")
+            } else {
+                write!(f, "{}", self.0)
+            }
+        }
+    }
+    pub struct ExtIntAdditive(Infallible);
+    impl Monoid for ExtIntAdditive {
+        type S = ExtInt;
+        fn identity() -> Self::S {
+            ExtInt::fin(0)
+        }
+        fn binary_operation(a: &Self::S, b: &Self::S) -> Self::S {
+            *a + *b
+        }
+    }
+    pub struct ExtIntMin(Infallible);
+    impl Monoid for ExtIntMin {
+        type S = ExtInt;
+        fn identity() -> Self::S {
+            ExtInt::INF
+        }
+        fn binary_operation(a: &Self::S, b: &Self::S) -> Self::S {
+            *a.min(b)
+        }
+    }
+}
+use mod_queue::*;
+pub mod mod_queue {
+    use std::collections::VecDeque;
+    #[derive(Clone, Debug, PartialEq, Eq)]
+    pub struct Queue<T> {
+        raw: VecDeque<T>,
+    }
+    impl<T> Queue<T> {
+        pub fn new() -> Self {
+            Queue {
+                raw: VecDeque::new(),
+            }
+        }
+        pub fn push(&mut self, value: T) {
+            self.raw.push_back(value)
+        }
+        pub fn pop(&mut self) -> Option<T> {
+            self.raw.pop_front()
+        }
+        pub fn peek(&self) -> Option<&T> {
+            self.raw.front()
+        }
+        pub fn is_empty(&self) -> bool {
+            self.raw.is_empty()
+        }
+        pub fn len(&self) -> usize {
+            self.raw.len()
+        }
+    }
+    impl<T> Default for Queue<T> {
+        fn default() -> Self {
+            Self::new()
+        }
+    }
+}
