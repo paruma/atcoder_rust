7885d25dbd59a3b71f0dcc642f627a52b9437ff5
paruma <paruma184az@gmail.com>
2025-05-04 20:50:15 +0900

    ARC197 テンプレ変更

--- src/contest/2025/arc197/d/d.rs
+++ src/contest/2025/arc197/d/d.rs
@@ -1,10 +1,45 @@
-fn main() {
-    input! {
-        n: usize,
-        xs: [i64; n],
+//#[derive_readable]
+#[derive(Debug, Clone)]
+struct Problem {
+    n: usize,
+    xs: Vec<i64>,
+}
+
+impl Problem {
+    fn read() -> Problem {
+        input! {
+            n: usize,
+            xs: [i64; n],
+        }
+        Problem { n, xs }
     }
-    let ans: i64 = 0;
-    println!("{}", ans);
+
+    fn solve(&self) -> Answer {
+        let ans = 0;
+        Answer { ans }
+    }
+
+    #[allow(dead_code)]
+    fn solve_naive(&self) -> Answer {
+        todo!();
+        // let ans = 0;
+        // Answer { ans }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Answer {
+    ans: i64,
+}
+
+impl Answer {
+    fn print(&self) {
+        println!("{}", self.ans);
+    }
+}
+
+fn main() {
+    Problem::read().solve().print();
 }
 
 #[cfg(test)]
@@ -17,6 +52,69 @@ mod tests {
     #[test]
     fn test_problem() {
         assert_eq!(1 + 1, 2);
+    }
+
+    #[allow(dead_code)]
+    #[derive(Debug)]
+    struct WrongTestCase {
+        problem: Problem,
+        main_ans: Answer,
+        naive_ans: Answer,
+    }
+
+    #[allow(dead_code)]
+    fn check(p: &Problem) -> Option<WrongTestCase> {
+        let main_ans = p.solve();
+        let naive_ans = p.solve_naive();
+        if main_ans != naive_ans {
+            Some(WrongTestCase {
+                problem: p.clone(),
+                main_ans,
+                naive_ans,
+            })
+        } else {
+            None
+        }
+    }
+
+    #[allow(dead_code)]
+    fn make_random_problem(rng: &mut SmallRng) -> Problem {
+        todo!()
+        // let n = rng.gen_range(1..=10);
+        // let p = Problem { _a: n };
+        // println!("{:?}", &p);
+        // p
+    }
+
+    #[allow(unreachable_code)]
+    #[test]
+    fn test_with_naive() {
+        let num_tests = 0;
+        let max_wrong_case = 10; // この件数間違いが見つかったら打ち切り
+        let mut rng = SmallRng::seed_from_u64(42);
+        // let mut rng = SmallRng::from_entropy();
+        let mut wrong_cases: Vec<WrongTestCase> = vec![];
+        for _ in 0..num_tests {
+            let p = make_random_problem(&mut rng);
+            let result = check(&p);
+            if let Some(wrong_test_case) = result {
+                wrong_cases.push(wrong_test_case);
+            }
+            if wrong_cases.len() >= max_wrong_case {
+                break;
+            }
+        }
+
+        if !wrong_cases.is_empty() {
+            for t in &wrong_cases {
+                println!("{:?}", t.problem);
+                println!("main ans : {:?}", t.main_ans);
+                println!("naive ans: {:?}", t.naive_ans);
+                println!();
+            }
+            println!("{} cases are wrong.", wrong_cases.len());
+            panic!();
+        }
     }
 }
 
