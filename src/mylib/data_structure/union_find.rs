a34f50e49c348a9c2d1f3032850359a55a18bb5f
paruma <paruma184az@gmail.com>
2025-08-03 20:16:01 +0900

    typo 修正

--- src/mylib/data_structure/union_find.rs
+++ src/mylib/data_structure/union_find.rs
@@ -2,5 +2,5 @@ pub mod enumerable_union_find;
 pub mod grid_union_find;
 pub mod monoid_union_find;
 pub mod potentialized_union_find;
-pub mod root_traking_union_find;
+pub mod root_tracking_union_find;
 pub mod simple_union_find;
