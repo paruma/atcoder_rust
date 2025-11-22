68479afe42c30e42f0999dc277a1ce842f19d544
paruma <paruma184az@gmail.com>
2025-11-22 10:10:53 +0900

    ABC432 E

--- src/contest/2025/abc432/e/e.rs
+++ src/contest/2025/abc432/e/e.rs
@@ -78,11 +78,11 @@ fn main() {
             }
             Query::Sum { l, r } => {
                 let ans = if l > r {
-                    cnts.sum(..) * (l as i64)
+                    cnts.range_sum(..) * (l as i64)
                 } else {
-                    let term1 = cnts.sum(0..l) * (l as i64);
-                    let term2 = vals.sum(l..r);
-                    let term3 = cnts.sum(r..) * (r as i64);
+                    let term1 = cnts.range_sum(0..l) * (l as i64);
+                    let term2 = vals.range_sum(l..r);
+                    let term3 = cnts.range_sum(r..) * (r as i64);
                     term1 + term2 + term3
                 };
                 println!("{}", ans);
@@ -149,7 +149,6 @@ mod tests {
     }
 }
 
-use ac_library::{Additive, FenwickTree, Segtree};
 // ====== import ======
 #[allow(unused_imports)]
 use {
@@ -218,293 +217,152 @@ pub mod print_util {
 }
 
 // ====== snippet ======
-pub fn fenwick_tree_to_vec<T>(fenwick_tree: &ac_library::FenwickTree<T>, len: usize) -> Vec<T>
-where
-    T: Clone + std::ops::AddAssign<T> + std::ops::Sub<Output = T>,
-{
-    (0..len).map(|i| fenwick_tree.sum(i..=i)).collect()
-}
-use lg::*;
-pub mod lg {
-    use std::borrow::Borrow;
-    use std::fmt;
-    use std::iter::once;
-    /// Print the values with the line number.
-    /// # Examples
-    /// ```rust
-    /// # use lg::*;
-    /// let x = 42;
-    /// let y = 43;
-    /// lg!(x);
-    /// lg!(x, y);
-    /// lg!(42, x, 43, y);
-    /// ```
-    #[macro_export]
-    macro_rules ! lg {(@ contents $ head : expr_2021 $ (, $ tail : expr_2021 ) * ) => {{$ crate :: __lg_internal ! ($ head ) ; $ (eprint ! ("," ) ; $ crate :: __lg_internal ! ($ tail ) ; ) * eprintln ! () ; } } ; ($ ($ expr : expr_2021 ) ,* $ (, ) ? ) => {{eprint ! ("{}\u{276f}" , line ! () ) ; $ crate :: lg ! (@ contents $ ($ expr ) ,* ) } } ; }
-    #[doc(hidden)]
-    #[macro_export]
-    macro_rules! __lg_internal {
-        ($ value : expr_2021 ) => {{
-            match $value {
-                head => {
-                    eprint!(
-                        " {} = {}",
-                        stringify!($value),
-                        $crate::__quiet(format!("{:?}", &head))
-                    );
+use fenwick_tree::*;
+#[allow(clippy::module_inception)]
+pub mod fenwick_tree {
+    use std::ops::{Bound, RangeBounds};
+    #[derive(Clone, Debug)]
+    pub struct FenwickTree<T> {
+        n: usize,
+        ary: Vec<T>,
+        e: T,
+    }
+    impl<T: Clone + std::ops::AddAssign<T>> FenwickTree<T> {
+        /// サイズ `n` の `FenwickTree` を作成します。
+        /// # 計算量
+        /// O(N)
+        pub fn new(n: usize, e: T) -> Self {
+            FenwickTree {
+                n,
+                ary: vec![e.clone(); n],
+                e,
+            }
+        }
+        /// スライスから `FenwickTree` を作成します。
+        /// # 計算量
+        /// O(N)
+        pub fn from_slice(slice: &[T], e: T) -> Self {
+            let n = slice.len();
+            let mut ary = slice.to_vec();
+            for i in 0..n {
+                let j = i | (i + 1);
+                if j < n {
+                    let val_i = ary[i].clone();
+                    ary[j] += val_i;
                 }
             }
-        }};
-    }
-    /// Print many 1D arrays side-by-side with the line number.
-    /// # Examples
-    /// ```rust
-    /// # use lg::*;
-    /// let a = [1, 2, 3];
-    /// let b = [4, 5, 6];
-    /// let c = [7, 8, 9];
-    /// rows! {
-    ///   "id", // the name of the index
-    ///   @"a" => a,
-    ///   b,
-    ///   @"c" => c,
-    /// }
-    /// ```
-    #[macro_export]
-    macro_rules ! rows {{$ index_label : literal , $ (@ offset $ offset : expr_2021 , ) ? $ (@ verticalbar $ verticalbar : expr_2021 , ) * $ ($ (@$ label : literal => ) ? $ values : expr_2021 ) ,* $ (, ) ? } => {{#! [allow (unused_assignments ) ] let mut rows = $ crate :: Rows :: default () ; rows . line_number (line ! () ) ; $ (rows . offset ($ offset ) ; ) ? $ (rows . verticalbar ($ verticalbar ) ; ) * rows . index_label ($ index_label ) ; $ ({let mut label = stringify ! ($ values ) . to_string () ; if label . starts_with ("&" ) {label = label [1 .. ] . to_string () ; } $ ({let label_ : &'static str = $ label ; label = label_ . to_string () ; } ) ? rows . row (label , $ values ) ; } ) * eprintln ! ("{}" , rows . to_string_table () ) ; } } ; }
-    /// Print the 2D array with the line number.
-    /// # Examples
-    /// ```rust
-    /// # use lg::*;
-    /// let a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-    /// table! {
-    ///    @"a" => a,
-    /// }
-    /// table! {
-    ///   a,
-    /// }
-    /// ```
-    #[macro_export]
-    macro_rules ! table {{$ (@$ name : literal => ) ? $ values : expr_2021 $ (, ) ? } => {{#! [allow (unused_assignments ) ] let mut name = stringify ! ($ values ) . to_string () ; if name . starts_with ("&" ) {name = name [1 .. ] . to_string () ; } $ ({let name_ : &'static str = $ name ; name = name_ . to_string () ; } ) ? let mut rows = $ crate :: Rows :: default () ; rows . line_number (line ! () ) ; rows . table_name (name ) ; # [allow (array_into_iter ) ] for (i , row ) in $ values . into_iter () . enumerate () {rows . row (i . to_string () , row ) ; } eprintln ! ("{}" , rows . to_string_table () ) ; } } ; }
-    #[doc(hidden)]
-    pub fn __quiet(s: impl AsRef<str>) -> String {
-        s.as_ref()
-            .replace("340282366920938463463374607431768211455", "*")
-            .replace("170141183460469231731687303715884105727", "*")
-            .replace("18446744073709551615", "*")
-            .replace("9223372036854775807", "*")
-            .replace("-9223372036854775808", "*")
-            .replace("4294967295", "*")
-            .replace("2147483647", "*")
-            .replace("-2147483648", "*")
-            .replace("None", "*")
-            .replace("Some", "")
-            .replace("true", "#")
-            .replace("false", ".")
-            .replace(['"', '\''], "")
-    }
-    #[doc(hidden)]
-    #[derive(Default)]
-    pub struct Rows {
-        line_number: String,
-        index_label: String,
-        offset: usize,
-        verticalbars: Vec<usize>,
-        table_name: String,
-        rows: Vec<Row>,
-    }
-    impl Rows {
-        pub fn line_number(&mut self, line_number: u32) -> &mut Self {
-            self.line_number = format!("{}", line_number);
-            self
+            FenwickTree { n, ary, e }
         }
-        pub fn index_label(&mut self, index_label: impl Into<String>) -> &mut Self {
-            self.index_label = index_label.into();
-            self
-        }
-        pub fn offset(&mut self, offset: usize) -> &mut Self {
-            self.offset = offset;
-            self
-        }
-        pub fn verticalbar(&mut self, verticalbar: impl IntoIterator<Item = usize>) -> &mut Self {
-            self.verticalbars.extend(verticalbar);
-            self
-        }
-        pub fn table_name(&mut self, table_name: impl Into<String>) -> &mut Self {
-            self.table_name = table_name.into();
-            self
-        }
-        pub fn row(
-            &mut self,
-            label: impl Into<String>,
-            values: impl IntoIterator<Item = impl fmt::Debug>,
-        ) -> &mut Self {
-            self.rows.push(Row {
-                label: label.into(),
-                values: values
-                    .into_iter()
-                    .map(|value| __quiet(format!("{:?}", value)))
-                    .collect(),
-            });
-            self
-        }
-        pub fn to_string_table(self) -> StringTable {
-            let Self {
-                line_number,
-                index_label,
-                offset,
-                verticalbars,
-                table_name,
-                rows,
-            } = self;
-            let w = rows
-                .iter()
-                .map(|row| row.values.len())
-                .max()
-                .unwrap_or_default();
-            let mut verticalbar_count = vec![0; w + 1];
-            for &v in &verticalbars {
-                if (offset..=offset + w).contains(&v) {
-                    verticalbar_count[v - offset] += 1;
-                }
+        /// `[0, idx)` の区間の累積和を計算します。
+        /// # 計算量
+        /// O(log N)
+        pub fn accum(&self, mut idx: usize) -> T {
+            assert!(
+                idx <= self.n,
+                "FenwickTree::accum: index out of bounds. idx: {}, n: {}",
+                idx,
+                self.n
+            );
+            let mut sum = self.e.clone();
+            while idx > 0 {
+                sum += self.ary[idx - 1].clone();
+                idx &= idx - 1;
             }
-            StringTable {
-                head: StringRow {
-                    label: format!(
-                        "{line_number}❯ {table_name}{index_label}",
-                        index_label = if index_label.is_empty() {
-                            String::new()
-                        } else {
-                            format!("[{}]", index_label)
-                        }
-                    ),
-                    values: (offset..offset + w)
-                        .map(|index| index.to_string())
-                        .collect(),
-                },
-                body: rows
-                    .iter()
-                    .map(|row| StringRow {
-                        label: row.label.clone(),
-                        values: row.values.clone(),
-                    })
-                    .collect(),
-                verticalbar_count,
+            sum
+        }
+        /// `idx`番目の要素に`val`を加算します。
+        /// # 計算量
+        /// O(log N)
+        pub fn add<U: Clone>(&mut self, mut idx: usize, val: U)
+        where
+            T: std::ops::AddAssign<U>,
+        {
+            assert!(
+                idx < self.n,
+                "FenwickTree::add: index out of bounds. idx: {}, n: {}",
+                idx,
+                self.n
+            );
+            let n = self.n;
+            idx += 1;
+            while idx <= n {
+                self.ary[idx - 1] += val.clone();
+                idx += idx & idx.wrapping_neg();
             }
         }
-    }
-    struct Row {
-        label: String,
-        values: Vec<String>,
-    }
-    #[doc(hidden)]
-    pub struct StringTable {
-        head: StringRow,
-        body: Vec<StringRow>,
-        verticalbar_count: Vec<usize>,
-    }
-    impl fmt::Display for StringTable {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            let Self {
-                head,
-                body,
-                verticalbar_count,
-            } = self;
-            let w = body
-                .iter()
-                .map(|row| row.values.len())
-                .max()
-                .unwrap_or_default();
-            let label_width = once(head.label.chars().count())
-                .chain(body.iter().map(|row| row.label.chars().count()))
-                .max()
-                .unwrap();
-            let value_width = (0..w)
-                .map(|j| {
-                    once(j.to_string().len())
-                        .chain(
-                            body.iter()
-                                .map(|row| row.values.get(j).map_or(0, |s| s.chars().count())),
-                        )
-                        .max()
-                        .unwrap()
-                })
-                .collect::<Vec<_>>();
-            gray(f)?;
-            write!(
-                f,
-                "{}",
-                head.to_string(label_width, &value_width, verticalbar_count, true)
-            )?;
-            resetln(f)?;
-            for row in body {
-                write!(
-                    f,
-                    "{}",
-                    row.to_string(label_width, &value_width, verticalbar_count, false)
-                )?;
-                writeln!(f)?;
-            }
-            Ok(())
+        /// `[l, r)` の区間和を計算します。
+        /// # 計算量
+        /// O(log N)
+        pub fn range_sum<R>(&self, range: R) -> T
+        where
+            T: std::ops::Sub<Output = T>,
+            R: RangeBounds<usize>,
+        {
+            let r = match range.end_bound() {
+                Bound::Included(r) => r + 1,
+                Bound::Excluded(r) => *r,
+                Bound::Unbounded => self.n,
+            };
+            let l = match range.start_bound() {
+                Bound::Included(l) => *l,
+                Bound::Excluded(l) => l + 1,
+                Bound::Unbounded => return self.accum(r),
+            };
+            assert!(
+                l <= r && r <= self.n,
+                "FenwickTree::range_sum: invalid range. l: {}, r: {}, n: {}",
+                l,
+                r,
+                self.n
+            );
+            self.accum(r) - self.accum(l)
         }
-    }
-    struct StringRow {
-        label: String,
-        values: Vec<String>,
-    }
-    impl StringRow {
-        fn to_string(
-            &self,
-            label_width: usize,
-            value_width: &[usize],
-            varticalbars_count: &[usize],
-            label_align_left: bool,
-        ) -> String {
-            let Self { label, values } = self;
-            let w = value_width.len();
-            let mut s = String::new();
-            s.push_str(&if label_align_left {
-                format!("{label:<label_width$} |")
-            } else {
-                format!("{label:^label_width$} |")
-            });
-            for j in 0..w {
-                let value_width = value_width[j];
-                s.push_str("|".repeat(varticalbars_count[j]).as_str());
-                if varticalbars_count[j] == 0 && j != 0 && value_width <= 1 {
-                    s.push(' ');
-                }
-                match values.get(j) {
-                    Some(value) => {
-                        s.push_str(&format!(" {value:>value_width$}",));
-                    }
-                    None => {
-                        s.push_str(" ".repeat(value_width + 1).as_str());
-                    }
-                }
-            }
-            s
+        /// `idx`番目の要素の値を取得します。
+        /// # 計算量
+        /// O(log N)
+        pub fn get(&self, idx: usize) -> T
+        where
+            T: std::ops::Sub<Output = T>,
+        {
+            assert!(
+                idx < self.n,
+                "FenwickTree::get: index out of bounds. idx: {}, n: {}",
+                idx,
+                self.n
+            );
+            self.range_sum(idx..=idx)
         }
-    }
-    const GRAY: &str = "\x1b[48;2;127;127;127;37m";
-    const RESET: &str = "\x1b[0m";
-    fn gray(f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{GRAY}")
-    }
-    fn resetln(f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "{RESET}")
-    }
-    /// Format a iterator of [`bool`]s.
-    pub fn bools<B, I>(iter: I) -> String
-    where
-        B: Borrow<bool>,
-        I: IntoIterator<Item = B>,
-    {
-        format!(
-            "[{}]",
-            iter.into_iter()
-                .map(|b| ['.', '#'][usize::from(*(b.borrow()))])
-                .collect::<String>(),
-        )
+        /// `idx`番目の要素の値を`val`に設定します。
+        /// # 計算量
+        /// O(log N)
+        pub fn set(&mut self, idx: usize, val: T)
+        where
+            T: std::ops::Sub<Output = T>,
+        {
+            assert!(
+                idx < self.n,
+                "FenwickTree::set: index out of bounds. idx: {}, n: {}",
+                idx,
+                self.n
+            );
+            let old_val = self.get(idx);
+            self.add(idx, val - old_val);
+        }
+        /// Fenwick Treeの現在の状態を`Vec<T>`として返します。
+        /// # 計算量
+        /// O(N log N)
+        pub fn to_vec(&self) -> Vec<T>
+        where
+            T: std::ops::Sub<Output = T>,
+        {
+            (0..self.n).map(|i| self.get(i)).collect()
+        }
+        /// Fenwick Treeが保持している要素の数を返します。
+        /// # 計算量
+        /// O(1)
+        #[allow(clippy::len_without_is_empty)]
+        pub fn len(&self) -> usize {
+            self.n
+        }
     }
 }
