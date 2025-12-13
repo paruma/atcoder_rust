3a08bf55c8cceb6fadcee309f07d6fd15e9a8490
paruma <paruma184az@gmail.com>
2025-12-13 20:12:45 +0900

    ABC436 準備

---
+++ src/contest/2025/abc436/c/c.rs
@@ -0,0 +1,136 @@
+// #[fastout]
+fn main() {
+    input! {
+        n: usize,
+        xs: [i64; n],
+    }
+    let ans: i64 = -2_i64;
+    println!("{}", ans);
+}
+
+#[cfg(test)]
+mod tests {
+    #[allow(unused_imports)]
+    use super::*;
+    #[allow(unused_imports)]
+    use rand::{rngs::SmallRng, seq::SliceRandom, *};
+
+    #[test]
+    fn test_problem() {
+        assert_eq!(1 + 1, 2);
+    }
+
+    /// 間違っていたら false を返す
+    fn process_one_test(rng: &mut SmallRng) -> bool {
+        // ==== 問題を作る ====
+        let n = rng.random_range(1..=10);
+        let xs = (0..n).map(|_| rng.random_range(0..10)).collect_vec();
+
+        // ==== 解く ====
+        let main_ans = xs.len();
+        let naive_ans = 1;
+
+        // ==== 間違っていたら報告をする ====
+        if main_ans != naive_ans {
+            // 問題を出力
+            println!("{:?}", (n, xs));
+            println!("main ans : {:?}", main_ans);
+            println!("naive ans: {:?}", naive_ans);
+            return false;
+        }
+        true
+    }
+
+    #[allow(unreachable_code)]
+    #[test]
+    #[ignore]
+    fn test_with_naive() {
+        let num_tests = 100;
+        let max_wrong_case = 10; // この件数間違いが見つかったら打ち切り
+        let mut cnt_wrong = 0;
+        let mut rng = SmallRng::seed_from_u64(42);
+        // let mut rng = SmallRng::from_os_rng();
+        for _ in 0..num_tests {
+            let is_ok = process_one_test(&mut rng);
+            if !is_ok {
+                cnt_wrong += 1;
+            }
+            if cnt_wrong >= max_wrong_case {
+                break;
+            }
+        }
+        if cnt_wrong > 0 {
+            println!("{} cases are wrong.", cnt_wrong);
+            panic!();
+        }
+    }
+}
+
+// ====== import ======
+#[allow(unused_imports)]
+use {
+    itertools::{Itertools, chain, iproduct, izip},
+    proconio::{
+        derive_readable, fastout, input,
+        marker::{Bytes, Chars, Usize1},
+    },
+    rand::{Rng, SeedableRng, rngs::SmallRng, seq::SliceRandom},
+    std::{
+        cmp::Reverse,
+        collections::{BinaryHeap, HashMap, HashSet},
+    },
+};
+
+// ====== output func ======
+#[allow(unused_imports)]
+use print_util::*;
+pub mod print_util {
+    use itertools::Itertools;
+    use proconio::fastout;
+    #[fastout]
+    pub fn print_vec<T: std::fmt::Display>(arr: &[T]) {
+        for a in arr {
+            println!("{}", a);
+        }
+    }
+    #[fastout]
+    pub fn print_vec_1line<T: std::fmt::Display>(arr: &[T]) {
+        let msg = arr.iter().map(|x| format!("{}", x)).join(" ");
+        println!("{}", msg);
+    }
+    #[fastout]
+    pub fn print_vec2<T: std::fmt::Display>(arr: &Vec<Vec<T>>) {
+        for row in arr {
+            let msg = row.iter().map(|x| format!("{}", x)).join(" ");
+            println!("{}", msg);
+        }
+    }
+    pub fn print_bytes(bytes: &[u8]) {
+        let msg = String::from_utf8(bytes.to_vec()).unwrap();
+        println!("{}", msg);
+    }
+    pub fn print_chars(chars: &[char]) {
+        let msg = chars.iter().collect::<String>();
+        println!("{}", msg);
+    }
+    #[fastout]
+    pub fn print_vec_bytes(vec_bytes: &[Vec<u8>]) {
+        for row in vec_bytes {
+            let msg = String::from_utf8(row.to_vec()).unwrap();
+            println!("{}", msg);
+        }
+    }
+    #[fastout]
+    pub fn print_vec_chars(vec_chars: &[Vec<char>]) {
+        for row in vec_chars {
+            let msg = row.iter().collect::<String>();
+            println!("{}", msg);
+        }
+    }
+    pub fn print_yesno(ans: bool) {
+        let msg = if ans { "Yes" } else { "No" };
+        println!("{}", msg);
+    }
+}
+
+// ====== snippet ======
