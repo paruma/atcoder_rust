243d79f4f882df78d8e7413f743c2481b8171250
paruma <paruma184az@gmail.com>
2026-01-04 12:48:04 +0900

    ABC439

---
+++ src/contest/2026/abc439/b/b.rs
@@ -0,0 +1,245 @@
+// #[fastout]
+fn solve1(mut n: i64) -> bool {
+    // 嘘解法ぽい
+    while n >= 10 {
+        n = to_base_n_value_iter(n, 10).map(|x| x * x).sum();
+    }
+
+    n == 1
+}
+
+fn solve2(mut n: i64) -> bool {
+    for _ in 0..1000 {
+        n = to_base_n_value_iter(n, 10).map(|x| x * x).sum();
+        dbg!(n);
+        if n == 1 {
+            return true;
+        }
+    }
+    false
+}
+fn main() {
+    input! {
+        n: i64,
+    }
+
+    // for i in 1..=200 {
+    //     assert_eq!(solve1(i), solve2(i), "{}", i);
+    // }
+
+    let ans: bool = solve1(n);
+    assert_eq!(solve1(n), solve2(n));
+    print_yesno(ans);
+}
+
+#[cfg(test)]
+mod tests {
+    #[allow(unused_imports)]
+    use super::*;
+    #[allow(unused_imports)]
+    use rand::{rngs::SmallRng, seq::SliceRandom, *};
+
+    #[test]
+    fn test_problem() {
+        assert_eq!(1 + 1, 2);
+    }
+
+    /// 間違っていたら false を返す
+    fn process_one_test(rng: &mut SmallRng) -> bool {
+        // ==== 問題を作る ====
+        let n = rng.random_range(1..=10);
+        let xs = (0..n).map(|_| rng.random_range(0..10)).collect_vec();
+
+        // ==== 解く ====
+        let main_ans = xs.len();
+        let naive_ans = 1;
+
+        // ==== 間違っていたら報告をする ====
+        if main_ans != naive_ans {
+            // 問題を出力
+            println!("{:?}", (n, xs));
+            println!("main ans : {:?}", main_ans);
+            println!("naive ans: {:?}", naive_ans);
+            return false;
+        }
+        true
+    }
+
+    #[allow(unreachable_code)]
+    #[test]
+    #[ignore]
+    fn test_with_naive() {
+        let num_tests = 100;
+        let max_wrong_case = 10; // この件数間違いが見つかったら打ち切り
+        let mut cnt_wrong = 0;
+        let mut rng = SmallRng::seed_from_u64(42);
+        // let mut rng = SmallRng::from_os_rng();
+        for _ in 0..num_tests {
+            let is_ok = process_one_test(&mut rng);
+            if !is_ok {
+                cnt_wrong += 1;
+            }
+            if cnt_wrong >= max_wrong_case {
+                break;
+            }
+        }
+        if cnt_wrong > 0 {
+            println!("{} cases are wrong.", cnt_wrong);
+            panic!();
+        }
+    }
+}
+
+// ====== import ======
+#[allow(unused_imports)]
+use {
+    itertools::{Itertools, chain, iproduct, izip},
+    proconio::{
+        derive_readable, fastout, input,
+        marker::{Bytes, Chars, Usize1},
+    },
+    rand::{Rng, SeedableRng, rngs::SmallRng, seq::SliceRandom},
+    std::{
+        cmp::Reverse,
+        collections::{BinaryHeap, HashMap, HashSet},
+    },
+};
+
+// ====== output func ======
+#[allow(unused_imports)]
+use print_util::*;
+pub mod print_util {
+    use itertools::Itertools;
+    use proconio::fastout;
+    #[fastout]
+    pub fn print_vec<T: std::fmt::Display>(arr: &[T]) {
+        for a in arr {
+            println!("{}", a);
+        }
+    }
+    #[fastout]
+    pub fn print_vec_1line<T: std::fmt::Display>(arr: &[T]) {
+        let msg = arr.iter().map(|x| format!("{}", x)).join(" ");
+        println!("{}", msg);
+    }
+    #[fastout]
+    pub fn print_vec2<T: std::fmt::Display>(arr: &Vec<Vec<T>>) {
+        for row in arr {
+            let msg = row.iter().map(|x| format!("{}", x)).join(" ");
+            println!("{}", msg);
+        }
+    }
+    pub fn print_bytes(bytes: &[u8]) {
+        let msg = String::from_utf8(bytes.to_vec()).unwrap();
+        println!("{}", msg);
+    }
+    pub fn print_chars(chars: &[char]) {
+        let msg = chars.iter().collect::<String>();
+        println!("{}", msg);
+    }
+    #[fastout]
+    pub fn print_vec_bytes(vec_bytes: &[Vec<u8>]) {
+        for row in vec_bytes {
+            let msg = String::from_utf8(row.to_vec()).unwrap();
+            println!("{}", msg);
+        }
+    }
+    #[fastout]
+    pub fn print_vec_chars(vec_chars: &[Vec<char>]) {
+        for row in vec_chars {
+            let msg = row.iter().collect::<String>();
+            println!("{}", msg);
+        }
+    }
+    pub fn print_yesno(ans: bool) {
+        let msg = if ans { "Yes" } else { "No" };
+        println!("{}", msg);
+    }
+}
+
+// ====== snippet ======
+use positional_notation::*;
+#[allow(clippy::module_inception)]
+pub mod positional_notation {
+    /// 配列 xs で表された base 進数の値を評価する。
+    /// 例: `eval_base_n_value(&[1, 2, 3], 10) == 123`
+    pub fn eval_base_n_value(xs: &[i64], base: i64) -> i64 {
+        xs.iter().fold(0, |acc, &x| acc * base + x)
+    }
+    /// x の base 進数での表記を Vec で表す。
+    /// 例
+    /// * `to_base_n_value(123, 10) == vec![1, 2, 3]`
+    /// * `to_base_n_value(0, 10) == vec![]`
+    pub fn to_base_n_value(x: i64, base: i64) -> Vec<i64> {
+        assert!(x >= 0);
+        assert!(base >= 2);
+        let mut ret = vec![];
+        let mut x = x;
+        while x > 0 {
+            ret.push(x % base);
+            x /= base;
+        }
+        ret.reverse();
+        ret
+    }
+    /// x を base 進数で表記した際の桁数を返す。
+    /// 例
+    /// * `count_digits(123, 10) == 3`
+    /// * `count_digits(0, 10) == 0`
+    pub fn count_digits(mut x: i64, base: i64) -> usize {
+        assert!(x >= 0);
+        assert!(base >= 2);
+        if x == 0 {
+            return 0;
+        }
+        let mut count = 0;
+        while x > 0 {
+            x /= base;
+            count += 1;
+        }
+        count
+    }
+    /// `to_base_n_value_iter` 関数が返すイテレータ。
+    /// 数値を指定された基数で表した際の各桁を順に生成する。
+    pub struct BaseNIterator {
+        n: i64,
+        base: i64,
+        current_power: i64,
+    }
+    impl Iterator for BaseNIterator {
+        type Item = i64;
+        fn next(&mut self) -> Option<Self::Item> {
+            if self.current_power == 0 {
+                return None;
+            }
+            let digit = self.n / self.current_power;
+            self.n %= self.current_power;
+            self.current_power /= self.base;
+            Some(digit)
+        }
+    }
+    /// x の base 進数での表記をイテレータで返す。
+    /// 例
+    /// - `to_base_n_value_iter(123, 10).collect() == vec![1, 2, 3]`
+    /// - `to_base_n_value_iter(0, 10).collect() == vec![]`
+    pub fn to_base_n_value_iter(x: i64, base: i64) -> BaseNIterator {
+        assert!(x >= 0);
+        assert!(base >= 2);
+        if x == 0 {
+            return BaseNIterator {
+                n: 0,
+                base,
+                current_power: 0,
+            };
+        }
+        let mut current_power = 1;
+        while x / current_power >= base {
+            current_power *= base;
+        }
+        BaseNIterator {
+            n: x,
+            base,
+            current_power,
+        }
+    }
+}
---
+++ src/contest/2026/abc439/e/e.rs
@@ -0,0 +1,221 @@
+// #[fastout]
+fn main() {
+    input! {
+        n: usize,
+        mut abs: [(i64, i64); n],
+    }
+    let neg_inf = -1_000_000_000_000_i64;
+
+    // 番兵
+    abs.insert(0, (neg_inf, neg_inf));
+    let n = n + 1;
+
+    let a_list = abs.iter().copied().map(|(a, _b)| a).collect_vec();
+    let b_list = abs.iter().copied().map(|(_a, b)| b).collect_vec();
+
+    let cc_a = CoordinateCompression::new(&a_list);
+    let cc_b = CoordinateCompression::new(&b_list);
+
+    let abs = abs
+        .iter()
+        .copied()
+        .map(|(a, b)| (cc_a.compress(a), cc_b.compress(b)))
+        .sorted_by_key(|&(a, b)| (a, Reverse(b)))
+        .collect_vec();
+
+    let mut dp = vec![0_i64; n];
+    let mut seg_dp = Segtree::<Max<i64>>::new(cc_b.space_size());
+
+    dp[0] = 1;
+    seg_dp.set(abs[0].1, 1);
+
+    for (i, (a, b)) in abs.iter().copied().enumerate().skip(1) {
+        //seg_dp を更新するときは chmax で更新する
+
+        dp[i] = seg_dp.prod(..b) + 1;
+        let seg_dp_b = seg_dp.get(b);
+        if seg_dp_b < dp[i] {
+            seg_dp.set(b, dp[i])
+        }
+    }
+
+    let ans: i64 = dp.iter().copied().max().unwrap() - 1;
+    println!("{}", ans);
+}
+
+#[cfg(test)]
+mod tests {
+    #[allow(unused_imports)]
+    use super::*;
+    #[allow(unused_imports)]
+    use rand::{rngs::SmallRng, seq::SliceRandom, *};
+
+    #[test]
+    fn test_problem() {
+        assert_eq!(1 + 1, 2);
+    }
+
+    /// 間違っていたら false を返す
+    fn process_one_test(rng: &mut SmallRng) -> bool {
+        // ==== 問題を作る ====
+        let n = rng.random_range(1..=10);
+        let xs = (0..n).map(|_| rng.random_range(0..10)).collect_vec();
+
+        // ==== 解く ====
+        let main_ans = xs.len();
+        let naive_ans = 1;
+
+        // ==== 間違っていたら報告をする ====
+        if main_ans != naive_ans {
+            // 問題を出力
+            println!("{:?}", (n, xs));
+            println!("main ans : {:?}", main_ans);
+            println!("naive ans: {:?}", naive_ans);
+            return false;
+        }
+        true
+    }
+
+    #[allow(unreachable_code)]
+    #[test]
+    #[ignore]
+    fn test_with_naive() {
+        let num_tests = 100;
+        let max_wrong_case = 10; // この件数間違いが見つかったら打ち切り
+        let mut cnt_wrong = 0;
+        let mut rng = SmallRng::seed_from_u64(42);
+        // let mut rng = SmallRng::from_os_rng();
+        for _ in 0..num_tests {
+            let is_ok = process_one_test(&mut rng);
+            if !is_ok {
+                cnt_wrong += 1;
+            }
+            if cnt_wrong >= max_wrong_case {
+                break;
+            }
+        }
+        if cnt_wrong > 0 {
+            println!("{} cases are wrong.", cnt_wrong);
+            panic!();
+        }
+    }
+}
+
+// ====== import ======
+#[allow(unused_imports)]
+use {
+    itertools::{Itertools, chain, iproduct, izip},
+    proconio::{
+        derive_readable, fastout, input,
+        marker::{Bytes, Chars, Usize1},
+    },
+    rand::{Rng, SeedableRng, rngs::SmallRng, seq::SliceRandom},
+    std::{
+        cmp::Reverse,
+        collections::{BinaryHeap, HashMap, HashSet},
+    },
+};
+
+// ====== output func ======
+#[allow(unused_imports)]
+use print_util::*;
+pub mod print_util {
+    use itertools::Itertools;
+    use proconio::fastout;
+    #[fastout]
+    pub fn print_vec<T: std::fmt::Display>(arr: &[T]) {
+        for a in arr {
+            println!("{}", a);
+        }
+    }
+    #[fastout]
+    pub fn print_vec_1line<T: std::fmt::Display>(arr: &[T]) {
+        let msg = arr.iter().map(|x| format!("{}", x)).join(" ");
+        println!("{}", msg);
+    }
+    #[fastout]
+    pub fn print_vec2<T: std::fmt::Display>(arr: &Vec<Vec<T>>) {
+        for row in arr {
+            let msg = row.iter().map(|x| format!("{}", x)).join(" ");
+            println!("{}", msg);
+        }
+    }
+    pub fn print_bytes(bytes: &[u8]) {
+        let msg = String::from_utf8(bytes.to_vec()).unwrap();
+        println!("{}", msg);
+    }
+    pub fn print_chars(chars: &[char]) {
+        let msg = chars.iter().collect::<String>();
+        println!("{}", msg);
+    }
+    #[fastout]
+    pub fn print_vec_bytes(vec_bytes: &[Vec<u8>]) {
+        for row in vec_bytes {
+            let msg = String::from_utf8(row.to_vec()).unwrap();
+            println!("{}", msg);
+        }
+    }
+    #[fastout]
+    pub fn print_vec_chars(vec_chars: &[Vec<char>]) {
+        for row in vec_chars {
+            let msg = row.iter().collect::<String>();
+            println!("{}", msg);
+        }
+    }
+    pub fn print_yesno(ans: bool) {
+        let msg = if ans { "Yes" } else { "No" };
+        println!("{}", msg);
+    }
+}
+
+// ====== snippet ======
+use {
+    ac_library::{Max, Segtree},
+    coordinate_compression::*,
+};
+#[allow(clippy::module_inception)]
+pub mod coordinate_compression {
+    use itertools::Itertools;
+    use superslice::Ext;
+    pub struct CoordinateCompression {
+        space: Vec<i64>,
+    }
+    impl CoordinateCompression {
+        /// # 計算量
+        /// O(|space|log(|space|))
+        pub fn new(space: &[i64]) -> Self {
+            let space = space.iter().copied().sorted().dedup().collect_vec();
+            Self { space }
+        }
+        /// # 計算量
+        /// O(log(|space|))
+        pub fn compress(&self, x: i64) -> usize {
+            self.space.binary_search(&x).unwrap()
+        }
+        /// 座標圧縮前の空間のうち x 以上である最小の値を座標圧縮したものを返す
+        /// # 計算量
+        /// O(log(|space|))
+        pub fn compress_floor(&self, x: i64) -> usize {
+            self.space.upper_bound(&x) - 1
+        }
+        /// 座標圧縮前の空間のうち x 以下である最大の値を座標圧縮したものを返す
+        /// # 計算量
+        /// O(log(|space|))
+        pub fn compress_ceil(&self, x: i64) -> usize {
+            self.space.lower_bound(&x)
+        }
+        /// # 計算量
+        /// O(|xs|log(|space|))
+        pub fn compress_vec(&self, xs: &[i64]) -> Vec<usize> {
+            xs.iter().copied().map(|x| self.compress(x)).collect_vec()
+        }
+        /// # 計算量
+        /// O(1)
+        pub fn decompress(&self, i: usize) -> i64 {
+            self.space[i]
+        }
+        pub fn space_size(&self) -> usize {
+            self.space.len()
+        }
+    }
+}
