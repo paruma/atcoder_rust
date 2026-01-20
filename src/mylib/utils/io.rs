b476065d5e7afe8cce72452a11b3dd6e8126f251
paruma <paruma184az@gmail.com>
2026-01-20 23:57:35 +0900

    define_queries 導入

--- src/mylib/utils/io.rs
+++ src/mylib/utils/io.rs
@@ -1,55 +1,87 @@
 use cargo_snippet::snippet;
 
-#[snippet]
-pub mod impl_readable_for_enum {
-    /// 利用例
+#[snippet("define_queries_macro")]
+#[macro_use]
+pub mod define_queries {
+    /// クエリ形式の入力を proconio::input! で読み込める enum を定義するマクロ。
+    ///
+    /// 出典： https://zenn.dev/magurofly/articles/6ee845bd5e385e
+    ///
+    /// # 利用例
     ///
     /// ```
-    /// use mylib::impl_readable_for_enum;
-    /// use proconio::input;
+    /// use mylib::define_queries;
     /// use proconio::marker::Usize1;
-    /// #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-    /// enum Query {
-    ///     Move { p: usize, h: usize },
-    ///     Swap { h1: usize, h2: usize },
-    ///     Output { p: usize },
-    /// }
     ///
-    /// impl_readable_for_enum! {
-    ///     Query {
-    ///         1 => Move { p: Usize1, h: Usize1 },
-    ///         2 => Swap { h1: Usize1, h2: Usize1 },
-    ///         3 => Output { p: Usize1 },
+    /// define_queries! {
+    ///     #[derive(Debug, PartialEq)]
+    ///     enum Query: usize {
+    ///         1 => Add { a: i64, b: i64 },
+    ///         2 => Show { k: Usize1 },
     ///     }
     /// }
     /// ```
     #[macro_export]
-    macro_rules! impl_readable_for_enum {
-        ($enum_name:ident {
-            $($idx:literal => $variant:ident $( { $($field:ident : $ty:ty),* } )? ),* $(,)?
-        }) => {
+    macro_rules! define_queries {
+    ($( $(#[$attr:meta])* enum $enum_name:ident : $sig:ty { $( $pattern:pat => $variant:ident $( { $($name:ident : $marker:ty $(,)?),* } )? $(,)?),* } )*) => {
+        $(
+            $(#[$attr])*
+            enum $enum_name {
+                $(
+                    $variant $( {
+                        $( $name : <$marker as proconio::source::Readable>::Output ),*
+                    } )?
+                ),*
+            }
+
             impl proconio::source::Readable for $enum_name {
-                type Output = $enum_name;
-                fn read<R: std::io::BufRead, S: proconio::source::Source<R>>(source: &mut S) -> $enum_name {
-                    input! { from &mut *source, t: usize }
-                    match t {
+                type Output = Self;
+                fn read<R: std::io::BufRead, S: proconio::source::Source<R>>(source: &mut S) -> Self {
+                    #![allow(unreachable_patterns)]
+                    match <$sig as proconio::source::Readable>::read(source) {
                         $(
-                            $idx => {
-                                impl_readable_for_enum!(@read_variant source, $enum_name, $variant $( { $($field: $ty),* } )? )
-                            }
-                        ),*,
-                        _ => unreachable!(),
+                            $pattern => $enum_name::$variant $( {
+                                $( $name: <$marker as proconio::source::Readable>::read(source) ),*
+                            } )?
+                        ),*
+                        , _ => unreachable!()
                     }
                 }
             }
-        };
+        )*
+    }
+}
+}
 
-        (@read_variant $source:ident, $enum_name:ident, $variant:ident { $($field:ident : $ty:ty),* } ) => {{
-            input! { from &mut *$source, $($field: $ty),* };
-            $enum_name::$variant { $($field),* }
-        }};
-        (@read_variant $source:ident, $enum_name:ident, $variant:ident) => {{
-            $enum_name::$variant
-        }};
+#[cfg(test)]
+mod tests {
+    use proconio::input;
+    use proconio::marker::Usize1;
+    use proconio::source::once::OnceSource;
+
+    define_queries! {
+        #[derive(Debug, PartialEq)]
+        enum Query: usize {
+            1 => Add { a: i64, b: i64 },
+            2 => Show { k: Usize1 },
+            3 => Quit,
+            4 => Reset {},
+        }
+    }
+
+    #[test]
+    fn test_define_queries() {
+        let input_str = "1 10 20\n2 5\n3\n4";
+        let source = OnceSource::from(input_str);
+
+        input! {
+            from source,
+            queries: [Query; 4],
+        }
+
+        assert_eq!(queries[0], Query::Add { a: 10, b: 20 });
+        assert_eq!(queries[1], Query::Show { k: 4 });
+        assert_eq!(queries[2], Query::Quit);
+        assert_eq!(queries[3], Query::Reset {});
     }
 }
