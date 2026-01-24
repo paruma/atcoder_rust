de1c0d30476612c90e3ca774373e7368ce33bfe1
paruma <paruma184az@gmail.com>
2026-01-24 19:02:29 +0900

    dfs サンプルコード修正

--- src/example/dfs_graph.rs
+++ src/example/dfs_graph.rs
@@ -1,7 +1,5 @@
 #![allow(dead_code)]
 
-use mylib::data_structure::stack::mod_stack::Stack;
-
 struct DfsGraph<'a> {
     adj: &'a [Vec<usize>],
 }
@@ -30,34 +28,6 @@ impl DfsGraph<'_> {
         }
         // 帰りがけ
     }
-}
-
-fn dfs_by_stack(adj: &[Vec<usize>]) -> Vec<bool> {
-    enum State {
-        Pre(usize),
-        Post(usize),
-    }
-
-    let nv = adj.len();
-    let mut visited = vec![false; nv];
-    let mut open = Stack::new();
-    open.push(State::Post(0));
-    open.push(State::Pre(0));
-    while let Some(current) = open.pop() {
-        match current {
-            State::Pre(v) => {
-                visited[v] = true;
-                for &edge in &adj[v] {
-                    if !visited[edge] {
-                        open.push(State::Post(edge));
-                        open.push(State::Pre(edge));
-                    }
-                }
-            }
-            State::Post(_v) => {}
-        }
-    }
-    visited
 }
 
 fn dfs_order_by_stack(adj: &[Vec<usize>], start: usize) -> (Vec<usize>, Vec<usize>) {
@@ -113,24 +83,6 @@ mod tests {
         let adj = make_adj_from_directed(n_vertex, &edges);
         let dfs = DfsGraph::new(&adj);
         let visited = dfs.exec_init(0);
-        assert_eq!(visited[0], true);
-        assert_eq!(visited[1], true);
-        assert_eq!(visited[2], true);
-        assert_eq!(visited[3], false);
-        assert_eq!(visited[4], false);
-    }
-
-    #[allow(clippy::bool_assert_comparison)]
-    #[test]
-    fn test_dfs_by_stack() {
-        // 0 ← 1
-        // ↓ ↗
-        // 2
-        // 3 → 4
-        let n_vertex = 5;
-        let edges = [(0, 1), (1, 2), (2, 0), (3, 4)];
-        let adj = make_adj_from_directed(n_vertex, &edges);
-        let visited = dfs_by_stack(&adj);
         assert_eq!(visited[0], true);
         assert_eq!(visited[1], true);
         assert_eq!(visited[2], true);
