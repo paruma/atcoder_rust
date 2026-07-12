53603db861446daa8c28fec3ad4ffe21b7448530
paruma <paruma184az@gmail.com>
2026-07-12 21:48:29 +0900

    ABC466 C リファクタリング

--- src/contest/2026/abc466/c/c.rs
+++ src/contest/2026/abc466/c/c.rs
@@ -20,12 +20,17 @@ struct TestInteractive {
 }
 impl IInteractive for TestInteractive {
     fn ask(&mut self, i: usize, j: usize) -> bool {
+        // 質問が正当かどうかを確認すること
+        assert!(i < self.xs.len());
+        assert!(j < self.xs.len());
+        assert!(i < j);
+        self.cnt_ask += 1;
         self.xs[j] - self.xs[i] <= 10
     }
 }
 
 impl TestInteractive {
-    // 10倍しておく
+    // 座標は10倍して管理する
     fn new(xs: Vec<i64>) -> Self {
         TestInteractive { xs, cnt_ask: 0 }
     }
@@ -38,9 +43,7 @@ fn solve<T: IInteractive>(asker: &mut T, n: usize) -> usize {
         while begin < end && !asker.ask(begin, end) {
             begin += 1;
         }
-        // dbg!(begin, end);
-
-        ans += end - begin; // + 1 ？
+        ans += end - begin;
     }
     ans
 }
@@ -56,17 +59,75 @@ fn main() {
 #[cfg(test)]
 mod tests {
 
+    use rand::{Rng, SeedableRng, rngs::SmallRng};
+
     #[allow(unused_imports)]
     use super::*;
 
     #[test]
     fn test_problem() {
         let xs = vec![0, 7, 15, 100];
-        let n = 3;
+        let n = 4;
         let mut asker = TestInteractive::new(xs);
         let ans = solve(&mut asker, n);
         dbg!(asker.cnt_ask);
         dbg!(ans);
+    }
+
+    fn solve_naive(xs: &[i64]) -> usize {
+        xs.iter()
+            .copied()
+            .tuple_combinations()
+            .filter(|(x1, x2)| x2 - x1 <= 10)
+            .count()
+    }
+
+    /// 間違っていたら false を返す
+    fn process_one_test(rng: &mut SmallRng) -> bool {
+        // ==== 問題を作る ====
+        let n = rng.random_range(1..=10);
+        let mut xs = vec![rng.random_range(-100..100)];
+        for i in 1..n {
+            xs.push(xs[i - 1] + rng.random_range(1..30))
+        }
+
+        // ==== 解く ====
+        let mut asker = TestInteractive::new(xs.clone());
+        let main_ans = solve(&mut asker, n);
+        let naive_ans = solve_naive(&xs);
+
+        // ==== 間違っていたら報告をする ====
+        if main_ans != naive_ans || asker.cnt_ask > 2 * n {
+            // 問題を出力
+            println!("{:?}", (n, xs));
+            println!("main ans : {:?}", main_ans);
+            println!("naive ans: {:?}", naive_ans);
+            return false;
+        }
+        true
+    }
+    #[allow(unreachable_code)]
+    #[test]
+    #[ignore]
+    fn test_with_naive() {
+        let num_tests = 100;
+        let max_wrong_case = 10; // この件数間違いが見つかったら打ち切り
+        let mut cnt_wrong = 0;
+        let mut rng = SmallRng::seed_from_u64(42);
+        // let mut rng = SmallRng::from_os_rng();
+        for _ in 0..num_tests {
+            let is_ok = process_one_test(&mut rng);
+            if !is_ok {
+                cnt_wrong += 1;
+            }
+            if cnt_wrong >= max_wrong_case {
+                break;
+            }
+        }
+        if cnt_wrong > 0 {
+            println!("{} cases are wrong.", cnt_wrong);
+            panic!();
+        }
     }
 }
 
@@ -84,48 +145,56 @@ use proconio::{
 
 // ====== output func ======
 #[allow(unused_imports)]
-use print_vec::*;
-pub mod print_vec {
+use print_util::*;
+pub mod print_util {
     use itertools::Itertools;
     use proconio::fastout;
+
     #[fastout]
-    pub fn print_vec<T: std::fmt::Debug>(arr: &[T]) {
+    pub fn print_vec<T: std::fmt::Display>(arr: &[T]) {
         for a in arr {
-            println!("{:?}", a);
+            println!("{}", a);
         }
     }
+
     #[fastout]
-    pub fn print_vec_1line<T: std::fmt::Debug>(arr: &[T]) {
-        let msg = arr.iter().map(|x| format!("{:?}", x)).join(" ");
-        println!("{}", msg);
+    pub fn print_vec_1line<T: std::fmt::Display>(arr: &[T]) {
+        println!("{}", arr.iter().join(" "));
     }
+
     #[fastout]
-    pub fn print_vec2<T: std::fmt::Debug>(arr: &Vec<Vec<T>>) {
+    pub fn print_vec2<T: std::fmt::Display, R: AsRef<[T]>>(arr: &[R]) {
         for row in arr {
-            let msg = row.iter().map(|x| format!("{:?}", x)).join(" ");
-            println!("{}", msg);
+            println!("{}", row.as_ref().iter().join(" "));
         }
     }
+
     pub fn print_bytes(bytes: &[u8]) {
-        let msg = String::from_utf8(bytes.to_vec()).unwrap();
-        println!("{}", msg);
+        println!("{}", std::str::from_utf8(bytes).unwrap());
     }
+
+    pub fn print_chars(chars: &[char]) {
+        println!("{}", chars.iter().collect::<String>());
+    }
+
     #[fastout]
-    pub fn print_vec_bytes(vec_bytes: &[Vec<u8>]) {
+    pub fn print_vec_bytes<R: AsRef<[u8]>>(vec_bytes: &[R]) {
         for row in vec_bytes {
-            let msg = String::from_utf8(row.to_vec()).unwrap();
-            println!("{}", msg);
+            println!("{}", std::str::from_utf8(row.as_ref()).unwrap());
         }
     }
-}
 
-#[allow(unused)]
-fn print_yesno(ans: bool) {
-    let msg = if ans { "Yes" } else { "No" };
-    println!("{}", msg);
-}
+    #[fastout]
+    pub fn print_vec_chars<R: AsRef<[char]>>(vec_chars: &[R]) {
+        for row in vec_chars {
+            println!("{}", row.as_ref().iter().collect::<String>());
+        }
+    }
 
-// ====== snippet ======
+    pub fn print_yesno(ans: bool) {
+        println!("{}", if ans { "Yes" } else { "No" });
+    }
+}
 
 #[macro_export]
 macro_rules! println_flush {
@@ -138,3 +207,5 @@ macro_rules! println_flush {
         stdout().flush().unwrap();
     }};
 }
+
+// ====== snippet ======
